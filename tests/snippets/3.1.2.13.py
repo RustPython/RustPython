46257724e7d6548882e19fa9e850a54c0ebcb6b3
Daniel Watkins <daniel.watkins@canonical.com>
2018-08-04 00:41:31 -0400

    Add tests to snippet for stepped string slicing

--- tests/snippets/3.1.2.13.py
+++ tests/snippets/3.1.2.13.py
@@ -8,3 +8,8 @@ assert "on" == word[4:]
 assert "on" == word[-2:]
 assert "Py" == word[:-4]
 # assert "Py" == word[::2]
+
+assert "Pto" == word[::2]
+assert "yhn" == word[1::2]
+assert "Pt" == word[:4:2]
+assert "yh" == word[1:4:2]
