a0844f1f398abcfee76127b95063e0d2e56f110a
Daniel Watkins <daniel.watkins@canonical.com>
2018-08-05 15:58:15 -0400

    Refactor list slicing out to separate function

--- vm/src/objlist.rs
+++ vm/src/objlist.rs
@@ -9,6 +9,36 @@ fn get_pos(l: &Vec<PyObjectRef>, p: i32) -> usize {
     }
 }
 
+fn get_slice_items(l: &Vec<PyObjectRef>, slice: &PyObjectRef) -> Vec<PyObjectRef> {
+    // TODO: we could potentially avoid this copy and use slice
+    match &(slice.borrow()).kind {
+        PyObjectKind::Slice { start, stop, step } => {
+            let start = match start {
+                &Some(start) => get_pos(l, start),
+                &None => 0,
+            };
+            let stop = match stop {
+                &Some(stop) => get_pos(l, stop),
+                &None => l.len() as usize,
+            };
+            match step {
+                &None | &Some(1) => l[start..stop].to_vec(),
+                &Some(num) => {
+                    if num < 0 {
+                        unimplemented!("negative step indexing not yet supported")
+                    };
+                    l[start..stop]
+                        .iter()
+                        .step_by(num as usize)
+                        .cloned()
+                        .collect()
+                }
+            }
+        }
+        kind => panic!("get_slice_items called with non-slice: {:?}", kind),
+    }
+}
+
 pub fn get_item(vm: &mut VirtualMachine, l: &Vec<PyObjectRef>, b: PyObjectRef) -> PyResult {
     match &(b.borrow()).kind {
         PyObjectKind::Integer { value } => {
@@ -20,35 +50,16 @@ pub fn get_item(vm: &mut VirtualMachine, l: &Vec<PyObjectRef>, b: PyObjectRef) -
                 Err(vm.new_exception("Index out of bounds!".to_string()))
             }
         }
-        PyObjectKind::Slice { start, stop, step } => {
-            let start = match start {
-                &Some(start) => get_pos(l, start),
-                &None => 0,
-            };
-            let stop = match stop {
-                &Some(stop) => get_pos(l, stop),
-                &None => l.len() as usize,
-            };
-            // TODO: we could potentially avoid this copy and use slice
-            Ok(PyObject::new(
-                PyObjectKind::List {
-                    elements: match step {
-                        &None | &Some(1) => l[start..stop].to_vec(),
-                        &Some(num) => {
-                            if num < 0 {
-                                unimplemented!("negative step indexing not yet supported")
-                            };
-                            l[start..stop]
-                                .iter()
-                                .step_by(num as usize)
-                                .cloned()
-                                .collect()
-                        }
-                    },
-                },
-                vm.get_type(),
-            ))
-        }
+        PyObjectKind::Slice {
+            start: _,
+            stop: _,
+            step: _,
+        } => Ok(PyObject::new(
+            PyObjectKind::List {
+                elements: get_slice_items(l, &b),
+            },
+            vm.get_type(),
+        )),
         _ => Err(vm.new_exception(format!(
             "TypeError: indexing type {:?} with index {:?} is not supported (yet?)",
             l, b
