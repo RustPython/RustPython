20d73d7478b848ef080ddb81979bbc0412bef6ce
Windel Bouwman <windel@windel.nl>
2018-08-20 21:48:58 +0200

    Some formatting

--- vm/src/pyobject.rs
+++ vm/src/pyobject.rs
@@ -770,10 +770,9 @@ impl PartialEq for PyObject {
                 PyObjectKind::Integer { value: ref v1i },
                 PyObjectKind::Integer { value: ref v2i },
             ) => v2i == v1i,
-            (
-                PyObjectKind::Float { value: ref v1i },
-                PyObjectKind::Float { value: ref v2i },
-            ) => v2i == v1i,
+            ( PyObjectKind::Float { value: ref v1i }, PyObjectKind::Float { value: ref v2i }) => {
+                v2i == v1i
+            }
             (PyObjectKind::String { value: ref v1i }, PyObjectKind::String { value: ref v2i }) => {
                 *v2i == *v1i
             }
