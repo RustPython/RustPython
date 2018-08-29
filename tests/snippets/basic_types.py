af8407697976123d18db42fc37a59bc2a0177f3d
Windel Bouwman <windel@windel.nl>
2018-08-29 17:39:20 +0200

    Add check to assert issue #92 is fixed

--- tests/snippets/basic_types.py
+++ tests/snippets/basic_types.py
@@ -4,13 +4,20 @@ print(None)
 # print(True) # LOAD_NAME???
 print(1)
 # print(1L) # Long
-# print(1.1)
+print(1.1)
 # ComplexType
 print("abc")
 # print(u"abc")
 # Structural below
-# print((1, 2)) # Tuple can be any length, but fixed after declared
-# x = (1,2)
-# print(x[0]) # Tuple can be any length, but fixed after declared
-# print([1, 2, 3])
+print((1, 2)) # Tuple can be any length, but fixed after declared
+x = (1,2)
+print(x[0]) # Tuple can be any length, but fixed after declared
+print([1, 2, 3])
 # print({"first":1,"second":2})
+
+
+assert type(1 - 2) is int
+assert type(2 / 3) is float
+x = 1
+assert type(x) is int
+assert type(x - 1) is int
