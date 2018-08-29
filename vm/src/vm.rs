adf04a5452a7495667f7cec642c5a6842d2132c5
Windel Bouwman <windel@windel.nl>
2018-08-29 20:34:54 +0200

    Minor fixes

--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -975,7 +975,7 @@ mod tests {
         let a = vm.ctx.new_str(String::from("Hello "));
         let b = vm.ctx.new_int(4);
         let res = vm._mul(a, b).unwrap();
-        let value = objstr::get_value(res);
+        let value = objstr::get_value(&res);
         assert_eq!(value, String::from("Hello Hello Hello Hello "))
     }
 }
