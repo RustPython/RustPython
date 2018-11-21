2c55c4793eb58e0ca69c4cd1f6673298fd82adfe
Shing Lyu <shing.lyu@gmail.com>
2018-11-21 14:45:58 +0100

    [WASM] Mapping print to console.log

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,28 +1,34 @@
+mod wasm_builtins;
+
 extern crate rustpython_vm;
 extern crate wasm_bindgen;
-use rustpython_vm::compile;
-use rustpython_vm::VirtualMachine;
-use wasm_bindgen::prelude::*;
+extern crate web_sys;
 
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
+use rustpython_vm::VirtualMachine;
+use rustpython_vm::compile;
+use rustpython_vm::pyobject::AttributeProtocol;
+use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 #[wasm_bindgen]
 pub fn run_code(source: &str) -> () {
     //add hash in here
-    log("Running RustPython");
-    log(&source.to_string());
+    console::log_1(&"Running RustPython".into());
+    console::log_1(&"Running code:".into());
+    console::log_1(&source.to_string().into());
+    console::log_1(&"----- console -----".into());
+
     let mut vm = VirtualMachine::new();
+    // We are monkey-patching the builtin print to use console.log
+    // TODO: moneky-patch sys.stdout instead, after print actually uses sys.stdout
+    vm.builtins.set_attr("print", vm.context().new_rustfunc(wasm_builtins::builtin_print));
+
     let code_obj = compile::compile(&mut vm, &source.to_string(), compile::Mode::Exec, None);
+
     let builtins = vm.get_builtin_scope();
     let vars = vm.context().new_scope(Some(builtins));
     match vm.run_code_obj(code_obj.unwrap(), vars) {
-        Ok(_value) => log("Execution successful"),
-        Err(_) => log("Execution failed"),
+        Ok(_value) => console::log_1(&"Execution successful".into()),
+        Err(_) => console::log_1(&"Execution failed".into()),
     }
 }
