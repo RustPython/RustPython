b428f2e3b326d3f6b91795cefeb20560c498860d
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-12-15 00:49:47 -0600

    Improve demo site

--- wasm/app/index.js
+++ wasm/app/index.js
@@ -1,26 +1,30 @@
 import * as rp from "rustpython_wasm";
 
-function runCodeFromTextarea(_) {
-  const consoleElement = document.getElementById('console');
-  // Clean the console
-  consoleElement.value = '';
+window.rp = rp;
 
-  const code = document.getElementById('code').value;
+function runCodeFromTextarea(_) {
+  const consoleElement = document.getElementById("console");
+  const errorElement = document.getElementById("error");
+  // Clean the console
+  consoleElement.value = "";
+
+  const code = document.getElementById("code").value;
   try {
-    if (!code.endsWith('\n')) { // HACK: if the code doesn't end with newline it crashes.
-      rp.run_code(code + '\n');
+    if (!code.endsWith("\n")) {
+      // HACK: if the code doesn't end with newline it crashes.
+      rp.run_code(code + "\n");
       return;
     }
 
     rp.run_code(code);
-
-  } catch(e) {
-    consoleElement.value = 'Execution failed. Please check if your Python code has any syntax error.';
+  } catch (e) {
+    errorElement.textContent = e;
     console.error(e);
   }
-
 }
 
-document.getElementById('run-btn').addEventListener('click', runCodeFromTextarea);
+document
+  .getElementById("run-btn")
+  .addEventListener("click", runCodeFromTextarea);
 
 runCodeFromTextarea(); // Run once for demo
