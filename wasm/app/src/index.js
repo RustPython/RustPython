03c619f79eeff17f84a0a1492beb7b98c0ceede1
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-12-18 18:22:30 -0600

    Rearrange website directory and change webpack config

---
+++ wasm/app/src/index.js
@@ -0,0 +1,43 @@
+import * as rp from '../../pkg';
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/python/python';
+import 'codemirror/addon/comment/comment';
+
+// so people can play around with it
+window.rp = rp;
+
+const editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+    extraKeys: {
+        'Ctrl-Enter': runCodeFromTextarea,
+        'Cmd-Enter': runCodeFromTextarea,
+        'Shift-Tab': 'indentLess',
+        'Ctrl-/': 'toggleComment',
+        'Cmd-/': 'toggleComment'
+    },
+    mode: 'text/x-python',
+    indentUnit: 4,
+    autofocus: true
+});
+
+function runCodeFromTextarea() {
+    const consoleElement = document.getElementById('console');
+    const errorElement = document.getElementById('error');
+
+    // Clean the console and errors
+    consoleElement.value = '';
+    errorElement.textContent = '';
+
+    const code = editor.getValue();
+    try {
+        rp.run_from_textbox(code);
+    } catch (e) {
+        errorElement.textContent = e;
+        console.error(e);
+    }
+}
+
+document
+    .getElementById('run-btn')
+    .addEventListener('click', runCodeFromTextarea);
+
+runCodeFromTextarea(); // Run once for demo
--- wasm/app/webpack.config.js
+++ wasm/app/webpack.config.js
@@ -1,14 +1,26 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
-  },
-  mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+    entry: './src/bootstrap.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bootstrap.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })
+    ]
 };
