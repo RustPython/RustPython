8c469a64662e051bd6c0396d3875d572c3e73e47
ZapAnton <wc3ft2@gmail.com>
2019-02-05 15:25:07 +0300

    int type: Added tests for the __invert__

--- tests/snippets/numbers.py
+++ tests/snippets/numbers.py
@@ -12,3 +12,7 @@ assert type(x) is A
 assert int(2).__bool__() == True
 assert int(0.5).__bool__() == False
 assert int(-1).__bool__() == True
+
+assert int(0).__invert__() == -1
+assert int(-3).__invert__() == 2
+assert int(4).__invert__() == -5
