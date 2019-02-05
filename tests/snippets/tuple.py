fb58ca3e6cf784203ec7a7f766f59667d679d61c
Homer McMillan <him16@my.fsu.edu>
2019-02-04 23:31:39 -0500

    Add comparison tests for tuple

--- tests/snippets/tuple.py
+++ tests/snippets/tuple.py
@@ -12,3 +12,6 @@ assert x * 3 == (1, 2, 1, 2, 1, 2)
 # assert 3 * x == (1, 2, 1, 2, 1, 2)
 assert x * 0 == ()
 assert x * -1 == ()  # integers less than zero treated as 0
+
+assert y < x, "tuple __lt__ failed"
+assert x > y, "tuple __gt__ failed"
