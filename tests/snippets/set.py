358aa6b2c03b3390cd3d53cc7a244ff4028abf96
Aviv Palivoda <palaviv@gmail.com>
2019-02-08 17:50:36 +0200

    Add set.__gt__

--- tests/snippets/set.py
+++ tests/snippets/set.py
@@ -2,4 +2,9 @@ assert set([1,2]) == set([1,2])
 assert not set([1,2,3]) == set([1,2])
 
 assert set([1,2,3]) >= set([1,2])
+assert set([1,2]) >= set([1,2])
 assert not set([1,3]) >= set([1,2])
+
+assert set([1,2,3]) > set([1,2])
+assert not set([1,2]) > set([1,2])
+assert not set([1,3]) > set([1,2])
