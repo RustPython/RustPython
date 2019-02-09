09dc751869e1216c97132004baba42cc5872e9ec
Joey Hain <jmhain@protonmail.com>
2019-02-09 08:09:48 -0800

    Avoid using magic methods in tests where possible

--- tests/snippets/builtin_complex.py
+++ tests/snippets/builtin_complex.py
@@ -1,22 +1,23 @@
 # __abs__
 
-assert complex(3, 4).__abs__() == 5
-assert complex(3, -4).__abs__() == 5
-assert complex(1.5, 2.5).__abs__() == 2.9154759474226504
+assert abs(complex(3, 4)) == 5
+assert abs(complex(3, -4)) == 5
+assert abs(complex(1.5, 2.5)) == 2.9154759474226504
 
 # __eq__
 
-assert complex(1, -1).__eq__(complex(1, -1))
-assert complex(1, 0).__eq__(1)
-assert not complex(1, 1).__eq__(1)
-assert complex(1, 0).__eq__(1.0)
-assert not complex(1, 1).__eq__(1.0)
-assert not complex(1, 0).__eq__(1.5)
-assert complex(1, 0).__eq__(True)
-assert not complex(1, 2).__eq__(complex(1, 1))
+assert complex(1, -1) == complex(1, -1)
+assert complex(1, 0) == 1
+assert not complex(1, 1) == 1
+assert complex(1, 0) == 1.0
+assert not complex(1, 1) == 1.0
+assert not complex(1, 0) == 1.5
+assert bool(complex(1, 0))
+assert not complex(1, 2) == complex(1, 1)
+assert complex(1, 2) != 'foo'
 assert complex(1, 2).__eq__('foo') == NotImplemented
 
 # __neg__
 
-assert complex(1, -1).__neg__() == complex(-1, 1)
-assert complex(0, 0).__neg__() == complex(0, 0)
+assert -complex(1, -1) == complex(-1, 1)
+assert -complex(0, 0) == complex(0, 0)
