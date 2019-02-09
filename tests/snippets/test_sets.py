9f3cc35e9c3f138222ab40dbbbde13b84f44751e
Matthew Constable <bonsty@gmail.com>
2019-02-09 21:24:16 +0000

    Allow sets to be initialized from literals.

--- tests/snippets/test_sets.py
+++ tests/snippets/test_sets.py
@@ -1,9 +1,13 @@
 
 empty_set = set()
 non_empty_set = set([1,2,3])
+set_from_literal = {1,2,3}
 
 assert 1 in non_empty_set
 assert 4 not in non_empty_set
+
+assert 1 in set_from_literal
+assert 4 not in set_from_literal
 
 # TODO: Assert that empty aruguments raises exception.
 non_empty_set.add('a')
--- vm/src/frame.rs
+++ vm/src/frame.rs
@@ -225,8 +225,11 @@ impl Frame {
                 Ok(None)
             }
             bytecode::Instruction::BuildSet { size, unpack } => {
-                let _elements = self.get_elements(vm, *size, *unpack)?;
+                let elements = self.get_elements(vm, *size, *unpack)?;
                 let py_obj = vm.ctx.new_set();
+                for item in elements {
+                    vm.call_method(&py_obj, "add", vec![item])?;
+                }
                 self.push_value(py_obj);
                 Ok(None)
             }
