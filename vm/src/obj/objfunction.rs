c04f4219eb1ff2f249c0a6f46e97af5427eeca40
Tommaso Thea Cioni <zacktommy1118@gmail.com>
2019-02-11 13:35:59 +0100

    Removed the `__code__` attribute from the `builtin_function_or_method` type.

--- vm/src/obj/objfunction.rs
+++ vm/src/obj/objfunction.rs
@@ -22,12 +22,6 @@ pub fn init(context: &PyContext) {
         context.new_rustfunc(bind_method),
     );
 
-    context.set_attr(
-        &builtin_function_or_method_type,
-        "__code__",
-        context.new_member_descriptor(function_code),
-    );
-
     let member_descriptor_type = &context.member_descriptor_type;
     context.set_attr(
         &member_descriptor_type,
