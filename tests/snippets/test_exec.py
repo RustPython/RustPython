0d4887abe6d6fe77ffdaae9dc669cf865778adfe
Adam Kelly <adam@cthulahoops.org>
2019-03-04 14:22:48 +0000

    Fix missing asserts and add (disabled) tests for things that don't work yet.

--- tests/snippets/test_exec.py
+++ tests/snippets/test_exec.py
@@ -11,7 +11,28 @@ exec("assert 4 == x", {'x': 2}, {'x': 4})
 
 exec("assert max(1, 2) == 2", {}, {})
 
-exec("max(1, 5, square(5)) == 25", None)
+exec("assert max(1, 5, square(5)) == 25", None)
+
+#
+# These doesn't work yet:
+#
+# Local environment shouldn't replace global environment:
+#
+# exec("assert max(1, 5, square(5)) == 25", None, {})
+#
+# Closures aren't available if local scope is replaced:
+#
+# def g():
+#     seven = "seven"
+#     def f():
+#         try:
+#             exec("seven", None, {})
+#         except NameError:
+#             pass
+#         else:
+#             raise NameError("seven shouldn't be in scope")
+#     f()
+# g()
 
 try:
     exec("", 1)
