c1a5e31c3ca0213514a602238c0daf202cc47590
Windel Bouwman <windel@windel.nl>
2019-03-05 20:24:48 +0100

    Extend re module.

--- tests/snippets/test_re.py
+++ tests/snippets/test_re.py
@@ -4,5 +4,9 @@ import re
 haystack = "Hello world"
 needle = 'ello'
 
-print(re.search(needle, haystack))
+mo = re.search(needle, haystack)
+print(mo)
 
+assert isinstance(mo, re.Match)
+assert mo.start() == 1
+assert mo.end() == 5
