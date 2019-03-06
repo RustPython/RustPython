69efc7f223e84a71a68cdec39d04d511de631763
Windel Bouwman <windel@windel.nl>
2019-03-06 13:48:30 +0100

    Compile type annotations on function.

--- tests/snippets/type_hints.py
+++ tests/snippets/type_hints.py
@@ -1,7 +1,7 @@
 
 # See also: https://github.com/RustPython/RustPython/issues/587
 
-def curry(foo: int) -> float:
-    return foo * 3.1415926 * 2
+def curry(foo: int, bla=2) -> float:
+    return foo * 3.1415926 * bla
 
 assert curry(2) > 10
