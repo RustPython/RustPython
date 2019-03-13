6436d2cb9c37c23e5ec0099f5591c765d749bc6c
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-03-13 08:16:41 -0500

    fmt again

--- wasm/lib/src/vm_class.rs
+++ wasm/lib/src/vm_class.rs
@@ -289,7 +289,7 @@ impl WASMVirtualMachine {
                     Box::new(wasm_builtins::builtin_print_console)
                 } else {
                     return Err(
-                        TypeError::new("stdout must be null, a function or a css selector").into()
+                        TypeError::new("stdout must be null, a function or a css selector").into(),
                     );
                 };
             let rustfunc = vm.ctx.new_rustfunc(print_fn);
