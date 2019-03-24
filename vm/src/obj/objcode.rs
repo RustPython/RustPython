6eb93a7000a97318fb0c3a380d03fdc3818d304b
ben <benlewisj@gmail.com>
2019-03-24 13:50:50 +1300

    Move code methods into impl PyCodeRef

--- vm/src/obj/objcode.rs
+++ vm/src/obj/objcode.rs
@@ -5,9 +5,8 @@
 use std::fmt;
 
 use crate::bytecode;
-use crate::function::PyFuncArgs;
 use crate::obj::objtype::PyClassRef;
-use crate::pyobject::{IdProtocol, PyContext, PyObjectRef, PyRef, PyResult, PyValue, TypeProtocol};
+use crate::pyobject::{IdProtocol, PyContext, PyObjectRef, PyRef, PyResult, PyValue};
 use crate::vm::VirtualMachine;
 
 pub type PyCodeRef = PyRef<PyCode>;
@@ -34,62 +33,63 @@ impl PyValue for PyCode {
     }
 }
 
+impl PyCodeRef {
+    fn new(_cls: PyClassRef, vm: &VirtualMachine) -> PyResult {
+        Err(vm.new_type_error("Cannot directly create code object".to_string()))
+    }
+
+    fn repr(self, _vm: &VirtualMachine) -> String {
+        let code = &self.code;
+        format!(
+            "<code object {} at 0x{:x} file {:?}, line {}>",
+            code.obj_name,
+            self.get_id(),
+            code.source_path,
+            code.first_line_number
+        )
+    }
+
+    fn co_argcount(self, _vm: &VirtualMachine) -> usize {
+        self.code.arg_names.len()
+    }
+
+    fn co_filename(self, _vm: &VirtualMachine) -> String {
+        self.code.source_path.clone()
+    }
+
+    fn co_firstlineno(self, _vm: &VirtualMachine) -> usize {
+        self.code.first_line_number
+    }
+
+    fn co_kwonlyargcount(self, _vm: &VirtualMachine) -> usize {
+        self.code.kwonlyarg_names.len()
+    }
+
+    fn co_consts(self, vm: &VirtualMachine) -> PyObjectRef {
+        let consts = self
+            .code
+            .get_constants()
+            .map(|x| vm.ctx.unwrap_constant(x))
+            .collect();
+        vm.ctx.new_tuple(consts)
+    }
+
+    fn co_name(self, _vm: &VirtualMachine) -> String {
+        self.code.obj_name.clone()
+    }
+}
+
 pub fn init(context: &PyContext) {
     let code_type = context.code_type.as_object();
     extend_class!(context, code_type, {
-        "__new__" => context.new_rustfunc(code_new),
-        "__repr__" => context.new_rustfunc(code_repr),
+        "__new__" => context.new_rustfunc(PyCodeRef::new),
+        "__repr__" => context.new_rustfunc(PyCodeRef::repr),
 
-        "co_argcount" => context.new_property(code_co_argcount),
-        "co_consts" => context.new_property(code_co_consts),
-        "co_filename" => context.new_property(code_co_filename),
-        "co_firstlineno" => context.new_property(code_co_firstlineno),
-        "co_kwonlyargcount" => context.new_property(code_co_kwonlyargcount),
-        "co_name" => context.new_property(code_co_name),
+        "co_argcount" => context.new_property(PyCodeRef::co_argcount),
+        "co_consts" => context.new_property(PyCodeRef::co_consts),
+        "co_filename" => context.new_property(PyCodeRef::co_filename),
+        "co_firstlineno" => context.new_property(PyCodeRef::co_firstlineno),
+        "co_kwonlyargcount" => context.new_property(PyCodeRef::co_kwonlyargcount),
+        "co_name" => context.new_property(PyCodeRef::co_name),
     });
-}
-
-fn code_new(vm: &VirtualMachine, args: PyFuncArgs) -> PyResult {
-    arg_check!(vm, args, required = [(_cls, None)]);
-    Err(vm.new_type_error("Cannot directly create code object".to_string()))
-}
-
-fn code_repr(o: PyCodeRef, _vm: &VirtualMachine) -> String {
-    let code = &o.code;
-    format!(
-        "<code object {} at 0x{:x} file {:?}, line {}>",
-        code.obj_name,
-        o.get_id(),
-        code.source_path,
-        code.first_line_number
-    )
-}
-
-fn code_co_argcount(code: PyCodeRef, _vm: &VirtualMachine) -> usize {
-    code.code.arg_names.len()
-}
-
-fn code_co_filename(code: PyCodeRef, _vm: &VirtualMachine) -> String {
-    code.code.source_path.clone()
-}
-
-fn code_co_firstlineno(code: PyCodeRef, _vm: &VirtualMachine) -> usize {
-    code.code.first_line_number
-}
-
-fn code_co_kwonlyargcount(code: PyCodeRef, _vm: &VirtualMachine) -> usize {
-    code.code.kwonlyarg_names.len()
-}
-
-fn code_co_consts(code: PyCodeRef, vm: &VirtualMachine) -> PyObjectRef {
-    let consts = code
-        .code
-        .get_constants()
-        .map(|x| vm.ctx.unwrap_constant(x))
-        .collect();
-    vm.ctx.new_tuple(consts)
-}
-
-fn code_co_name(code: PyCodeRef, _vm: &VirtualMachine) -> String {
-    code.code.obj_name.clone()
 }
