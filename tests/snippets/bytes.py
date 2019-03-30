c1e1bdbf166990e25d90c9e4191ee85e6f590d5a
jgirardet <ijkl@netc.fr>
2019-03-30 23:07:30 +0100

    fix bytes.__repr__

--- tests/snippets/bytes.py
+++ tests/snippets/bytes.py
@@ -21,12 +21,13 @@ assert a == c
 # assert hash(iter(a)) == hash(iter(b"abcd"))
 
 # repr
-# assert repr(bytes([0, 1, 2])) == repr(b'\x00\x01\x02')
-# assert (
-# repr(bytes([0, 9, 10, 11, 13, 31, 32, 33, 89, 120, 255])
-# == "b'\\x00\\t\\n\\x0b\\r\\x1f !Yx\\xff'")
-# )
+assert repr(bytes([0, 1, 2])) == repr(b'\x00\x01\x02')
+assert (
+repr(bytes([0, 9, 10, 11, 13, 31, 32, 33, 89, 120, 255])
+== "b'\\x00\\t\\n\\x0b\\r\\x1f !Yx\\xff'")
+)
 assert repr(a) == "b'abcd'"
+
 assert len(a) == 4
 
 assert a + b == b"abcdab"
@@ -51,4 +52,3 @@ assert d[2:6] == b"cdef"
 assert d[-6:] == b"efghij"
 assert d[1:8:2] == b"bdfh"
 assert d[8:1:-2] == b"igec" 
-# assert d[-1:-8:-2] == b"jhfd"
