1612c954a17e38ed3c4f1fdee40096a9eabf468f
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-04-14 20:14:54 -0500

    Add weakproxy test

--- tests/snippets/weakrefs.py
+++ tests/snippets/weakrefs.py
@@ -1,4 +1,5 @@
-from _weakref import ref
+from _weakref import ref, proxy
+from testutils import assert_raises
 
 
 class X:
@@ -11,3 +12,17 @@ b = ref(a)
 assert callable(b)
 assert b() is a
 
+
+class G:
+    def __init__(self, h):
+        self.h = h
+
+
+g = G(5)
+p = proxy(g)
+
+assert p.h == 5
+
+del g
+
+assert_raises(ReferenceError, lambda: p.h)
