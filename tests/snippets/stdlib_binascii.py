070529d8c7b2e3f4fb9d6cfb04a89db96ac9ac54
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2019-05-11 02:16:32 +0300

    Add `binascii.unhexlify`

--- tests/snippets/stdlib_binascii.py
+++ tests/snippets/stdlib_binascii.py
@@ -12,3 +12,22 @@ assert h(1000 * b"x") == 1000 * b"78"
 
 with assertRaises(TypeError):
     h("a")
+
+
+# unhexlify tests
+uh = binascii.unhexlify
+
+assert uh(b"616263") == b"abc"
+assert uh(1000 * b"78") == 1000 * b"x"
+x = 1000 * b"1234"
+assert uh(h(x)) == x
+assert uh(b"ABCDEF") == b"\xab\xcd\xef"
+
+# unhexlify on strings not supported yet
+# assert uh("abcd") == b"\xab\xcd"
+
+with assertRaises(ValueError):
+    uh(b"a")  # Odd-length string
+
+with assertRaises(ValueError):
+    uh(b"nn")  # Non-hexadecimal digit found
--- vm/src/stdlib/binascii.rs
+++ vm/src/stdlib/binascii.rs
@@ -24,10 +24,43 @@ fn binascii_hexlify(vm: &VirtualMachine, args: PyFuncArgs) -> PyResult {
     Ok(vm.ctx.new_bytes(hex))
 }
 
+fn unhex_nibble(c: u8) -> Option<u8> {
+    match c {
+        b'0'..=b'9' => Some(c - b'0'),
+        b'a'..=b'f' => Some(c - b'a' + 10),
+        b'A'..=b'F' => Some(c - b'A' + 10),
+        _ => None,
+    }
+}
+
+fn binascii_unhexlify(vm: &VirtualMachine, args: PyFuncArgs) -> PyResult {
+    // TODO: allow 'str' hexstrings as well
+    arg_check!(vm, args, required = [(hexstr, Some(vm.ctx.bytes_type()))]);
+
+    let hex_bytes = &objbytes::get_value(hexstr);
+    if hex_bytes.len() % 2 != 0 {
+        return Err(vm.new_value_error("Odd-length string".to_string()));
+    }
+
+    let mut unhex = Vec::<u8>::with_capacity(hex_bytes.len() / 2);
+    for i in (0..hex_bytes.len()).step_by(2) {
+        let n1 = unhex_nibble(hex_bytes[i]);
+        let n2 = unhex_nibble(hex_bytes[i + 1]);
+        if n1.is_some() && n2.is_some() {
+            unhex.push(n1.unwrap() << 4 | n2.unwrap());
+        } else {
+            return Err(vm.new_value_error("Non-hexadecimal digit found".to_string()));
+        }
+    }
+
+    Ok(vm.ctx.new_bytes(unhex))
+}
+
 pub fn make_module(vm: &VirtualMachine) -> PyObjectRef {
     let ctx = &vm.ctx;
 
     py_module!(vm, "binascii", {
         "hexlify" => ctx.new_rustfunc(binascii_hexlify),
+        "unhexlify" => ctx.new_rustfunc(binascii_unhexlify),
     })
 }
