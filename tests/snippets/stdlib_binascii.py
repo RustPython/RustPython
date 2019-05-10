8dd664ef2013c4bb0c16cf71da2f89a8272f074c
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2019-05-11 00:29:42 +0300

    Add `binascii.hexlify`

--- tests/snippets/stdlib_binascii.py
+++ tests/snippets/stdlib_binascii.py
@@ -1,1 +1,14 @@
 import binascii
+from testutils import assertRaises
+
+
+# hexlify tests
+h = binascii.hexlify
+
+assert h(b"abc") == b"616263"
+assert h(1000 * b"x") == 1000 * b"78"
+# bytearray not supported yet
+# assert h(bytearray(b"a")) = b"61"
+
+with assertRaises(TypeError):
+    h("a")
--- vm/src/stdlib/binascii.rs
+++ vm/src/stdlib/binascii.rs
@@ -1,7 +1,33 @@
-use crate::pyobject::PyObjectRef;
+use crate::function::PyFuncArgs;
+use crate::obj::objbytes;
+use crate::pyobject::{PyObjectRef, PyResult};
 use crate::vm::VirtualMachine;
 
+fn hex_nibble(n: u8) -> u8 {
+    match n {
+        0..=9 => b'0' + n,
+        10..=15 => b'a' + n,
+        _ => unreachable!(),
+    }
+}
+
+fn binascii_hexlify(vm: &VirtualMachine, args: PyFuncArgs) -> PyResult {
+    arg_check!(vm, args, required = [(data, Some(vm.ctx.bytes_type()))]);
+
+    let bytes = objbytes::get_value(data);
+    let mut hex = Vec::<u8>::with_capacity(bytes.len() * 2);
+    for b in bytes.iter() {
+        hex.push(hex_nibble(b >> 4));
+        hex.push(hex_nibble(b & 0xf));
+    }
+
+    Ok(vm.ctx.new_bytes(hex))
+}
+
 pub fn make_module(vm: &VirtualMachine) -> PyObjectRef {
+    let ctx = &vm.ctx;
+
     py_module!(vm, "binascii", {
+        "hexlify" => ctx.new_rustfunc(binascii_hexlify),
     })
 }
