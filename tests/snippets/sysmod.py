152c1654d6e5138f626f0614594638fc77054d89
Aviv Palivoda <palaviv@gmail.com>
2019-05-11 11:43:11 +0300

    Add sys.platform

--- tests/snippets/sysmod.py
+++ tests/snippets/sysmod.py
@@ -2,3 +2,5 @@ import sys
 
 print(sys.argv)
 assert sys.argv[0].endswith('.py')
+
+assert sys.platform == "linux" or sys.platform == "darwin" or sys.platform == "win32" or sys.platform == "unknown"
