8197b17c1462ae14b3892476215d9004da73fc98
Aviv Palivoda <palaviv@gmail.com>
2019-05-25 15:07:51 +0300

    Add empty _imp locks methods

--- vm/src/stdlib/imp.rs
+++ vm/src/stdlib/imp.rs
@@ -5,10 +5,28 @@ fn imp_extension_suffixes(vm: &VirtualMachine) -> PyResult {
     Ok(vm.ctx.new_list(vec![]))
 }
 
+fn imp_acquire_lock(_vm: &VirtualMachine) -> PyResult<()> {
+    // TODO
+    Ok(())
+}
+
+fn imp_release_lock(_vm: &VirtualMachine) -> PyResult<()> {
+    // TODO
+    Ok(())
+}
+
+fn imp_lock_held(_vm: &VirtualMachine) -> PyResult<()> {
+    // TODO
+    Ok(())
+}
+
 pub fn make_module(vm: &VirtualMachine) -> PyObjectRef {
     let ctx = &vm.ctx;
     let module = py_module!(vm, "_imp", {
-        "extension_suffixes" => ctx.new_rustfunc(imp_extension_suffixes)
+        "extension_suffixes" => ctx.new_rustfunc(imp_extension_suffixes),
+        "acquire_lock" => ctx.new_rustfunc(imp_acquire_lock),
+        "release_lock" => ctx.new_rustfunc(imp_release_lock),
+        "lock_held" => ctx.new_rustfunc(imp_lock_held),
     });
 
     module
