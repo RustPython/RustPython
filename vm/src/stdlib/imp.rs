1294b56cbe5a7a7689ed5782feae10e42de48341
Aviv Palivoda <palaviv@gmail.com>
2019-06-01 14:32:23 +0300

    Add _imp.create_builtin

--- vm/src/stdlib/imp.rs
+++ vm/src/stdlib/imp.rs
@@ -1,5 +1,6 @@
+use crate::obj::objstr;
 use crate::obj::objstr::PyStringRef;
-use crate::pyobject::{PyObjectRef, PyResult};
+use crate::pyobject::{ItemProtocol, PyObjectRef, PyResult};
 use crate::vm::VirtualMachine;
 
 fn imp_extension_suffixes(vm: &VirtualMachine) -> PyResult {
@@ -29,6 +30,22 @@ fn imp_is_frozen(name: PyStringRef, vm: &VirtualMachine) -> bool {
     vm.frozen.borrow().contains_key(name.as_str())
 }
 
+fn imp_create_builtin(spec: PyObjectRef, vm: &VirtualMachine) -> PyResult {
+    let sys_modules = vm.get_attribute(vm.sys_module.clone(), "modules").unwrap();
+
+    let name = &objstr::get_value(&vm.get_attribute(spec.clone(), "name")?);
+
+    if let Ok(module) = sys_modules.get_item(name, vm) {
+        Ok(module)
+    } else {
+        if let Some(make_module_func) = vm.stdlib_inits.borrow().get(name) {
+            Ok(make_module_func(vm))
+        } else {
+            Ok(vm.get_none())
+        }
+    }
+}
+
 pub fn make_module(vm: &VirtualMachine) -> PyObjectRef {
     let ctx = &vm.ctx;
     let module = py_module!(vm, "_imp", {
@@ -38,6 +55,7 @@ pub fn make_module(vm: &VirtualMachine) -> PyObjectRef {
         "lock_held" => ctx.new_rustfunc(imp_lock_held),
         "is_builtin" => ctx.new_rustfunc(imp_is_builtin),
         "is_frozen" => ctx.new_rustfunc(imp_is_frozen),
+        "create_builtin" => ctx.new_rustfunc(imp_create_builtin),
     });
 
     module
