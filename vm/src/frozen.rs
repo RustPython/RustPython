37b40c55dabc4997ed4e5bd558fbd7dc780611d1
Aviv Palivoda <palaviv@gmail.com>
2019-06-07 19:15:16 +0300

    Add frozen _bootstrap_external.py

--- vm/src/frozen.rs
+++ vm/src/frozen.rs
@@ -16,9 +16,25 @@ const IMPORTLIB_BOOTSTRAP: &'static str = include_str!(concat!(
     "_bootstrap.py"
 ));
 
+const IMPORTLIB_BOOTSTRAP_EXTERNAL: &'static str = include_str!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/",
+    "..",
+    "/",
+    "Lib",
+    "/",
+    "importlib",
+    "/",
+    "_bootstrap_external.py"
+));
+
 pub fn get_module_inits() -> HashMap<String, &'static str> {
     let mut modules = HashMap::new();
     modules.insert("__hello__".to_string(), HELLO);
     modules.insert("_frozen_importlib".to_string(), IMPORTLIB_BOOTSTRAP);
+    modules.insert(
+        "_frozen_importlib_external".to_string(),
+        IMPORTLIB_BOOTSTRAP_EXTERNAL,
+    );
     modules
 }
