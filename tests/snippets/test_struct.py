9e194b390484b1d28c938493197077567be979d6
Windel Bouwman <windel@windel.nl>
2019-06-26 16:38:11 +0200

    Add endianness support to struct module.

--- tests/snippets/test_struct.py
+++ tests/snippets/test_struct.py
@@ -8,3 +8,17 @@ v1, v2 = struct.unpack('IH', data)
 assert v1 == 14
 assert v2 == 12
 
+data = struct.pack('<IH', 14, 12)
+assert data == bytes([14, 0, 0, 0, 12, 0])
+
+v1, v2 = struct.unpack('<IH', data)
+assert v1 == 14
+assert v2 == 12
+
+data = struct.pack('>IH', 14, 12)
+assert data == bytes([0, 0, 0, 14, 0, 12])
+
+v1, v2 = struct.unpack('>IH', data)
+assert v1 == 14
+assert v2 == 12
+
