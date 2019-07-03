95373ed82bf9f96fb5326434f202642cbc1cc198
Anand Jeyahar <anand@kaliberlabs.com>
2019-07-03 18:06:31 +0530

    some fixes

--- vm/src/obj/objrange.rs
+++ vm/src/obj/objrange.rs
@@ -249,13 +249,13 @@ impl PyRange {
         let start = self.start.as_bigint();
         let stop = self.stop.as_bigint();
         let step = self.step.as_bigint();
-        let bint_sz=BigInt::from(mem::size_of_val(&start));
-        let mut size = 0; 
+        let bint_sz = BigInt::from(mem::size_of_val(&start));
+        let mut size = 0;
         if start < stop {
-            size =  ((stop - start - 1usize) / step + 1) * bint_sz;
+            size =  ((stop - start - 1usize) / step + 1)*bint_sz;
         }
         if start > stop {
-            size = ((start - stop - 1usize) / (-step) + 1) * bint_sz;
+            size = ((start - stop - 1usize) / (-step) + 1)*bint_sz;
         }
         PyInt::new(size)
     }
