8162b87a0010c988ae0988a537ebcc4dd8cb9b04
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-07-03 16:13:16 +0300

    Do not pollute stack when if-expression condition evaluated to False

---
+++ tests/snippets/if_expressions.py
@@ -0,0 +1,26 @@
+def ret(expression):
+    return expression
+
+
+assert ret("0" if True else "1") == "0"
+assert ret("0" if False else "1") == "1"
+
+assert ret("0" if False else ("1" if True else "2")) == "1"
+assert ret("0" if False else ("1" if False else "2")) == "2"
+
+assert ret(("0" if True else "1") if True else "2") == "0"
+assert ret(("0" if False else "1") if True else "2") == "1"
+
+a = True
+b = False
+assert ret("0" if a or b else "1") == "0"
+assert ret("0" if a and b else "1") == "1"
+
+
+def func1():
+    return 0
+
+def func2():
+    return 20
+
+assert ret(func1() or func2()) == 20
