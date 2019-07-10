d16c288ef1e0bf0deece631ad76ddc85c8d22e4b
Windel Bouwman <windel@windel.nl>
2019-07-10 11:19:01 +0200

    Improve re module. Favor string.py over string.rs

--- tests/snippets/test_re.py
+++ tests/snippets/test_re.py
@@ -13,3 +13,21 @@ assert mo.start() == 1
 assert mo.end() == 5
 
 assert re.escape('python.exe') == 'python\\.exe'
+
+p = re.compile('ab')
+s = p.sub('x', 'abcabca')
+print(s)
+assert s == 'xcxca'
+
+idpattern = r'([_a-z][_a-z0-9]*)'
+
+mo = re.search(idpattern, '7382 _boe0+2')
+print(mo)
+# TODO:
+# assert mo.group(0) == '_boe0'
+
+from string import Template
+s = Template('$who likes $what')
+# TODO:
+# r = s.substitute(who='tim', what='kung pow')
+# print(r)
