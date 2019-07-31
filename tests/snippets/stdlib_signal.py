48da527295d7005cad1acbda66edae79bbf2a05b
Aviv Palivoda <palaviv@gmail.com>
2019-07-31 22:18:49 +0300

    Test signal only on unix

--- tests/snippets/stdlib_signal.py
+++ tests/snippets/stdlib_signal.py
@@ -1,5 +1,6 @@
 import signal
 import time
+import sys
 from testutils import assert_raises
 
 assert_raises(TypeError, lambda: signal.signal(signal.SIGINT, 2))
@@ -10,24 +11,26 @@ def handler(signum, frame):
 	signals.append(signum)
 
 
-signal.signal(signal.SIGALRM, handler)
-assert signal.getsignal(signal.SIGALRM) is handler
+# unix
+if "win" not in sys.platform:
+	signal.signal(signal.SIGALRM, handler)
+	assert signal.getsignal(signal.SIGALRM) is handler
 
-signal.alarm(1)
-time.sleep(2.0)
-assert signals == [signal.SIGALRM]
+	signal.alarm(1)
+	time.sleep(2.0)
+	assert signals == [signal.SIGALRM]
 
-signal.signal(signal.SIGALRM, signal.SIG_IGN)
-signal.alarm(1)
-time.sleep(2.0)
+	signal.signal(signal.SIGALRM, signal.SIG_IGN)
+	signal.alarm(1)
+	time.sleep(2.0)
 
-assert signals == [signal.SIGALRM]
+	assert signals == [signal.SIGALRM]
 
-signal.signal(signal.SIGALRM, handler)
-signal.alarm(1)
-time.sleep(2.0)
+	signal.signal(signal.SIGALRM, handler)
+	signal.alarm(1)
+	time.sleep(2.0)
 
-assert signals == [signal.SIGALRM, signal.SIGALRM]
+	assert signals == [signal.SIGALRM, signal.SIGALRM]
 
 
 
