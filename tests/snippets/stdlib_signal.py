f24c6daeb0de4e959faff9141d7cc437a00146bd
Aviv Palivoda <palaviv@gmail.com>
2019-08-03 10:37:56 +0300

    Add some windows test to signal

--- tests/snippets/stdlib_signal.py
+++ tests/snippets/stdlib_signal.py
@@ -3,16 +3,24 @@ import time
 import sys
 from testutils import assert_raises
 
+assert_raises(TypeError, lambda: signal.signal(signal.SIGINT, 2))
+
 signals = []
 
 def handler(signum, frame):
 	signals.append(signum)
 
 
+signal.signal(signal.SIGILL, signal.SIG_IGN);
+assert signal.getsignal(signal.SIGILL) is signal.SIG_IGN
+
+old_signal = signal.signal(signal.SIGILL, signal.SIG_DFL)
+assert old_signal is signal.SIG_IGN
+assert signal.getsignal(signal.SIGILL) is signal.SIG_DFL
+
+
 # unix
 if "win" not in sys.platform:
-	assert_raises(TypeError, lambda: signal.signal(signal.SIGINT, 2))
-
 	signal.signal(signal.SIGALRM, handler)
 	assert signal.getsignal(signal.SIGALRM) is handler
 
