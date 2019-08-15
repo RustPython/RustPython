78e56c2b5c2755fe24c7293c3179fda98ff00002
rusty <rusty@spoqa.com>
2019-08-15 17:01:11 +0900

    Update signature of __repr__ and __reduce__ method of Ellipsis

--- vm/src/obj/objellipsis.rs
+++ vm/src/obj/objellipsis.rs
@@ -1,5 +1,5 @@
 use crate::function::PyFuncArgs;
-use crate::pyobject::{PyContext, PyResult};
+use crate::pyobject::{PyContext, PyEllipsisRef, PyResult};
 use crate::vm::VirtualMachine;
 
 pub fn init(context: &PyContext) {
@@ -15,12 +15,10 @@ fn ellipsis_new(vm: &VirtualMachine, args: PyFuncArgs) -> PyResult {
     Ok(vm.ctx.ellipsis())
 }
 
-fn ellipsis_repr(vm: &VirtualMachine, args: PyFuncArgs) -> PyResult {
-    arg_check!(vm, args, required = [(_cls, None)]);
-    Ok(vm.new_str("Ellipsis".to_string()))
+fn ellipsis_repr(_self: PyEllipsisRef, _vm: &VirtualMachine) -> String {
+    "Ellipsis".to_string()
 }
 
-fn ellipsis_reduce(vm: &VirtualMachine, args: PyFuncArgs) -> PyResult {
-    arg_check!(vm, args, required = [(_cls, None)]);
-    Ok(vm.new_str("Ellipsis".to_string()))
+fn ellipsis_reduce(_self: PyEllipsisRef, _vm: &VirtualMachine) -> String {
+    "Ellipsis".to_string()
 }
