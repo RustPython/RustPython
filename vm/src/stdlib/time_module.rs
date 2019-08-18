32861cee464301e3da30f2279838fb15b6b6b98b
Jeong YunWon <jeong@youknowone.org>
2019-08-19 06:13:13 +0900

    Add slice support for PyTimeStructure

--- vm/src/stdlib/time_module.rs
+++ vm/src/stdlib/time_module.rs
@@ -2,16 +2,19 @@
 /// See also:
 /// https://docs.python.org/3/library/time.html
 use std::fmt;
+use std::ops::Range;
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use crate::function::{OptionalArg, PyFuncArgs};
-use crate::obj::objint::PyIntRef;
+use crate::obj::objint::PyInt;
 use crate::obj::objsequence::get_sequence_index;
+use crate::obj::objsequence::PySliceableSequence;
+use crate::obj::objslice::PySlice;
 use crate::obj::objstr::PyStringRef;
 use crate::obj::objtype::PyClassRef;
 use crate::obj::{objfloat, objint, objtype};
-use crate::pyobject::{PyClassImpl, PyObjectRef, PyRef, PyResult, PyValue};
+use crate::pyobject::{PyClassImpl, PyObjectRef, PyRef, PyResult, PyValue, TypeProtocol};
 use crate::vm::VirtualMachine;
 
 use num_traits::cast::ToPrimitive;
@@ -190,13 +193,13 @@ impl PyStructTime {
     }
 
     #[pymethod(name = "__repr__")]
-    fn repr(&self, vm: &VirtualMachine) -> String {
+    fn repr(&self, _vm: &VirtualMachine) -> String {
         // TODO: extract year day and isdst somehow..
         format!(
             "time.struct_time(tm_year={}, tm_mon={}, tm_mday={}, tm_hour={}, tm_min={}, tm_sec={}, tm_wday={}, tm_yday={}, tm_isdst={})",
-            self.tm_year(vm), self.tm_mon(vm), self.tm_mday(vm),
-            self.tm_hour(vm), self.tm_min(vm), self.tm_sec(vm),
-            self.tm_wday(vm), self.tm_yday(vm), self.tm_isdst(vm),
+            self._year(), self._mon(), self._mday(),
+            self._hour(), self._min(), self._sec(),
+            self._wday(), self._yday(), self._isdst(),
         )
     }
 
@@ -210,65 +213,178 @@ impl PyStructTime {
     }
 
     #[pymethod(name = "__getitem__")]
-    fn getitem(&self, needle: PyIntRef, vm: &VirtualMachine) -> PyResult {
-        let index = get_sequence_index(vm, &needle, 9)?;
-        match index {
-            0 => Ok(vm.ctx.new_int(self.tm_year(vm))),
-            1 => Ok(vm.ctx.new_int(self.tm_mon(vm))),
-            2 => Ok(vm.ctx.new_int(self.tm_mday(vm))),
-            3 => Ok(vm.ctx.new_int(self.tm_hour(vm))),
-            4 => Ok(vm.ctx.new_int(self.tm_min(vm))),
-            5 => Ok(vm.ctx.new_int(self.tm_sec(vm))),
-            6 => Ok(vm.ctx.new_int(self.tm_wday(vm))),
-            7 => Ok(vm.ctx.new_int(self.tm_yday(vm))),
-            8 => Ok(vm.ctx.new_int(self.tm_isdst(vm))),
-            _ => unreachable!(),
+    fn getitem(&self, subscript: PyObjectRef, vm: &VirtualMachine) -> PyResult {
+        if subscript.payload::<PyInt>().is_some() {
+            let needle = subscript.downcast().unwrap();
+            let index = get_sequence_index(vm, &needle, 9)?;
+            let tm_fn = TM_FUNCTIONS[index];
+            Ok(vm.new_int(tm_fn(self)))
+        } else if subscript.payload::<PySlice>().is_some() {
+            let values = self.get_slice_items(vm, &subscript)?;
+            let objs = values.iter().map(|v| vm.new_int(*v));
+            Ok(vm.ctx.new_tuple(objs.collect()))
+        } else {
+            Err(vm.new_type_error(format!(
+                "TypeError: tuple indices must be integers or slices, {}",
+                subscript.class().name
+            )))
         }
+    }
+
+    #[inline]
+    fn _year(&self) -> i32 {
+        self.tm.date().year()
+    }
+
+    #[inline]
+    fn _mon(&self) -> i32 {
+        self.tm.date().month() as i32
+    }
+
+    #[inline]
+    fn _mday(&self) -> i32 {
+        self.tm.date().day() as i32
+    }
+
+    #[inline]
+    fn _hour(&self) -> i32 {
+        self.tm.time().hour() as i32
+    }
+
+    #[inline]
+    fn _min(&self) -> i32 {
+        self.tm.time().minute() as i32
+    }
+
+    #[inline]
+    fn _sec(&self) -> i32 {
+        self.tm.time().second() as i32
+    }
+
+    #[inline]
+    fn _wday(&self) -> i32 {
+        self.tm.date().weekday().num_days_from_monday() as i32
+    }
+
+    #[inline]
+    fn _yday(&self) -> i32 {
+        self.tm.date().ordinal() as i32
+    }
+
+    #[inline]
+    fn _isdst(&self) -> i32 {
+        self.isdst
     }
 
     #[pyproperty(name = "tm_year")]
     fn tm_year(&self, _vm: &VirtualMachine) -> i32 {
-        self.tm.date().year()
+        self._year()
     }
 
     #[pyproperty(name = "tm_mon")]
     fn tm_mon(&self, _vm: &VirtualMachine) -> u32 {
-        self.tm.date().month()
+        self._mon() as u32
     }
 
     #[pyproperty(name = "tm_mday")]
     fn tm_mday(&self, _vm: &VirtualMachine) -> u32 {
-        self.tm.date().day()
+        self._mday() as u32
     }
 
     #[pyproperty(name = "tm_hour")]
     fn tm_hour(&self, _vm: &VirtualMachine) -> u32 {
-        self.tm.time().hour()
+        self._hour() as u32
     }
 
     #[pyproperty(name = "tm_min")]
     fn tm_min(&self, _vm: &VirtualMachine) -> u32 {
-        self.tm.time().minute()
+        self._min() as u32
     }
 
     #[pyproperty(name = "tm_sec")]
     fn tm_sec(&self, _vm: &VirtualMachine) -> u32 {
-        self.tm.time().second()
+        self._sec() as u32
     }
 
     #[pyproperty(name = "tm_wday")]
     fn tm_wday(&self, _vm: &VirtualMachine) -> u32 {
-        self.tm.date().weekday().num_days_from_monday()
+        self._wday() as u32
     }
 
     #[pyproperty(name = "tm_yday")]
     fn tm_yday(&self, _vm: &VirtualMachine) -> u32 {
-        self.tm.date().ordinal()
+        self._yday() as u32
     }
 
     #[pyproperty(name = "tm_isdst")]
     fn tm_isdst(&self, _vm: &VirtualMachine) -> i32 {
-        self.isdst
+        self._isdst()
+    }
+}
+
+type TmFunction = fn(&PyStructTime) -> i32;
+const TM_FUNCTIONS: [TmFunction; 9] = [
+    PyStructTime::_year,
+    PyStructTime::_mon,
+    PyStructTime::_mday,
+    PyStructTime::_hour,
+    PyStructTime::_min,
+    PyStructTime::_sec,
+    PyStructTime::_wday,
+    PyStructTime::_yday,
+    PyStructTime::_isdst,
+];
+
+impl PySliceableSequence for PyStructTime {
+    type Sliced = Vec<i32>;
+
+    fn do_slice(&self, range: Range<usize>) -> Self::Sliced {
+        if let Some(fs) = TM_FUNCTIONS.get(range) {
+            fs.iter().map(|f| f(self)).collect()
+        } else {
+            vec![]
+        }
+    }
+
+    fn do_slice_reverse(&self, range: Range<usize>) -> Self::Sliced {
+        if let Some(fs) = TM_FUNCTIONS.get(range) {
+            fs.iter().rev().map(|f| f(self)).collect()
+        } else {
+            vec![]
+        }
+    }
+
+    fn do_stepped_slice(&self, range: Range<usize>, step: usize) -> Self::Sliced {
+        if let Some(fs) = TM_FUNCTIONS.get(range) {
+            fs.iter().cloned().step_by(step).map(|f| f(self)).collect()
+        } else {
+            vec![]
+        }
+    }
+
+    fn do_stepped_slice_reverse(&self, range: Range<usize>, step: usize) -> Self::Sliced {
+        if let Some(fs) = TM_FUNCTIONS.get(range) {
+            fs.iter()
+                .rev()
+                .cloned()
+                .step_by(step)
+                .map(|f| f(self))
+                .collect()
+        } else {
+            vec![]
+        }
+    }
+
+    fn empty() -> Self::Sliced {
+        panic!("struct_time is not empty");
+    }
+
+    fn len(&self) -> usize {
+        TM_FUNCTIONS.len()
+    }
+
+    fn is_empty(&self) -> bool {
+        false
     }
 }
 
