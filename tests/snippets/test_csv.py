25f656105f8e9ad274a44e7b1ebbcc9068bf5529
kluid <kluid.me@yahoo.com>
2019-10-13 17:20:33 +0900

    Add more test for csv

--- tests/snippets/test_csv.py
+++ tests/snippets/test_csv.py
@@ -1,3 +1,5 @@
+from testutils import assert_raises
+
 import csv
 
 for row in csv.reader(['one,two,three']):
@@ -21,3 +23,23 @@ def f():
 	assert six == 'six'
 
 f()
+
+def test_delim():
+	iter = ['one|two|three', 'four|five|six']
+	reader = csv.reader(iter, delimiter='|')
+
+	[one,two,three] = next(reader)
+	[four,five,six] = next(reader)
+
+	assert one == 'one'
+	assert two == 'two'
+	assert three == 'three'
+	assert four == 'four'
+	assert five == 'five'
+	assert six == 'six'
+
+	with assert_raises(TypeError):
+		iter = ['one,,two,,three']
+		csv.reader(iter, delimiter=',,')
+
+test_delim()
