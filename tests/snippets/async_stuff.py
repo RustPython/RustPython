fe2946bc7919e3daff32df33c703c94e247ea783
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-10-16 22:29:04 -0500

    Add tests

--- tests/snippets/async_stuff.py
+++ tests/snippets/async_stuff.py
@@ -1,13 +1,53 @@
+import asyncio_slow as asyncio
 
-import sys
-import ast
 
-src = """
-async def x():
-    async for x in [1,2,3]:
-        await y()
-"""
+class ContextManager:
+    async def __aenter__(self):
+        print("Entrada")
+        ls.append(1)
+        return 1
 
-mod = ast.parse(src)
-# print(mod)
-# print(ast.dump(mod))
+    def __str__(self):
+        ls.append(2)
+        return "c'est moi!"
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        ls.append(3)
+        print("Wiedersehen")
+
+
+ls = []
+
+
+async def a(s, m):
+    async with ContextManager() as b:
+        print(f"val = {b}")
+    await asyncio.sleep(s)
+    for _ in range(0, 2):
+        ls.append(m)
+        await asyncio.sleep(1)
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(
+    asyncio.wait([a(0, "hello1"), a(0.5, "hello2"), a(1, "hello3"), a(1.5, "hello4")])
+)
+
+assert ls == [
+    1,
+    3,
+    "hello1",
+    1,
+    3,
+    1,
+    3,
+    1,
+    3,
+    "hello2",
+    "hello1",
+    "hello3",
+    "hello4",
+    "hello2",
+    "hello3",
+    "hello4",
+]
