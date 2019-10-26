d0b39f9f1dc7c4c321c4cb55f840b821f8531354
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-10-26 15:17:49 -0500

    Add select test

--- tests/snippets/stdlib_select.py
+++ tests/snippets/stdlib_select.py
@@ -2,14 +2,17 @@ from testutils import assert_raises
 
 import select
 import sys
+import socket
 
 
 class Nope:
     pass
 
+
 class Almost:
     def fileno(self):
-        return 'fileno'
+        return "fileno"
+
 
 assert_raises(TypeError, select.select, 1, 2, 3)
 assert_raises(TypeError, select.select, [Nope()], [], [])
@@ -17,8 +20,22 @@ assert_raises(TypeError, select.select, [Almost()], [], [])
 assert_raises(TypeError, select.select, [], [], [], "not a number")
 assert_raises(ValueError, select.select, [], [], [], -1)
 
-if 'win' in sys.platform:
+if "win" in sys.platform:
     assert_raises(OSError, select.select, [0], [], [])
 
-# TODO: actually test select functionality
+recvr = socket.socket()
 
+recvr.bind(("0.0.0.0", 9988))
+
+recvr.listen()
+
+recvr.settimeout(10.0)
+
+sendr = socket.socket()
+
+sendr.connect(("0.0.0.0", 9988))
+sendr.send(b"aaaa")
+
+res = select.select([recvr], [sendr], [])
+
+assert res == ([recvr.fileno()], [sendr.fileno()], [])
