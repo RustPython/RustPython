1fb868a08aa210db688b25fcf4b09ba8c9c9aa15
Aviv Palivoda <palaviv@gmail.com>
2019-11-01 14:29:58 +0200

    Add test for exception with_traceback

--- tests/snippets/stdlib_traceback.py
+++ tests/snippets/stdlib_traceback.py
@@ -3,5 +3,15 @@ import traceback
 try:
 	1/0
 except ZeroDivisionError as ex:
-	tb = traceback.format_list(traceback.extract_tb(ex.__traceback__))
+	tb = traceback.extract_tb(ex.__traceback__)
 	assert len(tb) == 1
+
+
+try:
+	try:
+		1/0
+	except ZeroDivisionError as ex:
+		 raise KeyError().with_traceback(ex.__traceback__)
+except KeyError as ex2:
+	tb = traceback.extract_tb(ex2.__traceback__)
+	assert tb[1].line == "1/0"
