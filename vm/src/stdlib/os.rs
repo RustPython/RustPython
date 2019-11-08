5ca0e9a92799eadf7e346507e9b4458b4a0114a3
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-11-07 21:54:42 -0600

    Add more IO exceptions

--- vm/src/stdlib/os.rs
+++ vm/src/stdlib/os.rs
@@ -21,6 +21,7 @@ use nix::pty::openpty;
 #[cfg(unix)]
 use nix::unistd::{self, Gid, Pid, Uid, Whence};
 
+use super::errno::errors;
 use crate::function::{IntoPyNativeFunc, OptionalArg, PyFuncArgs};
 use crate::obj::objbytes::PyBytesRef;
 use crate::obj::objdict::PyDictRef;
@@ -152,21 +153,21 @@ pub fn os_open(vm: &VirtualMachine, args: PyFuncArgs) -> PyResult {
 }
 
 pub fn convert_io_error(vm: &VirtualMachine, err: io::Error) -> PyObjectRef {
-    let os_error = match err.kind() {
-        ErrorKind::NotFound => {
-            let exc_type = vm.ctx.exceptions.file_not_found_error.clone();
-            vm.new_exception(exc_type, err.to_string())
-        }
-        ErrorKind::PermissionDenied => {
-            let exc_type = vm.ctx.exceptions.permission_error.clone();
-            vm.new_exception(exc_type, err.to_string())
-        }
-        ErrorKind::AlreadyExists => {
-            let exc_type = vm.ctx.exceptions.file_exists_error.clone();
-            vm.new_exception(exc_type, err.to_string())
-        }
-        _ => vm.new_os_error(err.to_string()),
+    #[allow(unreachable_patterns)] // some errors are just aliases of each other
+    let exc_type = match err.kind() {
+        ErrorKind::NotFound => vm.ctx.exceptions.file_not_found_error.clone(),
+        ErrorKind::PermissionDenied => vm.ctx.exceptions.permission_error.clone(),
+        ErrorKind::AlreadyExists => vm.ctx.exceptions.file_exists_error.clone(),
+        ErrorKind::WouldBlock => vm.ctx.exceptions.blocking_io_error.clone(),
+        _ => match err.raw_os_error() {
+            Some(errors::EAGAIN)
+            | Some(errors::EALREADY)
+            | Some(errors::EWOULDBLOCK)
+            | Some(errors::EINPROGRESS) => vm.ctx.exceptions.blocking_io_error.clone(),
+            _ => vm.ctx.exceptions.os_error.clone(),
+        },
     };
+    let os_error = vm.new_exception(exc_type, err.to_string());
     if let Some(errno) = err.raw_os_error() {
         vm.set_attr(&os_error, "errno", vm.ctx.new_int(errno))
             .unwrap();
