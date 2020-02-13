2b5e32aaab457bf9eae6005687d24502dd219fd7
Noah <33094578+coolreader18@users.noreply.github.com>
2020-02-13 22:12:27 +0000

    Make _random work on wasm

--- Lib/random.py
+++ Lib/random.py
@@ -41,7 +41,13 @@ from warnings import warn as _warn
 from types import MethodType as _MethodType, BuiltinMethodType as _BuiltinMethodType
 from math import log as _log, exp as _exp, pi as _pi, e as _e, ceil as _ceil
 from math import sqrt as _sqrt, acos as _acos, cos as _cos, sin as _sin
-from os import urandom as _urandom
+try:
+    from os import urandom as _urandom
+except ImportError:
+    # On wasm, _random.Random.random() does give a proper random value, but
+    # we don't have the os module
+    def _urandom(*args, **kwargs):
+        raise NotImplementedError("urandom")
 from _collections_abc import Set as _Set, Sequence as _Sequence
 from hashlib import sha512 as _sha512
 import itertools as _itertools
