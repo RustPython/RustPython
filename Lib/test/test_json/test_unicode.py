d92cebd9533b520985761907ef438fe83d5741d0
Noah <33094578+coolreader18@users.noreply.github.com>
2020-04-14 13:06:32 -0500

    Unskip tests that depend on \N

--- Lib/test/test_json/test_unicode.py
+++ Lib/test/test_json/test_unicode.py
@@ -8,25 +8,21 @@ class TestUnicode:
     # test_encoding1 and test_encoding2 from 2.x are irrelevant (only str
     # is supported as input, not bytes).
 
-    @unittest.skip("TODO: RUSTPYTHON")
     def test_encoding3(self):
         u = '\N{GREEK SMALL LETTER ALPHA}\N{GREEK CAPITAL LETTER OMEGA}'
         j = self.dumps(u)
         self.assertEqual(j, '"\\u03b1\\u03a9"')
 
-    @unittest.skip("TODO: RUSTPYTHON")
     def test_encoding4(self):
         u = '\N{GREEK SMALL LETTER ALPHA}\N{GREEK CAPITAL LETTER OMEGA}'
         j = self.dumps([u])
         self.assertEqual(j, '["\\u03b1\\u03a9"]')
 
-    @unittest.skip("TODO: RUSTPYTHON")
     def test_encoding5(self):
         u = '\N{GREEK SMALL LETTER ALPHA}\N{GREEK CAPITAL LETTER OMEGA}'
         j = self.dumps(u, ensure_ascii=False)
         self.assertEqual(j, '"{0}"'.format(u))
 
-    @unittest.skip("TODO: RUSTPYTHON")
     def test_encoding6(self):
         u = '\N{GREEK SMALL LETTER ALPHA}\N{GREEK CAPITAL LETTER OMEGA}'
         j = self.dumps([u], ensure_ascii=False)
