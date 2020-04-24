3929e2393acb16f7ac52cc86a261ceee0d3c4b2e
TheAnyKey <ich@simonroth.de>
2020-04-24 07:11:51 -0400

    Cleaned up comments

--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -4,8 +4,6 @@
 //!   https://github.com/ProgVal/pythonvm-rust/blob/master/src/processor/mod.rs
 //!
 
-// test
-
 use std::borrow::Borrow;
 use std::cell::{Cell, Ref, RefCell};
 use std::collections::hash_map::HashMap;
