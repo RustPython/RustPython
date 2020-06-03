806d4bdfd41f66287b85685bdd3f61ade4067bc9
Noah <33094578+coolreader18@users.noreply.github.com>
2020-06-02 19:35:09 -0500

    Refactor a lot of objsequence

--- vm/src/obj/objsequence.rs
+++ vm/src/obj/objsequence.rs
@@ -1,4 +1,3 @@
-use std::marker::Sized;
 use std::ops::Range;
 
 use num_bigint::{BigInt, ToBigInt};
@@ -24,33 +23,13 @@ pub trait PySliceableSequence {
 
     fn len(&self) -> usize;
     fn is_empty(&self) -> bool;
-    fn get_pos(&self, p: i32) -> Option<usize> {
-        if p < 0 {
-            if -p as usize > self.len() {
-                None
-            } else {
-                Some(self.len() - ((-p) as usize))
-            }
-        } else if p as usize >= self.len() {
-            None
-        } else {
-            Some(p as usize)
-        }
+
+    fn get_pos(&self, p: isize) -> Option<usize> {
+        get_pos(p, self.len())
     }
 
     fn get_slice_pos(&self, slice_pos: &BigInt) -> usize {
-        if let Some(pos) = slice_pos.to_i32() {
-            if let Some(index) = self.get_pos(pos) {
-                // within bounds
-                return index;
-            }
-        }
-
-        if slice_pos.is_negative() {
-            0
-        } else {
-            self.len()
-        }
+        get_slice_pos(slice_pos, self.len())
     }
 
     fn get_slice_range(&self, start: &Option<BigInt>, stop: &Option<BigInt>) -> Range<usize> {
@@ -63,64 +42,56 @@ pub trait PySliceableSequence {
         start..stop
     }
 
-    fn get_slice_items(&self, vm: &VirtualMachine, slice: &PyObjectRef) -> PyResult<Self::Sliced>
-    where
-        Self: Sized,
-    {
-        match slice.clone().downcast::<PySlice>() {
-            Ok(slice) => {
-                let start = slice.start_index(vm)?;
-                let stop = slice.stop_index(vm)?;
-                let step = slice.step_index(vm)?.unwrap_or_else(BigInt::one);
-                if step.is_zero() {
-                    Err(vm.new_value_error("slice step cannot be zero".to_owned()))
-                } else if step.is_positive() {
-                    let range = self.get_slice_range(&start, &stop);
-                    if range.start < range.end {
-                        #[allow(clippy::range_plus_one)]
-                        match step.to_i32() {
-                            Some(1) => Ok(self.do_slice(range)),
-                            Some(num) => Ok(self.do_stepped_slice(range, num as usize)),
-                            None => Ok(self.do_slice(range.start..range.start + 1)),
-                        }
-                    } else {
-                        Ok(Self::empty())
-                    }
-                } else {
-                    // calculate the range for the reverse slice, first the bounds needs to be made
-                    // exclusive around stop, the lower number
-                    let start = start.as_ref().map(|x| {
-                        if *x == (-1).to_bigint().unwrap() {
-                            self.len() + BigInt::one() //.to_bigint().unwrap()
-                        } else {
-                            x + 1
-                        }
-                    });
-                    let stop = stop.as_ref().map(|x| {
-                        if *x == (-1).to_bigint().unwrap() {
-                            self.len().to_bigint().unwrap()
-                        } else {
-                            x + 1
-                        }
-                    });
-                    let range = self.get_slice_range(&stop, &start);
-                    if range.start < range.end {
-                        match (-step).to_i32() {
-                            Some(1) => Ok(self.do_slice_reverse(range)),
-                            Some(num) => Ok(self.do_stepped_slice_reverse(range, num as usize)),
-                            None => Ok(self.do_slice(range.end - 1..range.end)),
-                        }
-                    } else {
-                        Ok(Self::empty())
-                    }
+    fn get_slice_items(&self, vm: &VirtualMachine, slice: &PySlice) -> PyResult<Self::Sliced> {
+        let start = slice.start_index(vm)?;
+        let stop = slice.stop_index(vm)?;
+        let step = slice.step_index(vm)?.unwrap_or_else(BigInt::one);
+        if step.is_zero() {
+            Err(vm.new_value_error("slice step cannot be zero".to_owned()))
+        } else if step.is_positive() {
+            let range = self.get_slice_range(&start, &stop);
+            if range.start < range.end {
+                #[allow(clippy::range_plus_one)]
+                match step.to_i32() {
+                    Some(1) => Ok(self.do_slice(range)),
+                    Some(num) => Ok(self.do_stepped_slice(range, num as usize)),
+                    None => Ok(self.do_slice(range.start..range.start + 1)),
                 }
+            } else {
+                Ok(Self::empty())
             }
-            payload => panic!("get_slice_items called with non-slice: {:?}", payload),
+        } else {
+            // calculate the range for the reverse slice, first the bounds needs to be made
+            // exclusive around stop, the lower number
+            let start = start.as_ref().map(|x| {
+                if *x == (-1).to_bigint().unwrap() {
+                    self.len() + BigInt::one() //.to_bigint().unwrap()
+                } else {
+                    x + 1
+                }
+            });
+            let stop = stop.as_ref().map(|x| {
+                if *x == (-1).to_bigint().unwrap() {
+                    self.len().to_bigint().unwrap()
+                } else {
+                    x + 1
+                }
+            });
+            let range = self.get_slice_range(&stop, &start);
+            if range.start < range.end {
+                match (-step).to_i32() {
+                    Some(1) => Ok(self.do_slice_reverse(range)),
+                    Some(num) => Ok(self.do_stepped_slice_reverse(range, num as usize)),
+                    None => Ok(self.do_slice(range.end - 1..range.end)),
+                }
+            } else {
+                Ok(Self::empty())
+            }
         }
     }
 }
 
-impl<T: Clone> PySliceableSequence for Vec<T> {
+impl<T: Clone> PySliceableSequence for [T] {
     type Sliced = Vec<T>;
 
     fn do_slice(&self, range: Range<usize>) -> Self::Sliced {
@@ -141,28 +112,31 @@ impl<T: Clone> PySliceableSequence for Vec<T> {
         self[range].iter().rev().step_by(step).cloned().collect()
     }
 
+    #[inline(always)]
     fn empty() -> Self::Sliced {
         Vec::new()
     }
 
+    #[inline(always)]
     fn len(&self) -> usize {
         self.len()
     }
 
+    #[inline(always)]
     fn is_empty(&self) -> bool {
         self.is_empty()
     }
 }
 
 pub enum SequenceIndex {
-    Int(i32),
+    Int(isize),
     Slice(PySliceRef),
 }
 
 impl TryFromObject for SequenceIndex {
     fn try_from_object(vm: &VirtualMachine, obj: PyObjectRef) -> PyResult<Self> {
         match_class!(match obj {
-            i @ PyInt => Ok(SequenceIndex::Int(i32::try_from_object(
+            i @ PyInt => Ok(SequenceIndex::Int(isize::try_from_object(
                 vm,
                 i.into_object()
             )?)),
@@ -200,6 +174,42 @@ pub fn get_sequence_index(vm: &VirtualMachine, index: &PyIntRef, length: usize) 
     }
 }
 
+pub fn get_pos(p: isize, len: usize) -> Option<usize> {
+    let neg = p.is_negative();
+    let p = p.abs().to_usize()?;
+    if neg {
+        len.checked_sub(p)
+    } else if p >= len {
+        None
+    } else {
+        Some(p)
+    }
+}
+
+pub fn get_slice_pos(slice_pos: &BigInt, len: usize) -> usize {
+    if let Some(pos) = slice_pos.to_isize() {
+        if let Some(index) = get_pos(pos, len) {
+            // within bounds
+            return index;
+        }
+    }
+
+    if slice_pos.is_negative() {
+        // slice past start bound, round to start
+        0
+    } else {
+        // slice past end bound, round to end
+        len
+    }
+}
+
+pub fn get_slice_range(start: &Option<BigInt>, stop: &Option<BigInt>, len: usize) -> Range<usize> {
+    let start = start.as_ref().map_or(0, |x| get_slice_pos(x, len));
+    let stop = stop.as_ref().map_or(len, |x| get_slice_pos(x, len));
+
+    start..stop
+}
+
 pub fn get_item(
     vm: &VirtualMachine,
     sequence: &PyObjectRef,
@@ -207,9 +217,9 @@ pub fn get_item(
     subscript: PyObjectRef,
 ) -> PyResult {
     if let Some(i) = subscript.payload::<PyInt>() {
-        return match i.as_bigint().to_i32() {
+        return match i.as_bigint().to_isize() {
             Some(value) => {
-                if let Some(pos_index) = elements.to_vec().get_pos(value) {
+                if let Some(pos_index) = get_pos(value, elements.len()) {
                     let obj = elements[pos_index].clone();
                     Ok(obj)
                 } else {
@@ -222,16 +232,16 @@ pub fn get_item(
         };
     }
 
-    if subscript.payload::<PySlice>().is_some() {
+    if let Some(slice) = subscript.payload::<PySlice>() {
         if sequence.payload::<PyList>().is_some() {
             Ok(PyObject::new(
-                PyList::from(elements.to_vec().get_slice_items(vm, &subscript)?),
+                PyList::from(elements.get_slice_items(vm, slice)?),
                 sequence.class(),
                 None,
             ))
         } else if sequence.payload::<PyTuple>().is_some() {
             Ok(PyObject::new(
-                PyTuple::from(elements.to_vec().get_slice_items(vm, &subscript)?),
+                PyTuple::from(elements.get_slice_items(vm, slice)?),
                 sequence.class(),
                 None,
             ))
