db45e07bcee2ce7a6932680a4b673f86ed84b9d9
Jeong YunWon <jeong@youknowone.org>
2020-08-10 02:08:06 +0900

    _functools into pymodule

--- vm/src/stdlib/functools.rs
+++ vm/src/stdlib/functools.rs
@@ -1,46 +1,44 @@
-use crate::function::OptionalArg;
-use crate::obj::objiter;
-use crate::obj::objtype;
-use crate::pyobject::{PyObjectRef, PyResult};
-use crate::vm::VirtualMachine;
+pub(crate) use _functools::make_module;
 
-pub fn make_module(vm: &VirtualMachine) -> PyObjectRef {
-    let ctx = &vm.ctx;
+#[pymodule]
+mod _functools {
+    use crate::function::OptionalArg;
+    use crate::obj::objiter;
+    use crate::obj::objtype;
+    use crate::pyobject::{PyObjectRef, PyResult};
+    use crate::vm::VirtualMachine;
 
-    py_module!(vm, "_functools", {
-        "reduce" => ctx.new_function(functools_reduce),
-    })
-}
+    #[pyfunction]
+    fn reduce(
+        function: PyObjectRef,
+        sequence: PyObjectRef,
+        start_value: OptionalArg<PyObjectRef>,
+        vm: &VirtualMachine,
+    ) -> PyResult {
+        let iterator = objiter::get_iter(vm, &sequence)?;
 
-fn functools_reduce(
-    function: PyObjectRef,
-    sequence: PyObjectRef,
-    start_value: OptionalArg<PyObjectRef>,
-    vm: &VirtualMachine,
-) -> PyResult {
-    let iterator = objiter::get_iter(vm, &sequence)?;
+        let start_value = if let OptionalArg::Present(val) = start_value {
+            val
+        } else {
+            objiter::call_next(vm, &iterator).map_err(|err| {
+                if objtype::isinstance(&err, &vm.ctx.exceptions.stop_iteration) {
+                    let exc_type = vm.ctx.exceptions.type_error.clone();
+                    vm.new_exception_msg(
+                        exc_type,
+                        "reduce() of empty sequence with no initial value".to_owned(),
+                    )
+                } else {
+                    err
+                }
+            })?
+        };
 
-    let start_value = if let OptionalArg::Present(val) = start_value {
-        val
-    } else {
-        objiter::call_next(vm, &iterator).map_err(|err| {
-            if objtype::isinstance(&err, &vm.ctx.exceptions.stop_iteration) {
-                let exc_type = vm.ctx.exceptions.type_error.clone();
-                vm.new_exception_msg(
-                    exc_type,
-                    "reduce() of empty sequence with no initial value".to_owned(),
-                )
-            } else {
-                err
-            }
-        })?
-    };
+        let mut accumulator = start_value;
 
-    let mut accumulator = start_value;
+        while let Ok(next_obj) = objiter::call_next(vm, &iterator) {
+            accumulator = vm.invoke(&function, vec![accumulator, next_obj])?
+        }
 
-    while let Ok(next_obj) = objiter::call_next(vm, &iterator) {
-        accumulator = vm.invoke(&function, vec![accumulator, next_obj])?
+        Ok(accumulator)
     }
-
-    Ok(accumulator)
 }
