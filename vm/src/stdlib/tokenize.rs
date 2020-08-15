7df01d3f21d164dae335d7af96a4d60e9ebb3fb1
Jeong YunWon <jeong@youknowone.org>
2020-08-15 16:53:42 +0900

    pymodule tokenize

--- vm/src/stdlib/tokenize.rs
+++ vm/src/stdlib/tokenize.rs
@@ -1,31 +1,27 @@
 /*
  * python tokenize module.
  */
-
-use std::iter::FromIterator;
-
-use crate::obj::objstr::PyStringRef;
-use crate::pyobject::{BorrowValue, PyObjectRef, PyResult};
-use crate::vm::VirtualMachine;
-use rustpython_parser::lexer;
-
-fn tokenize_tokenize(s: PyStringRef, vm: &VirtualMachine) -> PyResult {
-    let source = s.borrow_value();
-
-    // TODO: implement generator when the time has come.
-    let lexer1 = lexer::make_tokenizer(source);
-
-    let tokens = lexer1.map(|st| vm.ctx.new_str(format!("{:?}", st.unwrap().1)));
-    let tokens = Vec::from_iter(tokens);
-    Ok(vm.ctx.new_list(tokens))
-}
+pub(crate) use decl::make_module;
 
 // TODO: create main function when called with -m
+#[pymodule(name = "tokenize")]
+mod decl {
+    use std::iter::FromIterator;
 
-pub fn make_module(vm: &VirtualMachine) -> PyObjectRef {
-    let ctx = &vm.ctx;
+    use crate::obj::objstr::PyStringRef;
+    use crate::pyobject::{BorrowValue, PyResult};
+    use crate::vm::VirtualMachine;
+    use rustpython_parser::lexer;
 
-    py_module!(vm, "tokenize", {
-        "tokenize" => ctx.new_function(tokenize_tokenize)
-    })
+    #[pyfunction]
+    fn tokenize(s: PyStringRef, vm: &VirtualMachine) -> PyResult {
+        let source = s.borrow_value();
+
+        // TODO: implement generator when the time has come.
+        let lexer1 = lexer::make_tokenizer(source);
+
+        let tokens = lexer1.map(|st| vm.ctx.new_str(format!("{:?}", st.unwrap().1)));
+        let tokens = Vec::from_iter(tokens);
+        Ok(vm.ctx.new_list(tokens))
+    }
 }
