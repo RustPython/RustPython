9a443047388faaf31bd9d09bb86c823ff6ead9de
Jeong YunWon <jeong@youknowone.org>
2020-08-20 01:10:30 +0900

    PyEllipsis #[pyclass]

--- vm/src/obj/objellipsis.rs
+++ vm/src/obj/objellipsis.rs
@@ -1,30 +1,32 @@
 use super::objtype::{issubclass, PyClassRef};
-use crate::pyobject::{PyContext, PyEllipsisRef, PyResult};
+use crate::pyobject::{PyClassImpl, PyContext, PyEllipsis, PyResult};
 use crate::vm::VirtualMachine;
 
-pub fn init(context: &PyContext) {
-    extend_class!(context, &context.ellipsis_type, {
-        (slot new) => ellipsis_new,
-        "__repr__" => context.new_method(ellipsis_repr),
-        "__reduce__" => context.new_method(ellipsis_reduce),
-    });
+pub(crate) fn init(context: &PyContext) {
+    PyEllipsis::extend_class(context, &context.ellipsis_type);
 }
 
-fn ellipsis_new(cls: PyClassRef, vm: &VirtualMachine) -> PyResult {
-    if issubclass(&cls, &vm.ctx.ellipsis_type) {
-        Ok(vm.ctx.ellipsis())
-    } else {
-        Err(vm.new_type_error(format!(
-            "ellipsis.__new__({ty}): {ty} is not a subtype of ellipsis",
-            ty = cls,
-        )))
+#[pyimpl]
+impl PyEllipsis {
+    #[pyslot]
+    fn tp_new(cls: PyClassRef, vm: &VirtualMachine) -> PyResult {
+        if issubclass(&cls, &vm.ctx.ellipsis_type) {
+            Ok(vm.ctx.ellipsis())
+        } else {
+            Err(vm.new_type_error(format!(
+                "ellipsis.__new__({ty}): {ty} is not a subtype of ellipsis",
+                ty = cls,
+            )))
+        }
     }
-}
 
-fn ellipsis_repr(_self: PyEllipsisRef) -> String {
-    "Ellipsis".to_owned()
-}
+    #[pymethod(magic)]
+    fn repr(&self) -> String {
+        "Ellipsis".to_owned()
+    }
 
-fn ellipsis_reduce(_self: PyEllipsisRef) -> String {
-    "Ellipsis".to_owned()
+    #[pymethod(magic)]
+    fn reduce(&self) -> String {
+        "Ellipsis".to_owned()
+    }
 }
