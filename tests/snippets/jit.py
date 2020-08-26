9598392aea0c67435ac1a8be4e8ce6e03d8a854f
Noah <33094578+coolreader18@users.noreply.github.com>
2020-08-26 08:36:56 -0500

    Allow floats in jitted functions (#2150)

--- tests/snippets/jit.py
+++ tests/snippets/jit.py
@@ -3,10 +3,18 @@ def foo():
     a = 5
     return 10 + a
 
+def bar():
+    a = 1e6
+    return a / 5.0
 
-assert foo() == 15
+
+def tests():
+    assert foo() == 15
+    assert bar() == 2e5
+
+tests()
 
 if hasattr(foo, "__jit__"):
     print("Has jit")
     foo.__jit__()
-    assert foo() == 15
+    tests()
