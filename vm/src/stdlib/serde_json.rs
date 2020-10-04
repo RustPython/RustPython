2e3d76679fd21150a0bfc9c2b13b91928165826b
Noah <33094578+coolreader18@users.noreply.github.com>
2020-10-03 23:59:07 -0500

    Fix logic bug in serde_json.rs

--- vm/src/stdlib/serde_json.rs
+++ vm/src/stdlib/serde_json.rs
@@ -3,6 +3,7 @@ pub(crate) use _serde_json::make_module;
 #[pymodule]
 mod _serde_json {
     use crate::common::borrow::BorrowValue;
+    use crate::exceptions::PyBaseExceptionRef;
     use crate::obj::objstr::PyStrRef;
     use crate::py_serde;
     use crate::pyobject::{PyResult, TryFromObject};
@@ -17,22 +18,30 @@ mod _serde_json {
             Ok(res)
         })();
 
-        res.or_else(|err| {
-            let decode_error = vm.try_class("json", "JSONDecodeError")?;
-            let from_serde = vm.get_attribute(decode_error.into_object(), "_from_serde")?;
-            let mut err_msg = err.to_string();
-            let pos = err_msg.rfind(" at line ").unwrap();
-            err_msg.truncate(pos);
-            let decode_error = vm.invoke(
-                &from_serde,
-                vec![
-                    vm.ctx.new_str(err_msg),
-                    s.into_object(),
-                    vm.ctx.new_int(err.line()),
-                    vm.ctx.new_int(err.column()),
-                ],
-            )?;
-            TryFromObject::try_from_object(vm, decode_error)
+        res.map_err(|err| match json_exception(err, s, vm) {
+            Ok(x) | Err(x) => x,
         })
+    }
+
+    fn json_exception(
+        err: serde_json::Error,
+        s: PyStrRef,
+        vm: &VirtualMachine,
+    ) -> PyResult<PyBaseExceptionRef> {
+        let decode_error = vm.try_class("json", "JSONDecodeError")?;
+        let from_serde = vm.get_attribute(decode_error.into_object(), "_from_serde")?;
+        let mut err_msg = err.to_string();
+        let pos = err_msg.rfind(" at line ").unwrap();
+        err_msg.truncate(pos);
+        let decode_error = vm.invoke(
+            &from_serde,
+            vec![
+                vm.ctx.new_str(err_msg),
+                s.into_object(),
+                vm.ctx.new_int(err.line()),
+                vm.ctx.new_int(err.column()),
+            ],
+        )?;
+        PyBaseExceptionRef::try_from_object(vm, decode_error)
     }
 }
