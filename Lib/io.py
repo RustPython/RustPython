402594f9f2230ac23a790c92862021684fe9d534
Noah <33094578+coolreader18@users.noreply.github.com>
2020-11-06 00:18:49 -0600

    Update io.py to CPython 3.8.3

--- Lib/io.py
+++ Lib/io.py
@@ -41,8 +41,8 @@ __author__ = ("Guido van Rossum <guido@python.org>, "
               "Amaury Forgeot d'Arc <amauryfa@gmail.com>, "
               "Benjamin Peterson <benjamin@python.org>")
 
-__all__ = ["BlockingIOError", "open", "IOBase", "RawIOBase", "FileIO",
-           "BytesIO", "StringIO", "BufferedIOBase",
+__all__ = ["BlockingIOError", "open", "open_code", "IOBase", "RawIOBase",
+           "FileIO", "BytesIO", "StringIO", "BufferedIOBase",
            "BufferedReader", "BufferedWriter", "BufferedRWPair",
            "BufferedRandom", "TextIOBase", "TextIOWrapper",
            "UnsupportedOperation", "SEEK_SET", "SEEK_CUR", "SEEK_END"]
@@ -51,12 +51,17 @@ __all__ = ["BlockingIOError", "open", "IOBase", "RawIOBase", "FileIO",
 import _io
 import abc
 
-from _io import *
+from _io import (DEFAULT_BUFFER_SIZE, BlockingIOError, UnsupportedOperation,
+                 open, open_code, FileIO, BytesIO, StringIO, BufferedReader,
+                 BufferedWriter, BufferedRWPair, BufferedRandom,
+                 # XXX RUSTPYTHON TODO: IncrementalNewlineDecoder
+                 # IncrementalNewlineDecoder, TextIOWrapper)
+                 TextIOWrapper)
 
 OpenWrapper = _io.open # for compatibility with _pyio
 
 # Pretend this exception was created here.
-#UnsupportedOperation.__module__ = "io"
+UnsupportedOperation.__module__ = "io"
 
 # for seek()
 SEEK_SET = 0
@@ -83,8 +88,8 @@ try:
 except NameError:
     pass
 
-for klass in (BytesIO, BufferedReader, BufferedWriter):#, BufferedRandom,
-              #BufferedRWPair):
+for klass in (BytesIO, BufferedReader, BufferedWriter, BufferedRandom,
+              BufferedRWPair):
     BufferedIOBase.register(klass)
 
 for klass in (StringIO, TextIOWrapper):
