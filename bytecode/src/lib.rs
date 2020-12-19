3627e4ace1d6b7ce6d0b6f882c6da0f9ce271590
Noah <33094578+coolreader18@users.noreply.github.com>
2020-12-19 14:34:38 -0600

    Move bytecode.rs to lib.rs

--- bytecode/src/lib.rs
+++ bytecode/src/lib.rs
@@ -1,6 +1,9 @@
 //! Implement python as a virtual machine with bytecodes. This module
 //! implements bytecode structure.
 
+#![doc(html_logo_url = "https://raw.githubusercontent.com/RustPython/RustPython/master/logo.png")]
+#![doc(html_root_url = "https://docs.rs/rustpython-bytecode/")]
+
 use bitflags::bitflags;
 use bstr::ByteSlice;
 use itertools::Itertools;
