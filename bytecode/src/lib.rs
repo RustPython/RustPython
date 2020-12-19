f1807c76750bbd18d4d93e57c731464f76c20394
Noah <33094578+coolreader18@users.noreply.github.com>
2020-12-19 14:34:12 -0600

    Remove bytecode lib.rs

--- bytecode/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#![doc(html_logo_url = "https://raw.githubusercontent.com/RustPython/RustPython/master/logo.png")]
-#![doc(html_root_url = "https://docs.rs/rustpython-bytecode/")]
-
-pub mod bytecode;
