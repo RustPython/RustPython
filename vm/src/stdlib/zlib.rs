9e5716124a0d4910635d444a6f41f9b847fd8419
Noah <33094578+coolreader18@users.noreply.github.com>
2021-01-23 22:41:02 -0600

    Don't panic on invalid wbits

--- vm/src/stdlib/zlib.rs
+++ vm/src/stdlib/zlib.rs
@@ -89,10 +89,14 @@ mod decl {
         Ok(vm.ctx.new_bytes(encoded_bytes))
     }
 
-    // TODO: validate wbits value here
-    fn header_from_wbits(wbits: OptionalArg<i8>) -> (bool, u8) {
+    fn header_from_wbits(wbits: OptionalArg<i8>, vm: &VirtualMachine) -> PyResult<(bool, u8)> {
         let wbits = wbits.unwrap_or(MAX_WBITS as i8);
-        (wbits > 0, wbits.abs() as u8)
+        let header = wbits > 0;
+        let wbits = wbits.abs() as u8;
+        match wbits {
+            9..=15 => Ok((header, wbits)),
+            _ => Err(vm.new_value_error("Invalid initialization option".to_owned())),
+        }
     }
 
     fn _decompress(
@@ -172,7 +176,7 @@ mod decl {
         vm: &VirtualMachine,
     ) -> PyResult<Vec<u8>> {
         data.with_ref(|data| {
-            let (header, wbits) = header_from_wbits(wbits);
+            let (header, wbits) = header_from_wbits(wbits, vm)?;
             let bufsize = bufsize.unwrap_or(DEF_BUF_SIZE);
 
             let mut d = Decompress::new_with_window_bits(header, wbits);
@@ -187,18 +191,18 @@ mod decl {
     }
 
     #[pyfunction]
-    fn decompressobj(args: DecopmressobjArgs, vm: &VirtualMachine) -> PyDecompress {
-        let (header, wbits) = header_from_wbits(args.wbits);
+    fn decompressobj(args: DecopmressobjArgs, vm: &VirtualMachine) -> PyResult<PyDecompress> {
+        let (header, wbits) = header_from_wbits(args.wbits, vm)?;
         let mut decompress = Decompress::new_with_window_bits(header, wbits);
         if let OptionalArg::Present(dict) = args.zdict {
             dict.with_ref(|d| decompress.set_dictionary(d).unwrap());
         }
-        PyDecompress {
+        Ok(PyDecompress {
             decompress: PyMutex::new(decompress),
             eof: AtomicCell::new(false),
             unused_data: PyMutex::new(PyBytes::from(vec![]).into_ref(vm)),
             unconsumed_tail: PyMutex::new(PyBytes::from(vec![]).into_ref(vm)),
-        }
+        })
     }
     #[pyattr]
     #[pyclass(name = "Decompress")]
@@ -350,7 +354,7 @@ mod decl {
         _zdict: OptionalArg<PyBytesLike>,
         vm: &VirtualMachine,
     ) -> PyResult<PyCompress> {
-        let (header, wbits) = header_from_wbits(wbits);
+        let (header, wbits) = header_from_wbits(wbits, vm)?;
         let level = level.unwrap_or(-1);
 
         let level = match level {
