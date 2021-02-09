02282cb080244b69a94483b6413dbaadd06371f3
Padraic Fanning <fanninpm@miamioh.edu>
2021-02-09 11:00:55 -0500

    Explain test_mac_ver skip

--- Lib/test/test_platform.py
+++ Lib/test/test_platform.py
@@ -220,6 +220,7 @@ class PlatformTest(unittest.TestCase):
     def test_win32_ver(self):
         res = platform.win32_ver()
 
+    @unittest.skip("TODO: RUSTPYTHON, NameError: name 'ParserCreate' is not defined")
     def test_mac_ver(self):
         res = platform.mac_ver()
 
