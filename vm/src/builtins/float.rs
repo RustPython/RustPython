fae7b0e8ed19178ea89bf1eb85328fba8a0d9bb2
Padraic Fanning <fanninpm@miamioh.edu>
2021-02-14 18:12:52 -0500

    Implement __ceil__ for float type
    Co-authored-by: Tetramad <cbwjd777@gmail.com>

--- vm/src/builtins/float.rs
+++ vm/src/builtins/float.rs
@@ -389,6 +389,11 @@ impl PyFloat {
         try_bigint(self.value, vm)
     }
 
+    #[pymethod(magic)]
+    fn ceil(&self, vm: &VirtualMachine) -> PyResult<BigInt> {
+        try_bigint(self.value.ceil(), vm)
+    }
+
     #[pymethod(name = "__round__")]
     fn round(&self, ndigits: OptionalOption<PyIntRef>, vm: &VirtualMachine) -> PyResult {
         let ndigits = ndigits.flatten();
