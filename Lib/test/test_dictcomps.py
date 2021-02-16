4b086785f108316591144db600f5064f2de9e12b
Padraic Fanning <fanninpm@miamioh.edu>
2021-02-16 18:21:16 -0500

    Mark failing test

--- Lib/test/test_dictcomps.py
+++ Lib/test/test_dictcomps.py
@@ -72,6 +72,8 @@ class DictComprehensionTest(unittest.TestCase):
         self.assertEqual(actual, expected)
         self.assertEqual(v, "Local variable")
 
+    # TODO: RUSTPYTHON
+    @unittest.expectedFailure
     def test_illegal_assignment(self):
         with self.assertRaisesRegex(SyntaxError, "cannot assign"):
             compile("{x: y for y, x in ((1, 2), (3, 4))} = 5", "<test>",
