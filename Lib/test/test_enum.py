2521e37bfd243ce39a68058397d574b71daac427
Padraic Fanning <fanninpm@miamioh.edu>
2021-02-19 17:03:26 -0500

    Add universal newlines skip for Windows

--- Lib/test/test_enum.py
+++ Lib/test/test_enum.py
@@ -2516,8 +2516,7 @@ class TestFlag(unittest.TestCase):
         self.assertEqual(Color.ALL.value, 7)
         self.assertEqual(str(Color.BLUE), 'blue')
 
-    # TODO: RUSTPYTHON
-    @unittest.expectedFailure
+    @unittest.skipIf(sys.platform == "nt", "TODO: RUSTPYTHON, universal newlines")
     @support.reap_threads
     def test_unique_composite(self):
         # override __eq__ to be identity only
