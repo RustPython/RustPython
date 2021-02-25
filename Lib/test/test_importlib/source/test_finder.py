1bebcb9ae9998d73ee5ed7b27fd7ebf089ce11d4
Padraic Fanning <fanninpm@miamioh.edu>
2021-02-25 17:27:37 -0500

    Unskip test(s) in source.test_finder

--- Lib/test/test_importlib/source/test_finder.py
+++ Lib/test/test_importlib/source/test_finder.py
@@ -178,7 +178,8 @@ class FinderTests(abc.FinderTests):
         found = self._find(finder, 'doesnotexist')
         self.assertEqual(found, self.NOT_FOUND)
 
-    @unittest.skip("TODO: RUSTPYTHON, OSError: Not a directory (os error 20)")
+    # TODO: RUSTPYTHON
+    @unittest.expectedFailure
     def test_ignore_file(self):
         # If a directory got changed to a file from underneath us, then don't
         # worry about looking for submodules.
