18491e7bb8f351f5d5a081da9b6df1f5529fef81
Padraic Fanning <fanninpm@miamioh.edu>
2021-02-25 20:18:39 -0500

    Unskip tests in test_importlib.test_util
    WARNING: Contains a monkeypatch to accomodate differential testing
    behavior.

--- Lib/test/test_importlib/test_util.py
+++ Lib/test/test_importlib/test_util.py
@@ -21,19 +21,22 @@ class DecodeSourceBytesTests:
 
     source = "string ='ü'"
 
-    @unittest.skip("TODO: RUSTPYTHON, AttributeError: module 'tokenize' has no attribute 'detect_encoding'")
+    # TODO: RUSTPYTHON
+    @unittest.expectedFailure
     def test_ut8_default(self):
         source_bytes = self.source.encode('utf-8')
         self.assertEqual(self.util.decode_source(source_bytes), self.source)
 
-    @unittest.skip("TODO: RUSTPYTHON, AttributeError: module 'tokenize' has no attribute 'detect_encoding'")
+    # TODO: RUSTPYTHON
+    @unittest.expectedFailure
     def test_specified_encoding(self):
         source = '# coding=latin-1\n' + self.source
         source_bytes = source.encode('latin-1')
         assert source_bytes != source.encode('utf-8')
         self.assertEqual(self.util.decode_source(source_bytes), source)
 
-    @unittest.skip("TODO: RUSTPYTHON, AttributeError: module 'tokenize' has no attribute 'detect_encoding'")
+    # TODO: RUSTPYTHON
+    @unittest.expectedFailure
     def test_universal_newlines(self):
         source = '\r\n'.join([self.source, self.source])
         source_bytes = source.encode('utf-8')
@@ -546,7 +549,6 @@ class MagicNumberTests:
         # Should be 4 bytes.
         self.assertEqual(len(self.util.MAGIC_NUMBER), 4)
 
-    @unittest.skip("TODO: RUSTPYTHON, Source_MagicNumberTests passes while Frozen_MagicNumberTests fails")
     def test_incorporates_rn(self):
         # The magic number uses \r\n to come out wrong when splitting on lines.
         self.assertTrue(self.util.MAGIC_NUMBER.endswith(b'\r\n'))
@@ -556,6 +558,13 @@ class MagicNumberTests:
  Source_MagicNumberTests
  ) = util.test_both(MagicNumberTests, util=importlib_util)
 
+# TODO: RUSTPYTHON
+@unittest.expectedFailure
+def test_incorporates_rn_MONKEYPATCH(self):
+    self.assertTrue(self.util.MAGIC_NUMBER.endswith(b'\r\n'))
+
+# TODO: RUSTPYTHON
+Frozen_MagicNumberTests.test_incorporates_rn = test_incorporates_rn_MONKEYPATCH
 
 class PEP3147Tests:
 
