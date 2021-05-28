6d4de560f7cdcdebcd4e5c46c6394d5c31a821d0
Noah <33094578+coolreader18@users.noreply.github.com>
2021-05-28 16:06:19 -0500

    Don't use RAND_pseudo_bytes anymore

--- stdlib/src/ssl.rs
+++ stdlib/src/ssl.rs
@@ -43,7 +43,6 @@ mod sys {
         #[cfg(ossl111)]
         pub fn SSL_CTX_set_post_handshake_auth(ctx: *mut SSL_CTX, val: c_int);
         pub fn RAND_add(buf: *const c_void, num: c_int, randomness: c_double);
-        pub fn RAND_pseudo_bytes(buf: *const u8, num: c_int) -> c_int;
         pub fn COMP_get_type(meth: *const COMP_METHOD) -> i32;
         pub fn d2i_X509_bio(b: *mut BIO, a: *mut *mut X509) -> *mut X509;
     }
@@ -279,7 +278,7 @@ fn _ssl_rand_pseudo_bytes(n: i32, vm: &VirtualMachine) -> PyResult<(Vec<u8>, boo
         return Err(vm.new_value_error("num must be positive".to_owned()));
     }
     let mut buf = vec![0; n as usize];
-    let ret = unsafe { sys::RAND_pseudo_bytes(buf.as_mut_ptr(), n) };
+    let ret = unsafe { sys::RAND_bytes(buf.as_mut_ptr(), n) };
     match ret {
         0 | 1 => Ok((buf, ret == 1)),
         _ => Err(convert_openssl_error(vm, ErrorStack::get())),
