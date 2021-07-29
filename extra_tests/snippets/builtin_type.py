f0edc4234dc80579be223421df8fddebd8d75683
sobolevn <mail@sobolevn.me>
2021-07-29 20:11:21 +0300

    Fixes `type.__module__` issue, refs #2310

--- extra_tests/snippets/builtin_type.py
+++ extra_tests/snippets/builtin_type.py
@@ -3,5 +3,7 @@ assert type.__qualname__ == 'type'
 assert type.__name__ == 'type'
 assert isinstance(type.__doc__, str)
 
+# Regression to
+# https://github.com/RustPython/RustPython/issues/2310
 import builtins
 assert builtins.iter.__class__.__module__ == 'builtins'
