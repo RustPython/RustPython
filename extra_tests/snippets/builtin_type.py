8f2679a97ae7a4e375ffc3920d7ab37bcfd6c06c
Nikita Sobolev <mail@sobolevn.me>
2021-08-04 07:59:40 +0300

    Adds `__qualname__` to `PyBuiltinFunction`

--- extra_tests/snippets/builtin_type.py
+++ extra_tests/snippets/builtin_type.py
@@ -109,3 +109,17 @@ assert MyTypeWithMethod().N.s.__qualname__ == 'MyTypeWithMethod.N.s'
 assert repr(str.replace) == "<method 'replace' of 'str' objects>"
 assert repr(str.replace) == str(str.replace)
 assert repr(int.to_bytes) == "<method 'to_bytes' of 'int' objects>"
+
+
+# Regression to
+# https://github.com/RustPython/RustPython/issues/2788
+
+assert iter.__qualname__ == iter.__name__ == 'iter'
+assert max.__qualname__ == max.__name__ == 'max'
+assert min.__qualname__ ==  min.__name__ == 'min'
+
+
+def custom_func():
+    pass
+
+assert custom_func.__qualname__ == 'custom_func'
