52149d0e708261eb69456e81a2e62e79e1f657d1
jfh <jfh@imijmi.eu>
2021-08-19 13:46:14 +0300

    Add remaining methods to sequence iterator.

--- vm/src/builtins/iter.rs
+++ vm/src/builtins/iter.rs
@@ -5,6 +5,7 @@
 use crossbeam_utils::atomic::AtomicCell;
 
 use super::pytype::PyTypeRef;
+use super::{int, PyInt};
 use crate::slots::PyIter;
 use crate::vm::VirtualMachine;
 use crate::{
@@ -24,8 +25,9 @@ pub enum IterStatus {
 #[pyclass(module = false, name = "iterator")]
 #[derive(Debug)]
 pub struct PySequenceIterator {
-    pub position: AtomicCell<isize>,
+    pub position: AtomicCell<usize>,
     pub obj: PyObjectRef,
+    pub status: AtomicCell<IterStatus>,
 }
 
 impl PyValue for PySequenceIterator {
@@ -36,26 +38,69 @@ impl PyValue for PySequenceIterator {
 
 #[pyimpl(with(PyIter))]
 impl PySequenceIterator {
-    pub fn new_forward(obj: PyObjectRef) -> Self {
+    pub fn new(obj: PyObjectRef) -> Self {
         Self {
             position: AtomicCell::new(0),
             obj,
+            status: AtomicCell::new(IterStatus::Active),
         }
     }
 
     #[pymethod(magic)]
-    fn length_hint(&self, vm: &VirtualMachine) -> PyResult<isize> {
-        let pos = self.position.load();
-        let len = vm.obj_len(&self.obj)?;
-        Ok(len as isize - pos)
+    fn length_hint(&self, vm: &VirtualMachine) -> PyObjectRef {
+        match self.status.load() {
+            IterStatus::Active => {
+                let pos = self.position.load();
+                // return NotImplemented if no length is around.
+                vm.obj_len(&self.obj)
+                    .map_or(vm.ctx.not_implemented(), |len| {
+                        PyInt::from(len.saturating_sub(pos)).into_object(vm)
+                    })
+            }
+            IterStatus::Exhausted => PyInt::from(0).into_object(vm),
+        }
+    }
+
+    #[pymethod(magic)]
+    fn reduce(&self, vm: &VirtualMachine) -> PyResult {
+        let iter = vm.get_attribute(vm.builtins.clone(), "iter")?;
+        Ok(match self.status.load() {
+            IterStatus::Exhausted => vm
+                .ctx
+                .new_tuple(vec![iter, vm.ctx.new_tuple(vec![vm.ctx.new_list(vec![])])]),
+            IterStatus::Active => vm.ctx.new_tuple(vec![
+                iter,
+                vm.ctx.new_tuple(vec![self.obj.clone()]),
+                vm.ctx.new_int(self.position.load()),
+            ]),
+        })
+    }
+
+    #[pymethod(magic)]
+    fn setstate(&self, state: PyObjectRef, vm: &VirtualMachine) -> PyResult<()> {
+        // When we're exhausted, just return.
+        if let IterStatus::Exhausted = self.status.load() {
+            return Ok(());
+        }
+        if let Some(i) = state.payload::<PyInt>() {
+            self.position
+                .store(int::try_to_primitive(i.as_bigint(), vm).unwrap_or(0));
+            Ok(())
+        } else {
+            Err(vm.new_type_error("an integer is required.".to_owned()))
+        }
     }
 }
 
 impl PyIter for PySequenceIterator {
     fn next(zelf: &PyRef<Self>, vm: &VirtualMachine) -> PyResult {
+        if let IterStatus::Exhausted = zelf.status.load() {
+            return Err(vm.new_stop_iteration());
+        }
         let pos = zelf.position.fetch_add(1);
         match zelf.obj.get_item(pos, vm) {
             Err(ref e) if e.isinstance(&vm.ctx.exceptions.index_error) => {
+                zelf.status.store(IterStatus::Exhausted);
                 Err(vm.new_stop_iteration())
             }
             // also catches stop_iteration => stop_iteration
