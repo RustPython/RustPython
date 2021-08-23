f4e863927de6351f192d4bc0d4b7fd7efe48c862
Jeong YunWon <jeong@youknowone.org>
2021-08-24 02:52:33 +0900

    reduce too deep indent depth

--- vm/src/builtins/float.rs
+++ vm/src/builtins/float.rs
@@ -171,9 +171,7 @@ impl PyFloat {
             OptionalArg::Present(val) => {
                 let val = if cls.is(&vm.ctx.types.float_type) {
                     match val.downcast_exact::<PyFloat>(vm) {
-                        Ok(f) => {
-                            return Ok(f);
-                        }
+                        Ok(f) => return Ok(f),
                         Err(val) => val,
                     }
                 } else {
