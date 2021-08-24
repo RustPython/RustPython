65fba6393b7d4004bb66e21fc7a7b2884870a271
ChJR <chjr8815@gmail.com>
2021-08-25 02:55:38 +0900

    Enable matched test

--- Lib/test/test_exception_hierarchy.py
+++ Lib/test/test_exception_hierarchy.py
@@ -81,8 +81,6 @@ class HierarchyTest(unittest.TestCase):
         return _map
     _map = _make_map(_pep_map)
 
-    # TODO: RUSTPYTHON
-    @unittest.expectedFailure
     def test_errno_mapping(self):
         # The OSError constructor maps errnos to subclasses
         # A sample test for the basic functionality
