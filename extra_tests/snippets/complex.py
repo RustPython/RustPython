707ad67c4fb63af76eefab92e3fcd0081d83664f
snowapril <sinjihng@gmail.com>
2021-08-29 09:02:49 +0900

    add skip test__complex__ before cpython 3.11
    Signed-off-by: snowapril <sinjihng@gmail.com>

--- extra_tests/snippets/complex.py
+++ extra_tests/snippets/complex.py
@@ -1,10 +1,15 @@
-# __complex__
-z = 3 + 4j
-assert z.__complex__() == z
-assert type(z.__complex__()) == complex
+import testutils
 
-class complex_subclass(complex):
-    pass
-z = complex_subclass(3 + 4j)
-assert z.__complex__() == 3 + 4j
-assert type(z.__complex__()) == complex
+# __complex__
+def test__complex__():
+    z = 3 + 4j
+    assert z.__complex__() == z
+    assert type(z.__complex__()) == complex
+    
+    class complex_subclass(complex):
+        pass
+    z = complex_subclass(3 + 4j)
+    assert z.__complex__() == 3 + 4j
+    assert type(z.__complex__()) == complex
+
+testutils.skip_if_unsupported(3,11,test__complex__)
