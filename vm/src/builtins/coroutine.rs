799fa690a477a01681de3f0c23a6f1781e977627
Athul Muralidhar <athul.muralidhar@bricr.com>
2021-09-26 16:09:19 +0200

    ADD: add doc strings to voroutine builtin

--- vm/src/builtins/coroutine.rs
+++ vm/src/builtins/coroutine.rs
@@ -34,6 +34,16 @@ impl PyCoroutine {
         }
     }
 
+    /// "str(object='') -> str
+    /// str(bytes_or_buffer[, encoding[, errors]]) -> str
+    /// 
+    /// Create a new string object from the given object. If encoding or
+    /// errors is specified, then the object must expose a data buffer
+    /// that will be decoded using the given encoding and error handler.
+    /// Otherwise, returns the result of object.__str__() (if defined)
+    /// or repr(object).
+    /// encoding defaults to sys.getdefaultencoding().
+    /// errors defaults to 'strict'."
     #[pyproperty(magic)]
     fn name(&self) -> PyStrRef {
         self.inner.name()
@@ -44,16 +54,21 @@ impl PyCoroutine {
         self.inner.set_name(name)
     }
 
+    /// 'Return repr(self).'
     #[pymethod(magic)]
     fn repr(zelf: PyRef<Self>) -> String {
         zelf.inner.repr(zelf.get_id())
     }
 
+    /// "send(arg) -> send 'arg' into generator,
+    /// return next yielded value or raise StopIteration."
     #[pymethod]
     fn send(&self, value: PyObjectRef, vm: &VirtualMachine) -> PyResult {
         self.inner.send(value, vm)
     }
 
+    /// 'throw(typ[,val[,tb]]) -> raise exception in generator,
+    /// return next yielded value or raise StopIteration.'
     #[pymethod]
     fn throw(
         &self,
@@ -70,6 +85,7 @@ impl PyCoroutine {
         )
     }
 
+    /// 'close() -> raise GeneratorExit inside generator.'
     #[pymethod]
     fn close(&self, vm: &VirtualMachine) -> PyResult<()> {
         self.inner.close(vm)
@@ -129,6 +145,8 @@ impl PyCoroutineWrapper {
         self.coro.send(val, vm)
     }
 
+    /// 'throw(typ[,val[,tb]]) -> raise exception in generator,
+    /// return next yielded value or raise StopIteration.'
     #[pymethod]
     fn throw(
         &self,
