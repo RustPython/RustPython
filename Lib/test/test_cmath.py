c2ea5e271041f590214290d376748f5455d5a6b7
rishit khandelwal <rishitkh@gmail.com>
2021-10-02 21:57:40 +0530

    Add acos and asin in test_functions

--- Lib/test/test_cmath.py
+++ Lib/test/test_cmath.py
@@ -56,8 +56,9 @@ class CMathTests(unittest.TestCase):
     #
     # list of all functions in cmath
     test_functions = [getattr(cmath, fname) for fname in [
-        'sin','cos','log','log10','sqrt','acosh','tan','tanh','asinh', 'atan', 'atanh'
-        # 'exp','acos','asin',
+        'sin','cos','log','log10','sqrt','acosh','tan','tanh','asinh', 'atan', 'atanh',
+        'acos', 'asin'
+        # 'exp'
         # 'sinh','cosh'
         ]]
     # test first and second arguments independently for 2-argument log
