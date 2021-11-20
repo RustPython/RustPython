a41d7c5d29755edbea206e0bb520fda3c70bf3f5
Moreal <dev.moreal@gmail.com>
2021-11-21 01:19:10 +0900

    Remove staled comments

--- extra_tests/not_impl_gen.py
+++ extra_tests/not_impl_gen.py
@@ -72,8 +72,6 @@ def attr_is_not_inherited(type_, attr):
 
 
 def extra_info(obj):
-    # RustPython doesn't support __text_signature__ for getting signatures of builtins
-    # https://github.com/RustPython/RustPython/issues/2410
     if callable(obj) and not inspect._signature_is_builtin(obj):
         try:
             sig = str(inspect.signature(obj))
