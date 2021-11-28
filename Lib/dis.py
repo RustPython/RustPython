acc75de4efb12828f489d191febce56c918f8430
Taihsiang Ho (tai271828) <e@tai271828.me>
2021-11-28 11:07:58 +0100

    feat(Lib): print the disassembled info
    Issue: #2889

--- Lib/dis.py
+++ Lib/dis.py
@@ -1,1 +1,18 @@
 from _dis import *
+
+
+# Disassembling a file by following cpython Lib/dis.py
+def _test():
+    """Simple test program to disassemble a file."""
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', type=argparse.FileType('rb'), nargs='?', default='-')
+    args = parser.parse_args()
+    with args.infile as infile:
+        source = infile.read()
+    code = compile(source, args.infile.name, "exec")
+    dis(code)
+
+if __name__ == "__main__":
+    _test()
