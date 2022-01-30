6c956b5270dc866c4c11b57df39ac64945c57ada
Padraic Fanning <fanninpm@miamioh.edu>
2022-01-30 17:49:58 -0500

    Mark erroring/failing tests

--- Lib/test/test_pickletools.py
+++ Lib/test/test_pickletools.py
@@ -7,6 +7,91 @@ import unittest
 
 class OptimizedPickleTests(AbstractPickleTests, unittest.TestCase):
 
+    # TODO: RUSTPYTHON, AttributeError: module 'pickle' has no attribute 'PickleBuffer'
+    @unittest.expectedFailure
+    def test_buffer_callback_error(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_buffer_callback_error()
+
+    # TODO: RUSTPYTHON, AttributeError: module 'pickle' has no attribute 'PickleBuffer'
+    @unittest.expectedFailure
+    def test_buffers_error(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_buffers_error()
+
+    # TODO: RUSTPYTHON, TypeError: cannot pickle 'method' object
+    @unittest.expectedFailure
+    def test_c_methods(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_c_methods()
+
+    # TODO: RUSTPYTHON, TypeError: cannot pickle 'map' object
+    @unittest.expectedFailure
+    def test_compat_pickle(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_compat_pickle()
+
+    # TODO: RUSTPYTHON
+    @unittest.expectedFailure
+    def test_complex_newobj_ex(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_complex_newobj_ex()
+
+    # TODO: RUSTPYTHON, TypeError: cannot pickle 'method' object
+    @unittest.expectedFailure
+    def test_in_band_buffers(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_in_band_buffers()
+
+    # TODO: RUSTPYTHON, pickle.PicklingError
+    @unittest.expectedFailure
+    def test_nested_names(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_nested_names()
+
+    # TODO: RUSTPYTHON
+    @unittest.expectedFailure
+    def test_newobj_generic(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_newobj_generic()
+
+    # TODO: RUSTPYTHON, TypeError: cannot pickle 'weakproxy' object
+    @unittest.expectedFailure
+    def test_newobj_proxies(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_newobj_proxies()
+
+    # TODO: RUSTPYTHON, TypeError: cannot pickle 'NotImplementedType' object
+    @unittest.expectedFailure
+    def test_notimplemented(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_notimplemented()
+
+    # TODO: RUSTPYTHON, AttributeError: module 'pickle' has no attribute 'PickleBuffer'
+    @unittest.expectedFailure
+    def test_oob_buffers(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_oob_buffers()
+
+    # TODO: RUSTPYTHON, AttributeError: module 'pickle' has no attribute 'PickleBuffer'
+    @unittest.expectedFailure
+    def test_oob_buffers_writable_to_readonly(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_oob_buffers_writable_to_readonly()
+
+    # TODO: RUSTPYTHON, TypeError: Expected type 'bytes', not 'bytearray'
+    @unittest.expectedFailure
+    def test_optional_frames(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_optional_frames()
+
+    # TODO: RUSTPYTHON, AttributeError: module 'pickle' has no attribute 'PickleBuffer'
+    @unittest.expectedFailure
+    def test_picklebuffer_error(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_picklebuffer_error()
+
+    # TODO: RUSTPYTHON, pickle.PicklingError
+    @unittest.expectedFailure
+    def test_py_methods(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_py_methods()
+
+    # TODO: RUSTPYTHON, AttributeError: attribute '__qualname__' of 'type' objects is not writable
+    @unittest.expectedFailure
+    def test_recursive_nested_names(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_recursive_nested_names()
+
+    # TODO: RUSTPYTHON, TypeError: cannot pickle 'NotImplementedType' object
+    @unittest.expectedFailure
+    def test_singleton_types(self): # TODO: RUSTPYTHON, remove when this passes
+        super().test_singleton_types()
+
     def dumps(self, arg, proto=None, **kwargs):
         return pickletools.optimize(pickle.dumps(arg, proto, **kwargs))
 
@@ -19,6 +104,8 @@ class OptimizedPickleTests(AbstractPickleTests, unittest.TestCase):
     # Test relies on writing by chunks into a file object.
     test_framed_write_sizes_with_delayed_writer = None
 
+    # TODO: RUSTPYTHON
+    @unittest.expectedFailure
     def test_optimize_long_binget(self):
         data = [str(i) for i in range(257)]
         data.append(data[-1])
