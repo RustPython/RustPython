4f4e87729b61da591bcf9e7666e0e7cb54d90bb0
Jeong Yunwon <jeong@youknowone.org>
2022-04-17 02:18:42 +0900

    Fix ast/asdl_rs.py

--- scripts/update_asdl.sh
+++ scripts/update_asdl.sh
@@ -4,5 +4,4 @@ set -e
 cd "$(dirname "$(dirname "$0")")"
 
 python ast/asdl_rs.py -D ast/src/ast_gen.rs -M vm/src/stdlib/ast/gen.rs ast/Python.asdl
-
-cargo fmt
+rustfmt ast/src/ast_gen.rs vm/src/stdlib/ast/gen.rs
