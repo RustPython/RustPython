a07fcd2b44fdb62c70418adef9b6612a7268e6ce
Jeong Yunwon <jeong@youknowone.org>
2022-04-18 05:13:23 +0900

    split convert module into sub modules

---
+++ vm/src/convert/mod.rs
@@ -0,0 +1,5 @@
+mod transmute_from;
+mod try_from;
+
+pub use transmute_from::TransmuteFromObject;
+pub use try_from::{TryFromBorrowedObject, TryFromObject};
---
+++ vm/src/convert/transmute_from.rs
@@ -0,0 +1,29 @@
+use crate::{
+    pyobject::{AsPyObject, PyObject, PyRef, PyResult, PyValue},
+    vm::VirtualMachine,
+};
+
+/// Marks a type that has the exact same layout as PyObjectRef, e.g. a type that is
+/// `repr(transparent)` over PyObjectRef.
+///
+/// # Safety
+/// Can only be implemented for types that are `repr(transparent)` over a PyObjectRef `obj`,
+/// and logically valid so long as `check(vm, obj)` returns `Ok(())`
+pub unsafe trait TransmuteFromObject: Sized {
+    fn check(vm: &VirtualMachine, obj: &PyObject) -> PyResult<()>;
+}
+
+unsafe impl<T: PyValue> TransmuteFromObject for PyRef<T> {
+    fn check(vm: &VirtualMachine, obj: &PyObject) -> PyResult<()> {
+        let class = T::class(vm);
+        if obj.fast_isinstance(class) {
+            if obj.payload_is::<T>() {
+                Ok(())
+            } else {
+                Err(vm.new_downcast_runtime_error(class, obj))
+            }
+        } else {
+            Err(vm.new_downcast_type_error(class, obj))
+        }
+    }
+}
