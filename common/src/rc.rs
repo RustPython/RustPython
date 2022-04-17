82eb352e9fc2167ded374247857d2f3e47b5ea7d
Jeong Yunwon <jeong@youknowone.org>
2022-04-18 03:21:58 +0900

    common::rc::PyWeak is not used and confusing by its name

--- common/src/rc.rs
+++ common/src/rc.rs
@@ -1,7 +1,7 @@
 #[cfg(not(feature = "threading"))]
-use std::rc::{Rc, Weak};
+use std::rc::Rc;
 #[cfg(feature = "threading")]
-use std::sync::{Arc, Weak};
+use std::sync::Arc;
 
 // type aliases instead of newtypes because you can't do `fn method(self: PyRc<Self>)` with a
 // newtype; requires the arbitrary_self_types unstable feature
@@ -10,5 +10,3 @@ use std::sync::{Arc, Weak};
 pub type PyRc<T> = Arc<T>;
 #[cfg(not(feature = "threading"))]
 pub type PyRc<T> = Rc<T>;
-
-pub type PyWeak<T> = Weak<T>;
