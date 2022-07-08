cb724200c1d2701b519e1ca2ba87be65a3188095
Gyubong Lee <jopemachine@naver.com>
2022-07-08 14:53:49 +0900

    Fix #3841

--- vm/src/builtins/str.rs
+++ vm/src/builtins/str.rs
@@ -928,9 +928,13 @@ impl PyStr {
     }
 
     #[pymethod]
-    fn join(&self, iterable: ArgIterable<PyStrRef>, vm: &VirtualMachine) -> PyResult<String> {
-        let iter = iterable.iter(vm)?;
-        self.as_str().py_join(iter)
+    fn join(&self, iterable: ArgIterable<PyStrRef>, vm: &VirtualMachine) -> PyResult<PyStrRef> {
+        let mut iter = iterable.iter(vm)?;
+
+        match iter.size_hint().0 {
+          1 => Ok(iter.next().unwrap().unwrap()),
+          _ => Ok(vm.ctx.new_str(self.as_str().py_join(iter).unwrap()))
+        }
     }
 
     // FIXME: two traversals of str is expensive
