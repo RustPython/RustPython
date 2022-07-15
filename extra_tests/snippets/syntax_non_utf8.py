d414fbc233e941c7941f56950b602b7f7e3de7dd
dennis zhuang <killme2008@gmail.com>
2022-07-15 10:40:32 +0800

    Update extra_tests/snippets/syntax_non_utf8.py
    Co-authored-by: fanninpm <fanninpm@miamioh.edu>

--- extra_tests/snippets/syntax_non_utf8.py
+++ extra_tests/snippets/syntax_non_utf8.py
@@ -1,11 +1,7 @@
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-try:
+# TODO: RUSTPYTHON, RustPython raises a SyntaxError here
+with assert_raises(ValueError):
     with open(os.path.join(dir_path , "non_utf8.txt")) as f:
         eval(f.read())
-except:
-    # TODO: RUSTPYTHON, rustpython raise SyntaxError but cpython raise ValueError here.
-    pass
-else:
-  assert False
