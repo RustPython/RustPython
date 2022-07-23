93cc5de41adca20e7cc4e329b42ddf2f231e4060
JungInRhee <ysrheee@kaist.ac.kr>
2022-07-23 20:50:01 +0900

    Fix: handle edge case for complex comparison, when both is both NaN+NaNj

--- vm/src/builtins/complex.rs
+++ vm/src/builtins/complex.rs
@@ -400,7 +400,15 @@ impl Comparable for PyComplex {
     ) -> PyResult<PyComparisonValue> {
         op.eq_only(|| {
             let result = if let Some(other) = other.payload_if_subclass::<PyComplex>(vm) {
-                zelf.value == other.value
+                if zelf.value.re.is_nan()
+                    && zelf.value.im.is_nan()
+                    && other.value.re.is_nan()
+                    && other.value.im.is_nan()
+                {
+                    true
+                } else {
+                    zelf.value == other.value
+                }
             } else {
                 match float::to_op_float(other, vm) {
                     Ok(Some(other)) => zelf.value == other.into(),
