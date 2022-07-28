4f67273475e75d7e206fc8d796ef627acfead0f7
Hyerim Choi <hyerimchoi@Hyerimui-MacBookPro.local>
2022-07-29 02:16:07 +0900

    Add: not(target_arch = wasm32),)

--- stdlib/src/lib.rs
+++ stdlib/src/lib.rs
@@ -51,7 +51,7 @@ mod select;
 mod ssl;
 #[cfg(all(unix, not(target_os = "redox")))]
 mod termios;
-#[cfg(any(target_os = "linux", target_os = "macos"))]
+#[cfg(any(target_os = "linux", target_os = "macos", not(target_arch = "wasm32"),))]
 mod uuid;
 
 use rustpython_common as common;
@@ -135,7 +135,7 @@ pub fn get_module_inits() -> impl Iterator<Item = (Cow<'static, str>, StdlibInit
         {
             "_scproxy" => scproxy::make_module,
         }
-        #[cfg(any(target_os = "linux", target_os = "macos"))]
+        #[cfg(any(target_os = "linux", target_os = "macos", not(target_arch = "wasm32"),))]
         {
             "_uuid" => uuid::make_module,
         }
