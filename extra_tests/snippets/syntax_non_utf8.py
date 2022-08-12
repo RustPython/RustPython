7e88c8ff55017f0c11b807d14e97398a650aaad5
Moreal <dev.moreal@gmail.com>
2022-08-13 07:30:32 +0900

    Inline error type

--- extra_tests/snippets/syntax_non_utf8.py
+++ extra_tests/snippets/syntax_non_utf8.py
@@ -5,7 +5,6 @@ from testutils import assert_raises
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-error = ValueError
-with assert_raises(error):
+with assert_raises(ValueError):
     with open(os.path.join(dir_path , "non_utf8.txt")) as f:
         eval(f.read())
