185e0cb62d6ac40511fba34a80049119953871e1
Jeong YunWon <jeong@youknowone.org>
2022-08-18 04:41:32 +0900

    clippy is platform-dependent

--- stdlib/src/ssl.rs
+++ stdlib/src/ssl.rs
@@ -1445,7 +1445,7 @@ mod windows {
             .iter()
             .filter_map(|open| open(store_name.as_str()).ok())
             .collect::<Vec<_>>();
-        let certs = stores.iter().map(|s| s.certs()).flatten().map(|c| {
+        let certs = stores.iter().flat_map(|s| s.certs()).map(|c| {
             let cert = vm.ctx.new_bytes(c.to_der().to_owned());
             let enc_type = unsafe {
                 let ptr = c.as_ptr() as wincrypt::PCCERT_CONTEXT;
