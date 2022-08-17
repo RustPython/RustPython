93d18bc0c0795d63834fef66937e79f6a2d04584
Jeong YunWon <jeong@youknowone.org>
2022-08-18 05:43:22 +0900

    rework pylib to generate all frozen libs

--- vm/pylib-crate/src/lib.rs
+++ vm/pylib-crate/src/lib.rs
@@ -6,12 +6,27 @@
 // so build.rs sets this env var
 pub const LIB_PATH: &str = match option_env!("win_lib_path") {
     Some(s) => s,
-    None => concat!(env!("CARGO_MANIFEST_DIR"), "/Lib"),
+    None => concat!(env!("CARGO_MANIFEST_DIR"), "/../../Lib"),
 };
 
-#[cfg(feature = "compiled-bytecode")]
 use rustpython_bytecode::FrozenModule;
-#[cfg(feature = "compiled-bytecode")]
+
+pub fn frozen_builtins() -> impl Iterator<Item = (String, FrozenModule)> {
+    rustpython_derive::py_freeze!(
+        dir = "../Lib/python_builtins",
+        crate_name = "rustpython_bytecode"
+    )
+}
+
+#[cfg(not(feature = "stdlib"))]
+pub fn frozen_core() -> impl Iterator<Item = (String, FrozenModule)> {
+    rustpython_derive::py_freeze!(
+        dir = "../Lib/core_modules",
+        crate_name = "rustpython_bytecode"
+    )
+}
+
+#[cfg(feature = "stdlib")]
 pub fn frozen_stdlib() -> impl Iterator<Item = (String, FrozenModule)> {
-    rustpython_derive::py_freeze!(dir = "Lib", crate_name = "rustpython_bytecode")
+    rustpython_derive::py_freeze!(dir = "../../Lib", crate_name = "rustpython_bytecode")
 }
