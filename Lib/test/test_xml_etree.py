3bfdddf3bcf8035463a0a33de76ac9fe7737dde9
Kangzhi Shi <shikangzhi@gmail.com>
2022-10-02 20:28:33 +0200

    mark unsuccess test_xml_etree

--- Lib/test/test_xml_etree.py
+++ Lib/test/test_xml_etree.py
@@ -2187,6 +2187,8 @@ class BugsTest(unittest.TestCase):
         self.assertEqual(t.find('.//paragraph').text,
             'A new cultivar of Begonia plant named \u2018BCT9801BEG\u2019.')
 
+    # TODO: RUSTPYTHON
+    @unittest.expectedFailure
     @unittest.skipIf(sys.gettrace(), "Skips under coverage.")
     def test_bug_xmltoolkit63(self):
         # Check reference leak.
