23c67806cda03e17abf5ec447185389dbba167c8
Chia-Hsiang Cheng <gary.chiahsiang.cheng@gmail.com>
2023-01-22 19:09:16 +0800

    Undo the last change

--- vm/src/stdlib/builtins.rs
+++ vm/src/stdlib/builtins.rs
@@ -117,8 +117,6 @@ mod builtins {
         {
             use crate::{class::PyClassImpl, stdlib::ast};
 
-            ast::make_module(vm);
-
             if args._feature_version.is_present() {
                 eprintln!("TODO: compile() got `_feature_version` but ignored");
             }
