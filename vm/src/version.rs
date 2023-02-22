8fc263f62f1daf6e8f2051cb56ff893cd74c921c
Jeong YunWon <jeong@youknowone.org>
2023-02-22 22:17:55 +0900

    Python 3.11 missed

--- vm/src/version.rs
+++ vm/src/version.rs
@@ -4,9 +4,9 @@
 use chrono::{prelude::DateTime, Local};
 use std::time::{Duration, UNIX_EPOCH};
 
-// = 3.10.0alpha
+// = 3.11.0alpha
 pub const MAJOR: usize = 3;
-pub const MINOR: usize = 10;
+pub const MINOR: usize = 11;
 pub const MICRO: usize = 0;
 pub const RELEASELEVEL: &str = "alpha";
 pub const RELEASELEVEL_N: usize = 0xA;
