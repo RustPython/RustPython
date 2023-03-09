22877202019b471e52afdc7a3236a84981b1ddd2
Jeong YunWon <jeong@youknowone.org>
2023-03-10 02:46:14 +0900

    Mark flaky test_enum::TestFlag::test_unique_composite skip

--- Lib/test/test_enum.py
+++ Lib/test/test_enum.py
@@ -2466,9 +2466,7 @@ class TestFlag(unittest.TestCase):
         self.assertEqual(Color.ALL.value, 7)
         self.assertEqual(str(Color.BLUE), 'blue')
 
-    # TODO: RUSTPYTHON
-    @unittest.expectedFailure
-    @unittest.skipIf(sys.platform == "win32", "TODO: RUSTPYTHON, inconsistent test result on Windows due to threading")
+    @unittest.skip("TODO: RUSTPYTHON, inconsistent test result on Windows due to threading")
     @threading_helper.reap_threads
     def test_unique_composite(self):
         # override __eq__ to be identity only
