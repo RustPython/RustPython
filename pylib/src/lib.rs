b928605823594c264c46c9d6c92fa7311ac44283
Rahul <84739019+basicfunc@users.noreply.github.com>
2023-03-15 22:55:58 +0530

    Update lib.rs
    Resolve the issue listed on [ Encoding and functools ModuleError #4691 ](https://github.com/RustPython/RustPython/issues/4691)

--- pylib/src/lib.rs
+++ pylib/src/lib.rs
@@ -6,9 +6,9 @@
 // so build.rs sets this env var
 pub const LIB_PATH: &str = match option_env!("win_lib_path") {
     Some(s) => s,
-    None => concat!(env!("CARGO_MANIFEST_DIR"), "/Lib"),
+    None => concat!(env!("CARGO_MANIFEST_DIR"), "../Lib"),
 };
 
 #[cfg(feature = "freeze-stdlib")]
 pub const FROZEN_STDLIB: &rustpython_compiler_core::frozen_lib::FrozenLib =
-    rustpython_derive::py_freeze!(dir = "./Lib", crate_name = "rustpython_compiler_core");
+    rustpython_derive::py_freeze!(dir = "../Lib", crate_name = "rustpython_compiler_core");
