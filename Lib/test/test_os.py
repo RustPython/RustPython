25a452efd1efaaf0f7acab0bcb94ee4a461d4cba
Dmitry Erlikh <derlih@gmail.com>
2023-03-25 17:48:03 +0100

    test_empty_path -> expectedFailureIfWindows

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -4498,8 +4498,7 @@ class TestScandir(unittest.TestCase):
                     st = os.stat(entry.name, dir_fd=fd, follow_symlinks=False)
                     self.assertEqual(entry.stat(follow_symlinks=False), st)
 
-    # TODO: RUSTPYTHON (AssertionError: FileNotFoundError not raised by scandir)
-    @unittest.expectedFailure
+    @unittest.expectedFailureIfWindows("TODO: RUSTPYTHON (AssertionError: FileNotFoundError not raised by scandir)")
     def test_empty_path(self):
         self.assertRaises(FileNotFoundError, os.scandir, '')
 
