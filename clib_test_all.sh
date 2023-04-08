8bd2bf3c16e02cef927f4010fe8b1c4119be57e9
MegasKomnenos <wogudwkd1234@naver.com>
2023-04-08 22:53:29 +0900

    Add bash script to autotest cpython lib
    To use, write down your local cpython path in clib_path.txt, and run,
    bash clib_test_one.sh
    
    and check the result in clib_out.txt
    
    The script will try to test every component in clib_list.txt, where,
    
    clib_test_one.sh : test each component individually
    clib_test_all.sh : test every components simultaneously

---
+++ clib_test_all.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+CPYTHONPATH=$(cat clib_path.txt)
+
+cp -r "Lib" "LibTmp"
+
+index=1
+
+while :
+do
+    token=$(cat clib_list.txt | cut -d ' ' -f $index)
+    if [ -z $token ]; then
+        break
+    fi
+
+    if [ -f "${CPYTHONPATH}/Lib/${token}.py" ]; then
+        cp "${CPYTHONPATH}/Lib/${token}.py" "Lib/${token}.py"
+    fi
+    
+    if [ -f "${CPYTHONPATH}/Lib/test/test_${token}.py" ]; then
+        cp "${CPYTHONPATH}/Lib/test/test_${token}.py" "Lib/test/test_${token}.py"
+    fi
+
+    ((index++))
+done
+
+cargo build -r
+
+if [ -f "clib_out.txt" ]; then
+    rm "clib_out.txt"
+fi
+
+touch "clib_out.txt"
+
+index=1
+
+while :
+do
+    token=$(cat clib_list.txt | cut -d ' ' -f $index)
+    if [ -z $token ]; then
+        break
+    fi
+
+    message="${token}:"
+
+    if [ -f "Lib/test/test_${token}.py" ]; then
+        test=$(cargo run -q -r "Lib/test/test_${token}.py" -q 2>&1 >/dev/null | grep "OK")
+
+        if [ ! -z "${test}" ] ; then
+            message="${message}  OK"
+        fi
+        if [ -z "${test}" ] ; then
+            message="${message}  FAILED"
+        fi
+    fi
+    if [ ! -f "Lib/test/test_${token}.py" ]; then
+        message="${message}  NOTEST"
+    fi
+
+    echo $message >> "clib_out.txt"
+
+    ((index++))
+done
+
+rm -r "Lib"
+mv -r "LibTmp" "Lib"
---
+++ clib_test_one.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+CPYTHONPATH=$(cat clib_path.txt)
+
+if [ -f "clib_out.txt" ]; then
+    rm "clib_out.txt"
+fi
+
+touch "clib_out.txt"
+
+index=1
+
+while :
+do
+    token=$(cat clib_list.txt | cut -d ' ' -f $index)
+    if [ -z $token ]; then
+        break
+    fi
+
+    lib_exist=false
+    test_exist=false
+    test_do=true
+    message="${token}:"
+
+    if [ -f "Lib/${token}.py" ]; then
+        lib_exist=true
+        cp "Lib/${token}.py" "Lib/${token}_tmp_cp.py"
+    fi
+    if [ -f "Lib/test/test_${token}.py" ]; then
+        test_exist=true
+        cp "Lib/test/test_${token}.py" "Lib/test/test_${token}_tmp_cp.py"
+    fi
+
+    if [ -f "${CPYTHONPATH}/Lib/${token}.py" ]; then
+        cp "${CPYTHONPATH}/Lib/${token}.py" "Lib/${token}.py"
+    fi
+    if [ ! -f "${CPYTHONPATH}/Lib/${token}.py" ]; then
+        test_do=false
+        message="${message}  No cpython/Lib/${token}.py"
+    fi
+    
+    if [ -f "${CPYTHONPATH}/Lib/test/test_${token}.py" ]; then
+        cp "${CPYTHONPATH}/Lib/test/test_${token}.py" "Lib/test/test_${token}.py"
+    fi
+    if [ ! -f "${CPYTHONPATH}/Lib/test/test_${token}.py" ]; then
+        test_do=false
+        message="${message}  No cpython/Lib/test/test_${token}.py"
+    fi
+
+    if $test_do ; then
+        test=$(cargo run -q "Lib/test/test_${token}.py" -q 2>&1 >/dev/null | grep "OK")
+
+        if [ ! -z "${test}" ] ; then
+            message="${message}  OK"
+        fi
+        if [ -z "${test}" ] ; then
+            message="${message}  FAILED"
+        fi
+    fi
+
+    if $lib_exist ; then
+        mv "Lib/${token}_tmp_cp.py" "Lib/${token}.py"
+    fi
+    if ! $lib_exist && [ -f "Lib/${token}.py" ] ; then
+        rm "Lib/${token}.py"
+    fi 
+
+     if $test_exist ; then
+        mv "Lib/test/test_${token}_tmp_cp.py" "Lib/test/test_${token}.py"
+    fi
+    if ! $test_exist && [ -f "Lib/test/test_${token}.py" ] ; then
+        rm "Lib/test/test_${token}.py"
+    fi 
+
+    echo $message >> "clib_out.txt"
+
+    ((index++))
+done
