77f26a0d0f992ce8bbca6a3a99debe60fde87b41
Ankit Kumar Pandey <itsankitkp@gmail.com>
2023-04-14 20:48:46 +0530

    chore! fix clipper error

--- vm/src/stdlib/sys.rs
+++ vm/src/stdlib/sys.rs
@@ -593,7 +593,7 @@ mod sys {
             writeln!(
                 stderr,
                 "{}",
-                e.as_object().repr(vm).unwrap_or_else(|| vm.ctx.empty_str.to_owned()).as_str()
+                e.as_object().repr(vm).unwrap_or_else(|_| vm.ctx.empty_str.to_owned()).as_str()
             );
         }
     }
