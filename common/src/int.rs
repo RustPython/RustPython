c17309b54a6902155fdd58d27337ede148af79ac
Kangzhi Shi <shikangzhi@gmail.com>
2023-04-18 21:30:57 +0200

    refactor bytes_to_int

--- common/src/int.rs
+++ common/src/int.rs
@@ -528,117 +528,86 @@ impl BigInt {
     }
 }
 
-pub fn bytes_to_int(lit: &[u8], mut base: u8) -> Option<BigInt> {
-    // split sign
+pub fn bytes_to_int(lit: &[u8], mut radix: u8) -> Option<BigInt> {
     let mut lit = lit.trim();
-    let sign = match lit.first()? {
-        b'+' => {
+
+    let mut first = *lit.first()?;
+
+    let sign = match first {
+        b'+' | b'-' => {
+            let sign = first == b'+';
             lit = &lit[1..];
-            true
-        }
-        b'-' => {
-            lit = &lit[1..];
-            false
+            first = *lit.first()?;
+            sign
         }
         _ => true,
     };
 
-    // split radix
-    let first = *lit.first()?;
-    let has_radix = if first == b'0' {
-        match base {
-            0 => {
-                if let Some(parsed) = lit.get(1).and_then(detect_base) {
-                    base = parsed;
-                    true
-                } else {
-                    if let [_first, ref others @ .., last] = lit {
-                        let is_zero =
-                            others.iter().all(|&c| c == b'0' || c == b'_') && *last == b'0';
-                        if !is_zero {
-                            return None;
-                        }
-                    }
-                    return Some(BigInt::zero());
-                }
-            }
-            16 => lit.get(1).map_or(false, |&b| matches!(b, b'x' | b'X')),
-            2 => lit.get(1).map_or(false, |&b| matches!(b, b'b' | b'B')),
-            8 => lit.get(1).map_or(false, |&b| matches!(b, b'o' | b'O')),
-            _ => false,
+    let detect = |second| -> Option<u8> {
+        if first != b'0' {
+            return None;
+        }
+        match second {
+            b'x' | b'X' => Some(16),
+            b'b' | b'B' => Some(2),
+            b'o' | b'O' => Some(8),
+            _ => None,
+        }
+    };
+
+    let detected = lit.get(1).cloned().and_then(detect);
+
+    let mut leading_zero = false;
+
+    if let Some(detected) = detected {
+        lit = &lit[2..];
+        if radix == 0 {
+            radix = detected;
+        } else if radix != detected {
+            return None;
         }
     } else {
-        if base == 0 {
-            base = 10;
+        if radix == 0 {
+            radix = 10;
+            leading_zero = first == b'0';
         }
-        false
+        // start with underscore
+        if first == b'_' {
+            return None;
+        }
+    }
+
+    let mut v: Vec<u8>;
+    lit = if lit.iter().any(|&x| x == b'_') {
+        v = Vec::with_capacity(lit.len());
+        let mut is_prev_underscore = false;
+        for &x in lit {
+            let is_underscore = x == b'_';
+            match (is_underscore, is_prev_underscore) {
+                // multiple underscores
+                (true, true) => return None,
+                (true, false) => (),
+                (false, _) => v.push(x),
+            }
+            is_prev_underscore = is_underscore;
+        }
+        // end with underscore
+        if is_prev_underscore {
+            return None;
+        }
+        v.as_slice()
+    } else {
+        lit
     };
-    if has_radix {
-        lit = &lit[2..];
-        if lit.first()? == &b'_' {
-            lit = &lit[1..];
-        }
-    }
 
-    // remove zeroes
-    let mut last = *lit.first()?;
-    if last == b'0' {
-        let mut count = 0;
-        for &cur in &lit[1..] {
-            if cur == b'_' {
-                if last == b'_' {
-                    return None;
-                }
-            } else if cur != b'0' {
-                break;
-            };
-            count += 1;
-            last = cur;
-        }
-        let prefix_last = lit[count];
-        lit = &lit[count + 1..];
-        if lit.is_empty() && prefix_last == b'_' {
-            return None;
-        }
-    }
+    let s = std::str::from_utf8(lit).ok()?;
+    let abs = Natural::from_string_base(radix, s)?;
 
-    // validate
-    for c in lit {
-        let c = *c;
-        if !(c.is_ascii_alphanumeric() || c == b'_') {
-            return None;
-        }
-
-        if c == b'_' && last == b'_' {
-            return None;
-        }
-
-        last = c;
-    }
-    if last == b'_' {
+    if abs != <Natural as malachite::num::basic::traits::Zero>::ZERO && leading_zero {
         return None;
     }
 
-    // parse
-    let number = if lit.is_empty() {
-        BigInt::zero()
-    } else {
-        let s = unsafe { std::str::from_utf8_unchecked(lit) };
-        let uint = Natural::from_string_base(base, s)?;
-        BigInt(Integer::from_sign_and_abs(sign, uint))
-    };
-    Some(number)
-}
-
-#[inline]
-pub fn detect_base(c: &u8) -> Option<u8> {
-    let base = match c {
-        b'x' | b'X' => 16,
-        b'b' | b'B' => 2,
-        b'o' | b'O' => 8,
-        _ => return None,
-    };
-    Some(base)
+    Some(Integer::from_sign_and_abs(sign, abs).into())
 }
 
 #[test]
