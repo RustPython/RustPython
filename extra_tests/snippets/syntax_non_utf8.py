7b52834e0b9bf4c0a5373bae2b2ec9cd6d6a4351
DimitrisJim <d.f.hilliard@gmail.com>
2023-06-17 14:51:42 +0300

    Return SyntaxError with null byte input in eval.

--- extra_tests/snippets/syntax_non_utf8.py
+++ extra_tests/snippets/syntax_non_utf8.py
@@ -5,6 +5,6 @@ from testutils import assert_raises
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-with assert_raises(ValueError):
+with assert_raises(SyntaxError):
     with open(os.path.join(dir_path , "non_utf8.txt")) as f:
         eval(f.read())
