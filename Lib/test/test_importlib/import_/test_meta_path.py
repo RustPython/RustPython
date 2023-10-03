69bd951dbdea7a4550253fd59aa247add058849c
CPython developers <>
2023-10-03 23:51:58 +0900

    Update test_importlib from Python3.12

--- Lib/test/test_importlib/import_/test_meta_path.py
+++ Lib/test/test_importlib/import_/test_meta_path.py
@@ -115,16 +115,6 @@ class CallSignoreSuppressImportWarning(CallSignature):
             super().test_no_path()
 
 
-class CallSignaturePEP302(CallSignoreSuppressImportWarning):
-    mock_modules = util.mock_modules
-    finder_name = 'find_module'
-
-
-(Frozen_CallSignaturePEP302,
- Source_CallSignaturePEP302
- ) = util.test_both(CallSignaturePEP302, __import__=util.__import__)
-
-
 class CallSignaturePEP451(CallSignature):
     mock_modules = util.mock_spec
     finder_name = 'find_spec'
