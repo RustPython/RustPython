01043f33b6d881663dad3f1f5a37cd7b49873f1c
Ashwin Naren <arihant2math@gmail.com>
2025-01-15 20:58:18 -0800

    fix test

--- Lib/test/test_logging.py
+++ Lib/test/test_logging.py
@@ -2170,8 +2170,6 @@ class HTTPHandlerTest(BaseTest):
         request.end_headers()
         self.handled.set()
 
-    # TODO: RUSTPYTHON
-    @unittest.expectedFailure
     def test_output(self):
         # The log message sent to the HTTPHandler is properly received.
         logger = logging.getLogger("http")
