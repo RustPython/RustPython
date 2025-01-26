6e91787eb7ffa25740aea4dcfa53e092cf8eea8c
Lee Dogeon <dev.moreal@gmail.com>
2025-01-26 21:20:57 +0900

    Complement _typing module

--- vm/src/stdlib/typing.rs
+++ vm/src/stdlib/typing.rs
@@ -1,4 +1,16 @@
-pub(crate) use _typing::make_module;
+use crate::{
+    builtins::{PyModule, PyTypeRef},
+    types::{Constructor, Representable},
+    PyRef, PyResult, VirtualMachine,
+};
+
+pub(crate) fn make_module(vm: &VirtualMachine) -> PyRef<PyModule> {
+    let module = _typing::make_module(vm);
+    module
+        .set_attr("NoDefault", vm.ctx.no_default.clone(), vm)
+        .unwrap();
+    module
+}
 
 #[pymodule]
 pub(crate) mod _typing {
@@ -184,4 +196,31 @@ pub(crate) mod _typing {
     //         &AS_MAPPING
     //     }
     // }
+}
+
+#[pyclass(module = false, name)]
+#[derive(Debug, PyPayload)]
+#[allow(dead_code)]
+pub struct NoDefaultType {}
+
+#[pyclass(with(Constructor, Representable))]
+impl NoDefaultType {
+    #[pymethod(magic)]
+    fn reduce() -> String {
+        "NoDefault".to_string()
+    }
+}
+
+impl Constructor for NoDefaultType {
+    type Args = ();
+
+    fn py_new(_: PyTypeRef, _args: Self::Args, vm: &VirtualMachine) -> PyResult {
+        Ok(vm.ctx.no_default.clone().into())
+    }
+}
+
+impl Representable for NoDefaultType {
+    fn repr_str(_zelf: &crate::Py<Self>, _vm: &VirtualMachine) -> crate::PyResult<String> {
+        Ok("typing.NoDefault".to_owned())
+    }
 }
