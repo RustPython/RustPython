edc3570c4218cf8c157d48080297bb4044f7dcc3
Ashwin Naren <arihant2math@gmail.com>
2025-03-05 12:06:38 -0800

    fix tk setup and add demo

--- stdlib/src/tkinter.rs
+++ stdlib/src/tkinter.rs
@@ -4,6 +4,32 @@ pub(crate) use self::_tkinter::make_module;
 mod _tkinter {
     use crate::vm::VirtualMachine;
     use crate::builtins::PyTypeRef;
+    use tk::*;
+    use tk::cmd::*;
+
+    #[pyattr]
+    const TK_VERSION: &str = "8.6.12";
+    #[pyattr]
+    const TCL_VERSION: &str = "8.6";
+
+    fn demo() -> tk::TkResult<()> {
+        let tk = make_tk!()?;
+        let root = tk.root();
+        root.add_label( -text("constructs widgets and layout step by step") )?
+            .pack(())?;
+        let f = root
+            .add_frame(())?
+            .pack(())?;
+        let _btn = f
+            .add_button( "btn" -text("quit") -command("destroy .") )?
+            .pack(())?;
+        Ok(main_loop())
+    }
+
+    #[pyfunction]
+    fn tk_demo() {
+        let _ = demo();
+    }
 
     #[pyattr(once, name = "TclError")]
     fn tcl_error(vm: &VirtualMachine) -> PyTypeRef {
