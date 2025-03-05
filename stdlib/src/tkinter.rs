768c207c6ad17321bd36a4ddeafa2fb0ca5fd5f0
Ashwin Naren <arihant2math@gmail.com>
2025-03-05 11:34:32 -0800

    add tcl_error

--- stdlib/src/tkinter.rs
+++ stdlib/src/tkinter.rs
@@ -2,5 +2,15 @@ pub(crate) use self::_tkinter::make_module;
 
 #[pymodule]
 mod _tkinter {
+    use crate::vm::VirtualMachine;
+    use crate::builtins::PyTypeRef;
 
+    #[pyattr(once, name = "TclError")]
+    fn tcl_error(vm: &VirtualMachine) -> PyTypeRef {
+        vm.ctx.new_exception_type(
+            "zlib",
+            "TclError",
+            Some(vec![vm.ctx.exceptions.exception_type.to_owned()]),
+        )
+    }
 }
