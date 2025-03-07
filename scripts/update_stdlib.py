3791427d3abe1f53ef78c727297c7a6395c2a669
Ashwin Naren <arihant2math@gmail.com>
2025-03-06 20:43:03 -0800

    cleanup

--- scripts/update_stdlib.py
+++ scripts/update_stdlib.py
@@ -1,4 +1,5 @@
 import argparse
+import filecmp
 import os
 import platform
 import shutil
@@ -6,6 +7,17 @@ import subprocess
 import sys
 from pathlib import Path
 import time
+
+def backup_file(file: Path):
+    shutil.copy(file, file.with_suffix(".temp"))
+
+def restore_file(file: Path):
+    shutil.copy(file.with_suffix(".temp"), file)
+
+def delete_backup(file: Path):
+    backup = file.with_suffix(".temp")
+    if backup.exists():
+        os.remove(backup)
 
 def check_cpython_path(pwd):
     pwd = Path(pwd)
@@ -76,12 +88,48 @@ for count, (lib_file, test_file) in enumerate(cpy_lib_test_paris):
     if test_file.name not in skips:
         print(f"[{count + 1}/{len(cpy_lib_test_paris)}] Upgrading {lib_file} and {test_file}")
         if not args.dry_run:
+            if filecmp.cmp(lib_file, cpy / "Lib" / lib_file.name) and filecmp.cmp(test_file, cpy / "Lib/test" / test_file.name):
+                print(f"Skipping {lib_file} and {test_file} because they are identical")
+            else:
+                # Copy current files to a backup location
+                backup_file(lib_file)
+                backup_file(test_file)
+                time.sleep(0.1)
+                # Copy the files
+                shutil.copy(lib_file, "Lib/")
+                shutil.copy(test_file, "Lib/test/")
+                time.sleep(0.1)
+                run = run_base.copy() + [test_file.name.replace(".py", "")]
+                if args.careful:
+                    run = careful_run
+                # Run the tests, don't fail, but print the output if verbose and revert if failed
+                try:
+                    subprocess.run(run, check=True)
+                except subprocess.CalledProcessError as e:
+                    time.sleep(1)
+                    print(f"Test failed, reverting changes to {lib_file} and {test_file}")
+                    restore_file(lib_file)
+                    restore_file(test_file)
+                    time.sleep(0.1)
+                    if args.verbose:
+                        print(e)           
+                delete_backup(lib_file)
+                delete_backup(test_file)     
+    else:
+        print(f"Skipping {test_file}")
+        continue
+
+print("Attempting upgrade of non-pairs")
+for count, test_file in enumerate(non_pairs):
+    print(f"[{count + 1}/{len(non_pairs)}] Upgrading {test_file}")
+    if not args.dry_run:
+        if filecmp.cmp(test_file, cpy / "Lib/test" / test_file.name):
+            print(f"Skipping {test_file} because they are identical")
+        else:
             # Copy current files to a backup location
-            shutil.copy(lib_file, lib_file.with_suffix(".temp"))
-            shutil.copy(test_file, test_file.with_suffix(".temp"))
+            backup_file(test_file)
             time.sleep(0.1)
             # Copy the files
-            shutil.copy(lib_file, "Lib/")
             shutil.copy(test_file, "Lib/test/")
             time.sleep(0.1)
             run = run_base.copy() + [test_file.name.replace(".py", "")]
@@ -91,37 +139,10 @@ for count, (lib_file, test_file) in enumerate(cpy_lib_test_paris):
             try:
                 subprocess.run(run, check=True)
             except subprocess.CalledProcessError as e:
-                time.sleep(1)
-                print(f"Test failed, reverting changes to {lib_file} and {test_file}")
-                shutil.copy(lib_file.with_suffix(".temp"), lib_file)
-                shutil.copy(test_file.with_suffix(".temp"), test_file)
+                time.sleep(0.1)
+                print(f"Test failed, reverting changes to {test_file}")
+                restore_file(test_file)
                 time.sleep(0.1)
                 if args.verbose:
-                    print(e)                
-    else:
-        print(f"Skipping {test_file}")
-        continue
-
-print("Attempting upgrade of non-pairs")
-for count, test_file in enumerate(non_pairs):
-    print(f"[{count + 1}/{len(non_pairs)}] Upgrading {test_file}")
-    if not args.dry_run:
-        # Copy current files to a backup location
-        shutil.copy(test_file, test_file.with_suffix(".temp"))
-        time.sleep(0.1)
-        # Copy the files
-        shutil.copy(test_file, "Lib/test/")
-        time.sleep(0.1)
-        run = run_base.copy() + [test_file.name.replace(".py", "")]
-        if args.careful:
-            run = careful_run
-        # Run the tests, don't fail, but print the output if verbose and revert if failed
-        try:
-            subprocess.run(run, check=True)
-        except subprocess.CalledProcessError as e:
-            time.sleep(0.1)
-            print(f"Test failed, reverting changes to {test_file}")
-            shutil.copy(test_file.with_suffix(".temp"), test_file)
-            time.sleep(0.1)
-            if args.verbose:
-                print(e) 
+                    print(e) 
+            delete_backup(test_file)
