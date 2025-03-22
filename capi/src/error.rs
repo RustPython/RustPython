ec84c3d38c9841acad187d27a51fefbce1564b3b
Ashwin Naren <arihant2math@gmail.com>
2025-03-21 20:10:28 -0700

    fix PyErr_NewException

--- capi/src/error.rs
+++ capi/src/error.rs
@@ -1,6 +1,6 @@
-use std::ffi;
+use std::{ffi, ptr};
 
-use rustpython_vm::PyObjectRef;
+use rustpython_vm::{PyObject, PyObjectRef};
 
 #[unsafe(export_name = "PyErr_Clear")]
 pub unsafe extern "C" fn err_clear() {
@@ -12,11 +12,17 @@ pub unsafe extern "C" fn err_new_exception(
     name: *const ffi::c_char,
     _base: *mut ffi::c_void,
     _dict: *mut ffi::c_void,
-) -> PyObjectRef {
+) -> *mut PyObject {
     let vm = crate::get_vm();
     let name_str = unsafe { std::ffi::CStr::from_ptr(name).to_str().unwrap() };
-    let name_split = name_str.split('.');
-    let module = name_split.clone().next().unwrap();
-    let name = name_split.last().unwrap();
-    vm.ctx.new_exception_type(module, name, Some(vec![vm.ctx.exceptions.exception_type.to_owned()])).into()
+    let last_dot = match name_str.rfind('.') {
+        Some(x) => x,
+        None => {
+            // TODO: Set error
+            return ptr::null_mut();
+        },
+    };
+    let module = &name_str[..last_dot];
+    let name = &name_str[last_dot + 1..];
+    Into::<PyObjectRef>::into(vm.ctx.new_exception_type(module, name, Some(vec![vm.ctx.exceptions.exception_type.to_owned()]))).into_raw().as_ptr()
 }
