2230d6c751eb8fdd2d7554df529574ea53b906b8
Hanif Ariffin <hanif.ariffin.4326@gmail.com>
2025-04-05 14:33:33 +0800

    Fix not throwing the same error as CPython in test_pathlib.test_expanduser (#5578)
    * Fix not throwing the same error as CPython when trying to expanduser of a non-existent user
    
    Signed-off-by: Hanif Ariffin <hanif.ariffin.4326@gmail.com>
    
    * add pwd test
    
    * Skip pwd test on windows
    
    ---------
    
    Signed-off-by: Hanif Ariffin <hanif.ariffin.4326@gmail.com>
    Co-authored-by: Jeong YunWon <jeong@youknowone.org>
    Co-authored-by: Jeong, YunWon <69878+youknowone@users.noreply.github.com>

---
+++ extra_tests/snippets/stdlib_pwd.py
@@ -0,0 +1,12 @@
+import sys
+# windows doesn't support pwd
+if sys.platform.startswith("win"):
+    exit(0)
+
+from testutils import assert_raises
+import pwd
+
+with assert_raises(KeyError):
+    fake_name = 'fake_user'
+    while pwd.getpwnam(fake_name):
+        fake_name += '1'
