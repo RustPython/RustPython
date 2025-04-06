2d39c491542df7da95a78419cabedacd3f9d02a7
Ashwin Naren <arihant2math@gmail.com>
2025-04-05 17:24:22 -0700

    formatting

--- stdlib/src/suggestions.rs
+++ stdlib/src/suggestions.rs
@@ -12,10 +12,7 @@ mod _suggestions {
         name: PyObjectRef,
         vm: &VirtualMachine,
     ) -> PyObjectRef {
-        match crate::vm::suggestion::calculate_suggestions(
-            canidates.iter(),
-            &name
-        ) {
+        match crate::vm::suggestion::calculate_suggestions(canidates.iter(), &name) {
             Some(suggestion) => suggestion.into(),
             None => vm.ctx.none(),
         }
