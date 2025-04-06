f9ddfdee6b2b5b0a1f3aff6a9c91f43d00f67a59
Ashwin Naren <arihant2math@gmail.com>
2025-04-05 19:54:02 -0700

    fix typo

--- stdlib/src/suggestions.rs
+++ stdlib/src/suggestions.rs
@@ -8,11 +8,11 @@ mod _suggestions {
 
     #[pyfunction]
     fn _generate_suggestions(
-        canidates: Vec<PyObjectRef>,
+        candidates: Vec<PyObjectRef>,
         name: PyObjectRef,
         vm: &VirtualMachine,
     ) -> PyObjectRef {
-        match crate::vm::suggestion::calculate_suggestions(canidates.iter(), &name) {
+        match crate::vm::suggestion::calculate_suggestions(candidates.iter(), &name) {
             Some(suggestion) => suggestion.into(),
             None => vm.ctx.none(),
         }
