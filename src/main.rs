a83e290229ef191500b44fbf2068dc5ee1d58e4d
Ashwin Naren <arihant2math@gmail.com>
2025-04-07 11:08:17 -0700

    fix CI

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use mimalloc::MiMalloc;
+#[cfg(any(target_os = "linux", target_os = "windows", target_os = "macos"))]
+mod _alloc {
+    use mimalloc::MiMalloc;
 
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+    #[global_allocator]
+    static GLOBAL: MiMalloc = MiMalloc;
+}
 
 pub fn main() -> std::process::ExitCode {
     rustpython::run(|_vm| {})
