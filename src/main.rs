226c1d544730b990d6da0d18d2d1b1a91aaf029e
Ashwin Naren <arihant2math@gmail.com>
2025-04-08 10:13:40 -0700

    eh

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-#[cfg(all(
-    any(target_os = "linux", target_os = "macos", target_os = "windows"),
-    not(any(target_env = "musl", target_env = "sgx"))
-))]
-mod _alloc {
-    use mimalloc::MiMalloc;
+use rustpython_vm::RustPythonAllocator;
 
-    #[global_allocator]
-    static GLOBAL: MiMalloc = MiMalloc;
-}
+#[global_allocator]
+static ALLOCATOR: RustPythonAllocator = RustPythonAllocator::new();
 
 pub fn main() -> std::process::ExitCode {
     rustpython::run(|_vm| {})
