d520d5f4a4ebf0dfec2cd494d68015b344bdfdd2
Ashwin Naren <arihant2math@gmail.com>
2025-04-08 11:31:44 -0700

    implement more of the concrete object layer

--- capi/src/int.rs
+++ capi/src/int.rs
@@ -1,7 +1,11 @@
+// https://docs.python.org/3/c-api/long.html
+
+use std::ffi;
+
 use rustpython_vm::{PyObject, PyObjectRef};
 
 #[unsafe(export_name = "PyLong_FromLong")]
-pub unsafe extern "C" fn long_from_long(value: i64) -> *mut PyObject {
+pub unsafe extern "C" fn long_from_long(value: ffi::c_long) -> *mut PyObject {
     let vm = crate::get_vm();
     Into::<PyObjectRef>::into(vm.ctx.new_int(value))
         .into_raw()
@@ -9,8 +13,35 @@ pub unsafe extern "C" fn long_from_long(value: i64) -> *mut PyObject {
 }
 
 #[unsafe(export_name = "PyLong_FromUnsignedLong")]
-pub unsafe extern "C" fn long_from_unsigned_long(value: u64) -> *mut PyObject {
+pub unsafe extern "C" fn long_from_unsigned_long(value: ffi::c_ulong) -> *mut PyObject {
     let vm = crate::get_vm();
+    Into::<PyObjectRef>::into(vm.ctx.new_int(value))
+        .into_raw()
+        .as_ptr()
+}
+
+// TODO: PyLong_FromSsize_t
+// TODO: PyLong_FromSize_t
+#[unsafe(export_name = "PyLong_FromLongLong")]
+pub unsafe extern "C" fn long_from_long_long(value: ffi::c_longlong) -> *mut PyObject {
+    let vm = crate::get_vm();
+    Into::<PyObjectRef>::into(vm.ctx.new_int(value))
+        .into_raw()
+        .as_ptr()
+}
+
+#[unsafe(export_name = "PyLong_FromUnsignedLongLong")]
+pub unsafe extern "C" fn long_from_unsigned_long_long(value: ffi::c_ulonglong) -> *mut PyObject {
+    let vm = crate::get_vm();
+    Into::<PyObjectRef>::into(vm.ctx.new_int(value))
+        .into_raw()
+        .as_ptr()
+}
+
+#[unsafe(export_name = "PyLong_FromDouble")]
+pub unsafe extern "C" fn long_from_double(value: ffi::c_double) -> *mut PyObject {
+    let vm = crate::get_vm();
+    let value = value as i64;
     Into::<PyObjectRef>::into(vm.ctx.new_int(value))
         .into_raw()
         .as_ptr()
