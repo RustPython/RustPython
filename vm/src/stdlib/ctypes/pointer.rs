ac379ce8666a67ac8f9fad1e95d7cca9222e86d4
Ashwin Naren <arihant2math@gmail.com>
2025-04-10 15:09:19 -0700

    fix import

--- vm/src/stdlib/ctypes/pointer.rs
+++ vm/src/stdlib/ctypes/pointer.rs
@@ -1,8 +1,13 @@
+crate::builtins::PyType;
+
 #[pyclass(name = "PyCPointerType", base = "PyType", module = "_ctypes")]
 #[derive(PyPayload)]
 pub struct PyCPointerType {
     pub(super) inner: PyCPointer,
 }
+
+#[pyclass]
+impl PyCPointerType {}
 
 #[pyclass(name = "_Pointer", base = "PyCData", metaclass = "PyCPointerType", module = "_ctypes")]
 pub struct PyCPointer {}
