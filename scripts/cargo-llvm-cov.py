df63e10b2fa7fd394beb2ab17f7d7eb6aeabe5f4
Rex Ledesma <rex@rexledesma.com>
2025-05-08 00:37:54 -0400

    chore: rely on the default inclusions for `ruff`

--- scripts/cargo-llvm-cov.py
+++ scripts/cargo-llvm-cov.py
@@ -3,18 +3,21 @@ import subprocess
 
 TARGET = "extra_tests/snippets"
 
+
 def run_llvm_cov(file_path: str):
-    """ Run cargo llvm-cov on a file. """
+    """Run cargo llvm-cov on a file."""
     if file_path.endswith(".py"):
         command = ["cargo", "llvm-cov", "--no-report", "run", "--", file_path]
         subprocess.call(command)
 
+
 def iterate_files(folder: str):
-    """ Iterate over all files in a folder. """
+    """Iterate over all files in a folder."""
     for root, _, files in os.walk(folder):
         for file in files:
             file_path = os.path.join(root, file)
             run_llvm_cov(file_path)
+
 
 if __name__ == "__main__":
     iterate_files(TARGET)
