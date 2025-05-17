da8971b0601e36ed09437ea6ad3279e0ceb0774d
Ashwin Naren <arihant2math@gmail.com>
2025-05-17 06:28:35 +0000

    display logo on windows executable

---
+++ build.rs
@@ -0,0 +1,7 @@
+fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("logo.ico");
+        res.compile().unwrap();
+    }
+}
