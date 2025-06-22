dfc8fe018fe05116885ec1dc21356737afd95458
Lee Dogeon <dev.moreal@gmail.com>
2025-06-23 08:55:28 +0900

    Unmark fixed tests (#5818)

--- Lib/test/test_descr.py
+++ Lib/test/test_descr.py
@@ -1851,8 +1851,6 @@ order (MRO) for bases """
         object.__init__(A(3))
         self.assertRaises(TypeError, object.__init__, A(3), 5)
 
-    @unittest.expectedFailure
-    @unittest.skip("TODO: RUSTPYTHON")
     def test_restored_object_new(self):
         class A(object):
             def __new__(cls, *args, **kwargs):
