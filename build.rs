ca874988e72c4de8cdabc824a4201bfbe3839fba
Ashwin Naren <arihant2math@gmail.com>
2025-06-28 10:18:36 -0700

    rerun build.rs if logo is changed

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        println!("cargo:rerun-if-changed=logo.ico");
         let mut res = winresource::WindowsResource::new();
         if std::path::Path::new("logo.ico").exists() {
             res.set_icon("logo.ico");
