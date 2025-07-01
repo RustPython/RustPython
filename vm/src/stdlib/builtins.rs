5db7ccc022849ebb2a97506990efa515ddda6730
Jeong YunWon <jeong@youknowone.org>
2025-07-01 22:38:02 +0900

    __type_params__ in __build_class__

--- vm/src/stdlib/builtins.rs
+++ vm/src/stdlib/builtins.rs
@@ -943,8 +943,51 @@ mod builtins {
             )?;
         }
 
+        // Check if we need to set __parameters__ for PEP 695 classes
+        let function_obj: PyObjectRef = function.clone().into();
+        let has_type_params =
+            if let Ok(type_params) = function_obj.get_attr(identifier!(vm, __type_params__), vm) {
+                if let Ok(type_params_tuple) = type_params.downcast::<PyTuple>() {
+                    !type_params_tuple.is_empty()
+                } else {
+                    false
+                }
+            } else {
+                false
+            };
+
+        let needs_parameters = if has_type_params {
+            if let Ok(bases_tuple) = bases.clone().downcast::<PyTuple>() {
+                bases_tuple.iter().any(|base| {
+                    if let Ok(base_name) = base.get_attr(identifier!(vm, __name__), vm) {
+                        if let Ok(name_str) = base_name.downcast::<PyStr>() {
+                            let name = name_str.as_str();
+                            return name == "Protocol" || name == "Generic";
+                        }
+                    }
+                    false
+                })
+            } else {
+                false
+            }
+        } else {
+            false
+        };
+
         let args = FuncArgs::new(vec![name_obj.into(), bases, namespace.into()], kwargs);
         let class = metaclass.call(args, vm)?;
+
+        // Set __type_params__ on the class if the function has type params
+        if has_type_params {
+            if let Ok(type_params) = function_obj.get_attr(identifier!(vm, __type_params__), vm) {
+                class.set_attr(identifier!(vm, __type_params__), type_params.clone(), vm)?;
+
+                if needs_parameters {
+                    // Set __parameters__ to the same value as __type_params__
+                    class.set_attr(identifier!(vm, __parameters__), type_params, vm)?;
+                }
+            }
+        }
 
         if let Some(ref classcell) = classcell {
             let classcell = classcell.get().ok_or_else(|| {
