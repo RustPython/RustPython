5985ec8be0069ea84d681beadc09e220b1eb3388
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-07-09 18:47:24 +0300

    `sys.platform` android & ios (#5921)
    * Make sys.platform to return 'android'
    
    * Added missing 'ios' platform

--- vm/src/stdlib/sys.rs
+++ vm/src/stdlib/sys.rs
@@ -67,11 +67,14 @@ mod sys {
     #[pyattr(name = "platform")]
     pub(crate) const PLATFORM: &str = {
         cfg_if::cfg_if! {
-            if #[cfg(any(target_os = "linux", target_os = "android"))] {
-                // Android is linux as well. see https://bugs.python.org/issue32637
+            if #[cfg(target_os = "linux")] {
                 "linux"
+            } else if #[cfg(target_os = "android")] {
+                "android"
             } else if #[cfg(target_os = "macos")] {
                 "darwin"
+            } else if #[cfg(target_os = "ios")] {
+                "ios"
             } else if #[cfg(windows)] {
                 "win32"
             } else if #[cfg(target_os = "wasi")] {
