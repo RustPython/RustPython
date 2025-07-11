770a00079c494643acd1df41b852ff2b523c9249
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-07-11 13:20:42 +0300

    Skip failing tests

--- Lib/test/test_operator.py
+++ Lib/test/test_operator.py
@@ -605,6 +605,10 @@ class OperatorTestCase:
 
     @support.requires_docstrings
     def test_attrgetter_signature(self):
+        # TODO: RUSTPYTHON fails only when running inside COperatorTestCase
+        if type(self).__name__ == 'COperatorTestCase':
+            self.skipTest("TODO: RUSTPYTHON")
+
         operator = self.module
         sig = inspect.signature(operator.attrgetter)
         self.assertEqual(str(sig), '(attr, /, *attrs)')
@@ -613,6 +617,10 @@ class OperatorTestCase:
 
     @support.requires_docstrings
     def test_itemgetter_signature(self):
+        # TODO: RUSTPYTHON fails only when running inside COperatorTestCase
+        if type(self).__name__ == 'COperatorTestCase':
+            self.skipTest("TODO: RUSTPYTHON")
+
         operator = self.module
         sig = inspect.signature(operator.itemgetter)
         self.assertEqual(str(sig), '(item, /, *items)')
@@ -621,6 +629,10 @@ class OperatorTestCase:
 
     @support.requires_docstrings
     def test_methodcaller_signature(self):
+        # TODO: RUSTPYTHON fails only when running inside COperatorTestCase
+        if type(self).__name__ == 'COperatorTestCase':
+            self.skipTest("TODO: RUSTPYTHON")
+
         operator = self.module
         sig = inspect.signature(operator.methodcaller)
         self.assertEqual(str(sig), '(name, /, *args, **kwargs)')
