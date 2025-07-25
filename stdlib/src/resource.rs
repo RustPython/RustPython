01edb939579c1cb1d3dccb46a26ace936b142adf
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2025-07-25 03:46:32 +0200

    Resolve `libc::RLIM_NLIMITS` warning on android (#6025)
    * Resolve warning on android
    
    * Apply CodeRabbit suggestion

--- stdlib/src/resource.rs
+++ stdlib/src/resource.rs
@@ -14,7 +14,8 @@ mod resource {
 
     cfg_if::cfg_if! {
         if #[cfg(target_os = "android")] {
-            use libc::RLIM_NLIMITS;
+            #[expect(deprecated)]
+            const RLIM_NLIMITS: i32 = libc::RLIM_NLIMITS;
         } else {
             // This constant isn't abi-stable across os versions, so we just
             // pick a high number so we don't get false positive ValueErrors and just bubble up the
