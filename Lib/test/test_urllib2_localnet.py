48cf12f7ab31e6c65f2ae7c469c9b35aaf7f6e66
Jeong YunWon <jeong@youknowone.org>
2025-10-26 10:06:33 +0900

    fix certdata path

--- Lib/test/test_urllib2_localnet.py
+++ Lib/test/test_urllib2_localnet.py
@@ -19,9 +19,9 @@ except ImportError:
 
 here = os.path.dirname(__file__)
 # Self-signed cert file for 'localhost'
-CERT_localhost = os.path.join(here, 'keycert.pem')
+CERT_localhost = os.path.join(here, 'certdata', 'keycert.pem')
 # Self-signed cert file for 'fakehostname'
-CERT_fakehostname = os.path.join(here, 'keycert2.pem')
+CERT_fakehostname = os.path.join(here, 'certdata', 'keycert2.pem')
 
 
 # Loopback http server infrastructure
