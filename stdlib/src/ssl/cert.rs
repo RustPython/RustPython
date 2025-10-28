9aa9b93332e21aa9591c8f01d8e6924a760025f8
Jeong YunWon <jeong@youknowone.org>
2025-10-28 10:58:31 +0900

    fix ipv6 formattig

--- stdlib/src/ssl/cert.rs
+++ stdlib/src/ssl/cert.rs
@@ -164,12 +164,16 @@ pub(crate) mod ssl_cert {
                             // IPv4
                             format!("{}.{}.{}.{}", ip[0], ip[1], ip[2], ip[3])
                         } else if ip.len() == 16 {
-                            // IPv6 - use standard library for canonical compressed format
+                            // IPv6 - format with all zeros visible (not compressed)
                             let ip_addr = std::net::Ipv6Addr::from([
                                 ip[0], ip[1], ip[2], ip[3], ip[4], ip[5], ip[6], ip[7], ip[8],
                                 ip[9], ip[10], ip[11], ip[12], ip[13], ip[14], ip[15],
                             ]);
-                            ip_addr.to_string()
+                            let s = ip_addr.segments();
+                            format!(
+                                "{:X}:{:X}:{:X}:{:X}:{:X}:{:X}:{:X}:{:X}",
+                                s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7]
+                            )
                         } else {
                             // Fallback for unexpected length
                             String::from_utf8_lossy(ip).into_owned()
