55c71aa0ee1d252d7a52f57c9031d47a0285c2f3
Padraic Fanning <fanninpm@miamioh.edu>
2026-01-04 12:45:21 -0500

    Fix multiprocessing tests on Darwin

--- Lib/test/test_multiprocessing_forkserver/test_processes.py
+++ Lib/test/test_multiprocessing_forkserver/test_processes.py
@@ -4,9 +4,15 @@ from test._test_multiprocessing import install_tests_in_module_dict
 install_tests_in_module_dict(globals(), 'forkserver', only_type="processes")
 
 import os, sys  # TODO: RUSTPYTHON
+class WithProcessesTestCondition(WithProcessesTestCondition):  # TODO: RUSTPYTHON
+    @unittest.skipIf(sys.platform == 'darwin', 'TODO: RUSTPYTHON flaky timeout')
+    def test_notify(self): super().test_notify()
+    @unittest.skipIf(sys.platform == 'darwin', 'TODO: RUSTPYTHON flaky timeout')
+    def test_notify(self): super().test_notify_n()
+
 class WithProcessesTestLock(WithProcessesTestLock):  # TODO: RUSTPYTHON
     @unittest.skipIf(  # TODO: RUSTPYTHON
-        sys.platform == 'linux' and 'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
+        'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
         'TODO: RUSTPYTHON environment pollution when running rustpython -m test --fail-env-changed due to unknown reason'
     )  # TODO: RUSTPYTHON
     def test_repr_lock(self): super().test_repr_lock()  # TODO: RUSTPYTHON
@@ -18,17 +24,17 @@ class WithProcessesTestLock(WithProcessesTestLock):  # TODO: RUSTPYTHON
 
 class WithProcessesTestPool(WithProcessesTestPool):  # TODO: RUSTPYTHON
     @unittest.skipIf(  # TODO: RUSTPYTHON
-        sys.platform == 'linux' and 'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
+        'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
         'TODO: RUSTPYTHON environment pollution when running rustpython -m test --fail-env-changed due to unknown reason'
     )  # TODO: RUSTPYTHON
     def test_async_timeout(self): super().test_async_timeout()  # TODO: RUSTPYTHON
     @unittest.skipIf(  # TODO: RUSTPYTHON
-        sys.platform == 'linux' and 'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
+        'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
         'TODO: RUSTPYTHON environment pollution when running rustpython -m test --fail-env-changed due to unknown reason'
     )  # TODO: RUSTPYTHON
     def test_terminate(self): super().test_terminate()  # TODO: RUSTPYTHON
     @unittest.skipIf(  # TODO: RUSTPYTHON
-        sys.platform == 'linux' and 'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
+        'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
         'TODO: RUSTPYTHON environment pollution when running rustpython -m test --fail-env-changed due to unknown reason'
     )  # TODO: RUSTPYTHON
     def test_traceback(self): super().test_traceback()  # TODO: RUSTPYTHON
--- Lib/test/test_multiprocessing_forkserver/test_threads.py
+++ Lib/test/test_multiprocessing_forkserver/test_threads.py
@@ -3,10 +3,10 @@ from test._test_multiprocessing import install_tests_in_module_dict
 
 install_tests_in_module_dict(globals(), 'forkserver', only_type="threads")
 
-import os, sys  # TODO: RUSTPYTHON
+import os  # TODO: RUSTPYTHON
 class WithThreadsTestPool(WithThreadsTestPool):  # TODO: RUSTPYTHON
     @unittest.skipIf(  # TODO: RUSTPYTHON
-        sys.platform == 'linux' and 'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
+        'RUSTPYTHON_SKIP_ENV_POLLUTERS' in os.environ,  # TODO: RUSTPYTHON
         'TODO: RUSTPYTHON environment pollution when running rustpython -m test --fail-env-changed due to unknown reason'
     )  # TODO: RUSTPYTHON
     def test_terminate(self): super().test_terminate()  # TODO: RUSTPYTHON
