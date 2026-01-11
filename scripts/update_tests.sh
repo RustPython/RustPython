0fe8ac2e9b60d7987a8de39e04e5f0278f13a898
Terry Luan <terryluan12@gmail.com>
2026-01-10 20:56:12 -0500

    Updated update_tests.sh * Added usage instructions * Added basic usability (debugging + automatic adding expectedFailure)

--- scripts/update_tests.sh
+++ scripts/update_tests.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-c <cpython path>] [-r <rustpython path>] [-u <copy untracked test>]" 1>&2; exit 1; }
+usage() {
+    cat >&2 <<EOF
+Usage: $0 [OPTIONS]
+
+Copy tests from CPython to RustPython.
+Optionally copy untracked tests, and dynamic annotation of failures.
+
+Options:
+  -c/--cpython-path <path>   Path to the CPython source tree (older version)
+  -r/--rpython-path <path>   Path to the RustPython source tree (newer version)
+  -u/--copy-untracked        Copy untracked tests only
+  -a/--annotate              While copying tests, run them and annotate failures dynamically
+  -h/--help                  Show this help message and exit
+
+Example Usage: $0 -c ~/cpython -r .
+EOF
+    exit 1
+}
+
+if [[ $# -eq 0 ]]; then
+    usage
+    exit 1
+fi
 
 
 cpython_path=""
 rpython_path=""
 copy_untracked=false
+annotate=false
 libraries=()
 
 while [[ $# -gt 0 ]]; do
@@ -22,6 +45,14 @@ while [[ $# -gt 0 ]]; do
             copy_untracked=true
             shift
             ;;
+        -h|--help)
+            usage
+            return
+            ;;
+        -a|--annotate)
+            annotate=true
+            shift
+            ;;
         *)
             libraries+=("$1")
             shift
@@ -32,35 +63,49 @@ done
 cpython_path="$cpython_path/Lib/test"
 rpython_path="$rpython_path/Lib/test"
 
-
-if [[ ${#libraries[@]} -eq 0 ]]; then
-    libraries=$(find ${cpython_path} -type f -printf "%P\n")
-fi
-echo "libraries is ${libraries}"
-
-for lib in "${arr[@]}"
-do 
-    cpython_file="$cpython_path/$lib"
-    rpython_file="$rpython_path/$lib"
-
-
-    if [[ $files_equal $cpython_file $rpython_file -eq 0 ]]; then
-        continue
+update_libraries() {
+    if [[ ${#libraries[@]} -eq 0 ]]; then
+        libraries=$(find ${cpython_path} -type f -printf "%P\n")
     fi
 
-    if [[ ! -f $cpython_file ]]; then
-        if $copy_untracked then
-            echo "Test file $lib missing. Copying..."
-            cp "$cpython_file" "$rpython_file"
+    for lib in "${libraries[@]}"
+    do 
+        cpython_file="$cpython_path/$lib"
+        rpython_file="$rpython_path/$lib"
+
+        filename=${lib##*/}
+        basename=${filename%.py}
+
+        if files_equal "$cpython_file" "$rpython_file"; then
+            echo "No changes in $lib. Skipping..." 
+            continue
         fi
-    else
-        echo "Updating $lib..."
-        ./scripts/lib_updater.py --from ${rpython_path}/Lib/test/$lib --to ${rpython_path}/Lib/test/$lib
-    fi
-    
 
-    cargo run
-done
+        if [[ ! -f "$rpython_file" ]]; then
+            echo "Test file $lib missing."
+            if $copy_untracked; then
+                echo "Copying..."
+                cp "$cpython_file" "$rpython_file"
+            fi
+        else
+            ./scripts/lib_updater.py --from $cpython_file --to $rpython_file -o $rpython_file
+        fi
+
+        if $annotate; then
+            output=$(cargo run --release --features encodings,sqlite -- -m test -j 1 -u all --slowest --fail-env-changed -v $lib 2>&1)
+            failed_tests=$(echo "$output" | grep '^FAIL: ' | awk '{print $2}')
+            errored_tests=$(echo "$output" | grep '^ERROR ' | awk '{print $2}')
+            failed_tests=$(echo "$failed_tests" | sort -u)
+            
+            for test in "${failed_tests[@]}"
+            do
+                sed -i "s/^\([[:space:]]*\)def $test(/\1@unittest.expectedFailure # TODO: RUSTPYTHON\n\1def $test(/" "$rpython_file"
+            done
+            output=$(cargo run --release --features encodings,sqlite -- -m test -j 1 -u all --slowest --fail-env-changed -v $lib 2>&1)
+            echo "$failed_tests"
+        fi
+    done
+}
 
 
 
@@ -71,3 +116,5 @@ files_equal() {
     return $files_equal
 }
 
+
+update_libraries
