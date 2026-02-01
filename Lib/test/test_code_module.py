8eda61c3960d9d7e2dec709233563fd0b66035f6
Padraic Fanning <fanninpm@miamioh.edu>
2026-02-01 18:57:43 -0500

    Mark failing tests

--- Lib/test/test_code_module.py
+++ Lib/test/test_code_module.py
@@ -91,6 +91,7 @@ class TestInteractiveConsole(unittest.TestCase, MockSys):
         else:
             raise AssertionError("no console stdout")
 
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; +  'SyntaxError: invalid syntax']
     def test_syntax_error(self):
         self.infunc.side_effect = ["def f():",
                                    "    x = ?",
@@ -111,6 +112,7 @@ class TestInteractiveConsole(unittest.TestCase, MockSys):
         self.assertIsNone(self.sysmod.last_value.__traceback__)
         self.assertIs(self.sysmod.last_exc, self.sysmod.last_value)
 
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; -  'IndentationError: unexpected indentation']
     def test_indentation_error(self):
         self.infunc.side_effect = ["  1", EOFError('Finished')]
         self.console.interact()
@@ -127,6 +129,7 @@ class TestInteractiveConsole(unittest.TestCase, MockSys):
         self.assertIsNone(self.sysmod.last_value.__traceback__)
         self.assertIs(self.sysmod.last_exc, self.sysmod.last_value)
 
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; AssertionError: 'UnicodeDecodeError: invalid utf-8 sequence of 1 bytes from index 1\n\nnow exiti [truncated]... doesn't start with 'UnicodeEncodeError: '
     def test_unicode_error(self):
         self.infunc.side_effect = ["'\ud800'", EOFError('Finished')]
         self.console.interact()
@@ -163,6 +166,7 @@ class TestInteractiveConsole(unittest.TestCase, MockSys):
             '  File "<console>", line 2, in f\n',
             'ValueError: BOOM!\n'])
 
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; +  'SyntaxError: invalid syntax\n']
     def test_sysexcepthook_syntax_error(self):
         self.infunc.side_effect = ["def f():",
                                    "    x = ?",
@@ -186,6 +190,7 @@ class TestInteractiveConsole(unittest.TestCase, MockSys):
             '        ^\n',
             'SyntaxError: invalid syntax\n'])
 
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; +  'IndentationError: unexpected indent\n']
     def test_sysexcepthook_indentation_error(self):
         self.infunc.side_effect = ["  1", EOFError('Finished')]
         hook = mock.Mock()
@@ -280,7 +285,7 @@ class TestInteractiveConsole(unittest.TestCase, MockSys):
         self.assertEqual(err_msg, ['write', (expected,), {}])
 
 
-    @unittest.expectedFailure  # TODO: RUSTPYTHON
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; AssertionError: '\nAttributeError\n\nThe above exception was the direct cause of the following exception:\n\nTraceback (most recent call last):\n  File "<console>", line 1, in <module>\nValueError\n' not found in 'Python <MagicMock name=\'sys.version\' id=\'94615517503920\'> on <MagicMock name=\'sys.platform\' id=\'94615517656384\'>\nType "help", "copyright", "credits" or "license" for more information.\n(InteractiveConsole)\nAttributeError\n\nThe above exception was the direct cause of the following exception:\n\nTraceback (most recent call last):\n  File "<console>", line 1, in <module>\nValueError: \n\nnow exiting InteractiveConsole...\n'
     def test_cause_tb(self):
         self.infunc.side_effect = ["raise ValueError('') from AttributeError",
                                     EOFError('Finished')]
