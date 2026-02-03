9706cec09da38fc28f8b4cf90c769c36421c7660
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-02-03 15:16:05 +0200

    Update `_compat_pickle.py` from 3.14.2

--- Lib/_compat_pickle.py
+++ Lib/_compat_pickle.py
@@ -22,7 +22,6 @@ IMPORT_MAPPING = {
     'tkMessageBox': 'tkinter.messagebox',
     'ScrolledText': 'tkinter.scrolledtext',
     'Tkconstants': 'tkinter.constants',
-    'Tix': 'tkinter.tix',
     'ttk': 'tkinter.ttk',
     'Tkinter': 'tkinter',
     'markupbase': '_markupbase',
@@ -148,14 +147,6 @@ except NameError:
 else:
     PYTHON2_EXCEPTIONS += ("WindowsError",)
 
-# NOTE: RUSTPYTHON exceptions
-try:
-    JitError
-except NameError:
-    pass
-else:
-    PYTHON2_EXCEPTIONS += ("JitError",)
-
 for excname in PYTHON2_EXCEPTIONS:
     NAME_MAPPING[("exceptions", excname)] = ("builtins", excname)
 
@@ -257,3 +248,4 @@ PYTHON3_IMPORTERROR_EXCEPTIONS = (
 
 for excname in PYTHON3_IMPORTERROR_EXCEPTIONS:
     REVERSE_NAME_MAPPING[('builtins', excname)] = ('exceptions', 'ImportError')
+del excname
