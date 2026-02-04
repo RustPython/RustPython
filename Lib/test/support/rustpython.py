a65a1e534d895c6938f125746f90db21b3959c61
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-02-04 09:53:02 +0200

    Address review

--- Lib/test/support/rustpython.py
+++ Lib/test/support/rustpython.py
@@ -12,9 +12,13 @@ EXPECTED_FAILURE = doctest.register_optionflag("EXPECTED_FAILURE")
 class DocTestChecker(doctest.OutputChecker):
     """
     Custom output checker that lets us to add: `+EXPECTED_FAILURE` for doctest tests.
+
+    We want to be able to mark failing doctest test the same way we do with normal
+    unit test, without this class we would have to skip the doctest for the CI to pass.
     """
 
     def check_output(self, want, got, optionflags):
+        res = super().check_output(want, got, optionflags)
         if optionflags & EXPECTED_FAILURE:
-            return want != got
-        return super().check_output(want, got, optionflags)
+            res = not res
+        return res
