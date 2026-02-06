14f3030454bb1835c00c852b832fce1abd6468d6
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-02-06 13:33:27 +0200

    Add const assert

--- crates/compiler-core/src/bytecode/oparg.rs
+++ crates/compiler-core/src/bytecode/oparg.rs
@@ -209,6 +209,8 @@ macro_rules! impl_oparg_enum {
         }
 
         impl OpArgType for $name {}
+
+        const _: () = assert!(::core::mem::size_of::<$name>() == ::core::mem::size_of::<u8>());
     };
 }
 
