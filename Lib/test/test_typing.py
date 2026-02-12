bdcc04798e20e480a018ad606e308ca30fc4abf6
Elmir Jagudin <elmir.jagudin@gmail.com>
2026-02-12 14:36:14 +0100

    raise TypeError when `__iter__` method is `None`
    Setting special `__iter__` method to `None` is used to mark
    objects as non-iterable.
    
    https://docs.python.org/3/reference/datamodel.html#special-method-names
    
    Check if `__iter__` is set to `None` and raise TypeError exception.

--- Lib/test/test_typing.py
+++ Lib/test/test_typing.py
@@ -10916,7 +10916,6 @@ class TypeIterationTests(BaseTestCase):
         Annotated[T, ''],
     )
 
-    @unittest.expectedFailure  # TODO: RUSTPYTHON
     def test_cannot_iterate(self):
         expected_error_regex = "object is not iterable"
         for test_type in self._UNITERABLE_TYPES:
