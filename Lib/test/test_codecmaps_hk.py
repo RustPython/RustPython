5caa9009b746cc459c36c6b46ed26ca73f1e90a2
Lee Dogeon <dev.moreal@gmail.com>
2026-02-15 14:33:05 +0900

    Mark failing tests for test_codecmaps_hk

--- Lib/test/test_codecmaps_hk.py
+++ Lib/test/test_codecmaps_hk.py
@@ -11,5 +11,9 @@ class TestBig5HKSCSMap(multibytecodec_support.TestBase_Mapping,
     encoding = 'big5hkscs'
     mapfileurl = 'http://www.pythontest.net/unicode/BIG5HKSCS-2004.TXT'
 
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; LookupError: unknown encoding: big5hkscs
+    def test_mapping_file(self):
+        return super().test_mapping_file()
+
 if __name__ == "__main__":
     unittest.main()
