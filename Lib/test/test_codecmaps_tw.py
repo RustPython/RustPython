2fee84e38a1b4d75ea480779eb457cfe31254376
Lee Dogeon <dev.moreal@gmail.com>
2026-02-15 14:33:37 +0900

    Mark failing tests for test_codecmaps_tw

--- Lib/test/test_codecmaps_tw.py
+++ Lib/test/test_codecmaps_tw.py
@@ -11,6 +11,10 @@ class TestBIG5Map(multibytecodec_support.TestBase_Mapping,
     encoding = 'big5'
     mapfileurl = 'http://www.pythontest.net/unicode/BIG5.TXT'
 
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; LookupError: unknown encoding: big5
+    def test_mapping_file(self):
+        return super().test_mapping_file()
+
 class TestCP950Map(multibytecodec_support.TestBase_Mapping,
                    unittest.TestCase):
     encoding = 'cp950'
@@ -22,6 +26,14 @@ class TestCP950Map(multibytecodec_support.TestBase_Mapping,
     codectests = (
         (b"\xFFxy", "replace",  "\ufffdxy"),
     )
+
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; LookupError: unknown encoding: cp950
+    def test_errorhandle(self):
+        return super().test_errorhandle()
+
+    @unittest.expectedFailure  # TODO: RUSTPYTHON; LookupError: unknown encoding: cp950
+    def test_mapping_file(self):
+        return super().test_mapping_file()
 
 if __name__ == "__main__":
     unittest.main()
