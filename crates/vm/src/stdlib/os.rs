1b6d849ab46dd26b2a62aef08c0ca115339bf0da
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-02-27 12:52:40 +0100

    Try to add cfg_attr at struct

--- crates/vm/src/stdlib/os.rs
+++ crates/vm/src/stdlib/os.rs
@@ -1215,6 +1215,7 @@ pub(super) mod _os {
         }
     }
 
+    #[cfg_attr(target_env = "musl", allow(deprecated))]
     #[derive(Debug, FromArgs)]
     #[pystruct_sequence_data]
     struct StatResultData {
