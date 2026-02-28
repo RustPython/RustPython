79983fdd2e6336ac02a4389714538a4dc4fe9699
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-02-28 02:34:38 +0100

    Update CI ruff to `0.15.4` (#7250)
    * Update CI ruff to 0.15.4
    
    * Format

--- extra_tests/snippets/builtin_filter.py
+++ extra_tests/snippets/builtin_filter.py
@@ -1,4 +1,4 @@
-assert list(filter(lambda x: ((x % 2) == 0), [0, 1, 2])) == [0, 2]
+assert list(filter(lambda x: (x % 2) == 0, [0, 1, 2])) == [0, 2]
 
 # None implies identity
 assert list(filter(None, [0, 1, 2])) == [1, 2]
@@ -18,7 +18,7 @@ class Counter(object):
         return self
 
 
-it = filter(lambda x: ((x % 2) == 0), Counter())
+it = filter(lambda x: (x % 2) == 0, Counter())
 assert next(it) == 2
 assert next(it) == 4
 
