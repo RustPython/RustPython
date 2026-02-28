a685d23cbb345c4c794772312a0bc2f85c764f0f
Jeong, YunWon <jeong@youknowone.org>
2026-02-28 10:38:59 +0900

    skip

--- Lib/test/test_multiprocessing_fork/test_processes.py
+++ Lib/test/test_multiprocessing_fork/test_processes.py
@@ -3,6 +3,14 @@ from test._test_multiprocessing import install_tests_in_module_dict
 
 install_tests_in_module_dict(globals(), 'fork', only_type="processes")
 
+import sys  # TODO: RUSTPYTHON
+class WithProcessesTestManagerRestart(WithProcessesTestManagerRestart):  # TODO: RUSTPYTHON
+    @unittest.skipIf(sys.platform == 'linux', 'TODO: RUSTPYTHON flaky segfault in Manager accepter thread after fork')
+    def test_rapid_restart(self): super().test_rapid_restart()  # TODO: RUSTPYTHON
+
+class WithProcessesTestSharedMemory(WithProcessesTestSharedMemory):  # TODO: RUSTPYTHON
+    @unittest.skipIf(sys.platform == 'linux', 'TODO: RUSTPYTHON flaky segfault in Manager accepter thread after fork')
+    def test_shared_memory_SharedMemoryManager_basics(self): super().test_shared_memory_SharedMemoryManager_basics()  # TODO: RUSTPYTHON
 
 if __name__ == '__main__':
     unittest.main()
