63dd6a6d9d8c7e0fa38c29a34d60d5ec178d6b06
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-02-28 02:36:09 +0100

    Update `subprocess.py` to 3.14.3 (#7244)

--- Lib/test/test_subprocess.py
+++ Lib/test/test_subprocess.py
@@ -1903,7 +1903,7 @@ class RunFuncTestCase(BaseTestCase):
         res = subprocess.run(args)
         self.assertEqual(res.returncode, 57)
 
-    @unittest.skipIf(mswindows, 'TODO: RUSTPYTHON; empty env block fails nondeterministically')
+    @unittest.skipIf(mswindows, "TODO: RUSTPYTHON; empty env block fails nondeterministically")
     @unittest.skipUnless(mswindows, "Maybe test trigger a leak on Ubuntu")
     def test_run_with_an_empty_env(self):
         # gh-105436: fix subprocess.run(..., env={}) broken on Windows
