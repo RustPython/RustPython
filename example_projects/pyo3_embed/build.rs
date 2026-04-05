565bf5ebfbc8c56ad5a450832fdbd85b997c41ac
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-04-05 14:07:16 +0200

    Cleanup

--- example_projects/pyo3_embed/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!(
-        "cargo:rustc-link-arg=/Users/basschoenmaeckers/repo/RustPython/target/debug/librustpython_capi.a"
-    );
-    println!("cargo:rustc-link-lib=framework=CoreFoundation");
-}
