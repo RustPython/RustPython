24b2c6384b82aa3ad5ac54154f854c7330e953c2
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-04-05 11:41:57 +0200

    implement `PyLong_AsLong`

--- crates/capi/src/longobject.rs
+++ crates/capi/src/longobject.rs
@@ -4,6 +4,7 @@ use core::ptr;
 use crate::PyObject;
 use crate::pylifecycle::INTERP;
 use rustpython_vm::PyObjectRef;
+use rustpython_vm::builtins::PyInt;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn PyLong_FromLong(value: c_long) -> *mut PyObject {
@@ -48,4 +49,31 @@ pub extern "C" fn PyLong_FromUnsignedLong(_value: c_ulong) -> *mut PyObject {
 pub extern "C" fn PyLong_FromUnsignedLongLong(_value: c_ulonglong) -> *mut PyObject {
     crate::log_stub("PyLong_FromUnsignedLongLong");
     ptr::null_mut()
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn PyLong_AsLong(obj: *mut PyObject) -> c_long {
+    if obj.is_null() {
+        panic!("PyLong_AsLong called with null object");
+    }
+
+    INTERP.with(|interp_ref| {
+        let interp = interp_ref.borrow();
+        let interp = interp
+            .as_ref()
+            .expect("PyLong_AsLong called before Py_InitializeEx");
+
+        interp.enter(|_vm| {
+            // SAFETY: non-null checked above; caller promises a valid PyObject pointer.
+            let obj_ref = unsafe { &*obj };
+            let int_obj = obj_ref
+                .downcast_ref::<PyInt>()
+                .expect("PyLong_AsLong currently only accepts int instances");
+
+            int_obj
+                .as_bigint()
+                .try_into()
+                .expect("PyLong_AsLong: value out of range for c_long")
+        })
+    })
 }
