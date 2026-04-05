b08d41ab2703917e9a67b7240a0d6fc5f42413bf
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-04-05 11:22:46 +0200

    Implement `PyUnicode_FromStringAndSize`

--- example_projects/pyo3_embed/src/main.rs
+++ example_projects/pyo3_embed/src/main.rs
@@ -7,5 +7,8 @@ fn main() {
     Python::attach(|py| {
         let number = PyInt::new(py, 123);
         assert!(number.is_instance_of::<PyInt>());
+
+        let string = PyString::new(py, "Hello, World!");
+        assert!(string.is_instance_of::<PyString>());
     });
 }
