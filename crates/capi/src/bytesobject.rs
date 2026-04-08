09466e2faf23e848008aa9f6ccf31146045e3efa
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-04-08 11:51:25 +0200

    Add support for PyBytes

--- crates/capi/src/bytesobject.rs
+++ crates/capi/src/bytesobject.rs
@@ -1,16 +1,42 @@
-use core::ffi::c_char;
-use core::ptr;
-
 use crate::PyObject;
+use crate::pystate::with_vm;
+use core::ffi::c_char;
+use rustpython_vm::builtins::PyBytes;
+use rustpython_vm::convert::IntoObject;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn PyBytes_Size(_bytes: *mut PyObject) -> isize {
-    crate::log_stub("PyBytes_Size");
-    0
+pub extern "C" fn PyBytes_FromStringAndSize(bytes: *mut c_char, len: isize) -> *mut PyObject {
+    with_vm(|vm| {
+        if bytes.is_null() {
+            std::ptr::null_mut()
+        } else {
+            let bytes_slice =
+                unsafe { core::slice::from_raw_parts(bytes as *const u8, len as usize) };
+            vm.ctx
+                .new_bytes(bytes_slice.to_vec())
+                .into_object()
+                .into_raw()
+                .as_ptr()
+        }
+    })
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn PyBytes_AsString(_bytes: *mut PyObject) -> *mut c_char {
-    crate::log_stub("PyBytes_AsString");
-    ptr::null_mut()
+pub extern "C" fn PyBytes_Size(bytes: *mut PyObject) -> isize {
+    with_vm(|_vm| {
+        let bytes = unsafe { &*bytes }
+            .downcast_ref::<PyBytes>()
+            .expect("PyBytes_Size argument must be a bytes object");
+        bytes.as_bytes().len() as _
+    })
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn PyBytes_AsString(bytes: *mut PyObject) -> *mut c_char {
+    with_vm(|_vm| {
+        let bytes = unsafe { &*bytes }
+            .downcast_ref::<PyBytes>()
+            .expect("PyBytes_AsString argument must be a bytes object");
+        bytes.as_bytes().as_ptr() as _
+    })
 }
