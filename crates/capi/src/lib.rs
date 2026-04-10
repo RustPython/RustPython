a233441cdf3988822f5caa9f3c81952ef072334a
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-04-10 11:28:20 +0200

    Make all modules private

--- crates/capi/src/lib.rs
+++ crates/capi/src/lib.rs
@@ -3,20 +3,20 @@ pub use rustpython_vm::PyObject;
 
 extern crate alloc;
 
-pub mod abstract_;
-pub mod bytesobject;
-pub mod ceval;
-pub mod dictobject;
-pub mod import;
-pub mod longobject;
-pub mod object;
-pub mod pyerrors;
-pub mod pylifecycle;
-pub mod pystate;
-pub mod refcount;
-pub mod traceback;
-pub mod tupleobject;
-pub mod unicodeobject;
+pub(crate) mod abstract_;
+pub(crate) mod bytesobject;
+pub(crate) mod ceval;
+pub(crate) mod dictobject;
+pub(crate) mod import;
+pub(crate) mod longobject;
+pub(crate) mod object;
+pub(crate) mod pyerrors;
+pub(crate) mod pylifecycle;
+pub(crate) mod pystate;
+pub(crate) mod refcount;
+pub(crate) mod traceback;
+pub(crate) mod tupleobject;
+pub(crate) mod unicodeobject;
 pub(crate) mod util;
 
 #[inline]
