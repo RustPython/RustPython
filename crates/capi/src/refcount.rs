131bb0e549b805da7537bf9c29acc322b8c0db47
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-04-21 15:42:42 +0200

    Mark refcount methods as unsafe

--- crates/capi/src/refcount.rs
+++ crates/capi/src/refcount.rs
@@ -3,15 +3,13 @@ use core::ptr::NonNull;
 use rustpython_vm::PyObjectRef;
 
 #[unsafe(no_mangle)]
-#[allow(clippy::not_unsafe_ptr_arg_deref)]
-pub extern "C" fn _Py_DecRef(op: *mut PyObject) {
+pub unsafe extern "C" fn _Py_DecRef(op: *mut PyObject) {
     // By dropping PyObjectRef, we will decrement the reference count.
     unsafe { PyObjectRef::from_raw(NonNull::new_unchecked(op)) };
 }
 
 #[unsafe(no_mangle)]
-#[allow(clippy::not_unsafe_ptr_arg_deref)]
-pub extern "C" fn _Py_IncRef(op: *mut PyObject) {
+pub unsafe extern "C" fn _Py_IncRef(op: *mut PyObject) {
     // Don't drop the owned value, as we just want to increment the refcount.
     core::mem::forget(unsafe { (*op).to_owned() });
 }
