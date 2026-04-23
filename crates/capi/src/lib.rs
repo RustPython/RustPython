e6cfbb10aac7953ed154364a33b1ef471138207d
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-04-23 10:18:51 +0200

    Add capi support to `rustpython` executable

--- crates/capi/src/lib.rs
+++ crates/capi/src/lib.rs
@@ -1,8 +1,19 @@
 #![allow(clippy::missing_safety_doc)]
+
+use crate::pylifecycle::MAIN_INTERP;
+use rustpython_vm::Interpreter;
 pub use rustpython_vm::PyObject;
+use std::sync::MutexGuard;
 
 extern crate alloc;
 
 pub mod pylifecycle;
 pub mod pystate;
 pub mod refcount;
+
+/// Get main interpreter of this process. Will be None if it has not been initialized yet.
+pub fn get_main_interpreter() -> MutexGuard<'static, Option<Interpreter>> {
+    MAIN_INTERP
+        .lock()
+        .expect("Failed to lock interpreter mutex")
+}
