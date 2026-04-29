f7cb6b490f2b53b305ee9cb509251911e9990db9
Jeongseop Lim <jseoplim@gmail.com>
2026-04-29 16:25:19 +0900

    fix: Raise overflow error on complex exponentiation overflow

--- crates/vm/src/builtins/float.rs
+++ crates/vm/src/builtins/float.rs
@@ -161,7 +161,12 @@ pub fn float_pow(v1: f64, v2: f64, vm: &VirtualMachine) -> PyResult {
     } else if v1.is_sign_negative() && (v2.floor() - v2).abs() > f64::EPSILON {
         let v1 = Complex64::new(v1, 0.);
         let v2 = Complex64::new(v2, 0.);
-        Ok(v1.powc(v2).to_pyobject(vm))
+        let ans = v1.powc(v2);
+        if ans.is_infinite() && !(v1.is_infinite() || v2.is_infinite()) {
+            Err(vm.new_overflow_error("complex exponentiation"))
+        } else {
+            Ok(ans.to_pyobject(vm))
+        }
     } else {
         Ok(v1.powf(v2).to_pyobject(vm))
     }
