d8c032d11ff6efb645c1d856c9cb0dca3d5ae3ba
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-05-01 15:42:48 +0300

    Add `or_fun_call` rule

--- crates/vm/src/exceptions.rs
+++ crates/vm/src/exceptions.rs
@@ -1960,7 +1960,11 @@ pub(super) mod types {
 
             // args are truncated to 2 for compatibility (only when 2-5 args and filename is not None)
             // truncation happens inside "if (filename && filename != Py_None)" block
-            let has_filename = exc.filename.to_owned().filter(|f| !vm.is_none(f)).is_some();
+            let has_filename = exc
+                .filename
+                .to_owned()
+                .as_ref()
+                .is_some_and(|f| !vm.is_none(f));
             if (3..=5).contains(&len) && has_filename {
                 new_args.args.truncate(2);
             }
