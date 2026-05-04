3e65cecea1c60fa2303454117bda99a67eadbe9f
Jeong, YunWon <jeong@youknowone.org>
2026-05-04 22:15:34 +0900

    Unify C API thread VM tracking

--- crates/capi/src/pystate.rs
+++ crates/capi/src/pystate.rs
@@ -1,34 +1,14 @@
 use crate::pylifecycle::request_vm_from_interpreter;
-use core::cell::RefCell;
 use core::ffi::c_int;
 use core::ptr;
-use rustpython_vm::VirtualMachine;
-use rustpython_vm::vm::thread::{ThreadedVirtualMachine, VM_CURRENT, with_current_vm};
-
-thread_local! {
-    static VM: RefCell<Option<ThreadedVirtualMachine>> = const { RefCell::new(None) };
-}
-
-#[allow(dead_code)]
-pub(crate) fn with_vm<R>(f: impl FnOnce(&VirtualMachine) -> R) -> R {
-    if VM_CURRENT.is_set() {
-        // We have an active VM set, so use that.
-        with_current_vm(f)
-    } else {
-        // We do not have an active vm running in this thread. Let's use our own.
-        // This will panic if `PyGILState_Ensure` was not called beforehand.
-        VM.with(|vm_ref| {
-            let vm = vm_ref.borrow();
-            let vm = vm
-                .as_ref()
-                .expect("Thread was not attached to an interpreter");
-            vm.run(|vm| f(vm))
-        })
-    }
-}
+use rustpython_vm::vm::thread::{
+    CurrentVmAttachState, attach_current_thread, release_current_thread,
+};
 
 #[allow(non_camel_case_types)]
 type PyGILState_STATE = c_int;
+const PYGILSTATE_LOCKED: PyGILState_STATE = 0;
+const PYGILSTATE_UNLOCKED: PyGILState_STATE = 1;
 
 #[repr(C)]
 pub struct PyThreadState {
@@ -37,24 +17,24 @@ pub struct PyThreadState {
 
 /// Make sure this thread has a running vm attached. This only creates a new vm if we don't already
 /// have one. So this will only create a new vm when we are in a new thread created outside RustPython.
-pub(crate) fn ensure_thread_has_vm_attached() {
-    if !VM_CURRENT.is_set() {
-        VM.with(|vm| {
-            vm.borrow_mut()
-                .get_or_insert_with(request_vm_from_interpreter);
-        });
-    }
+pub(crate) fn ensure_thread_has_vm_attached() -> CurrentVmAttachState {
+    attach_current_thread(request_vm_from_interpreter)
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn PyGILState_Ensure() -> PyGILState_STATE {
-    ensure_thread_has_vm_attached();
-
-    0
+    match ensure_thread_has_vm_attached() {
+        CurrentVmAttachState::AlreadyAttached => PYGILSTATE_LOCKED,
+        CurrentVmAttachState::Attached => PYGILSTATE_UNLOCKED,
+    }
 }
 
 #[unsafe(no_mangle)]
-pub extern "C" fn PyGILState_Release(_state: PyGILState_STATE) {}
+pub extern "C" fn PyGILState_Release(state: PyGILState_STATE) {
+    if state == PYGILSTATE_UNLOCKED {
+        release_current_thread(CurrentVmAttachState::Attached);
+    }
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn PyEval_SaveThread() -> *mut PyThreadState {
@@ -64,25 +44,25 @@ pub extern "C" fn PyEval_SaveThread() -> *mut PyThreadState {
 #[cfg(test)]
 mod tests {
     use crate::get_main_interpreter;
-    use crate::pystate::{VM, with_vm};
+    use crate::pystate::{PyGILState_Ensure, PyGILState_Release};
     use pyo3::prelude::*;
-    use rustpython_vm::vm::thread::VM_CURRENT;
+    use rustpython_vm::vm::thread::{current_vm_is_set, with_current_vm};
 
     #[test]
     fn test_new_thread() {
         Python::attach(|_py| {
-            with_vm(|_vm| {
+            with_current_vm(|_vm| {
                 assert!(
-                    VM_CURRENT.is_set(),
+                    current_vm_is_set(),
                     "This thread did not have a vm attached"
                 )
             });
 
             std::thread::spawn(move || {
                 Python::attach(|_py| {
-                    with_vm(|_vm| {
+                    with_current_vm(|_vm| {
                         assert!(
-                            VM_CURRENT.is_set(),
+                            current_vm_is_set(),
                             "This thread did not have a vm attached"
                         )
                     });
@@ -97,9 +77,6 @@ mod tests {
     fn test_current_vm_main_thread() {
         Python::initialize();
 
-        // Detach vm from thread, because initialize attaches one automatically.
-        VM.with(|vm| vm.borrow_mut().take());
-
         // let RustPython create a vm for this thread.
         let vm = get_main_interpreter()
             .as_ref()
@@ -108,17 +85,27 @@ mod tests {
 
         // Attach the vm using RustPython
         vm.run(|_vm| {
-            assert!(VM_CURRENT.is_set(), "This thread should have a vm attached");
+            assert!(current_vm_is_set(), "This thread should have a vm attached");
 
             Python::attach(|_py| {
-                with_vm(|_vm| {
-                    assert!(VM_CURRENT.is_set());
-                    assert!(
-                        VM.with(|vm| vm.borrow().is_none()),
-                        "We should not create a new vm when there is already a vm active"
-                    )
+                with_current_vm(|_vm| {
+                    assert!(current_vm_is_set());
                 })
             })
         });
+    }
+
+    #[test]
+    fn test_gilstate_release_detaches_external_thread() {
+        Python::initialize();
+
+        std::thread::spawn(|| {
+            let state = PyGILState_Ensure();
+            assert!(current_vm_is_set());
+            PyGILState_Release(state);
+            assert!(!current_vm_is_set());
+        })
+        .join()
+        .unwrap();
     }
 }
