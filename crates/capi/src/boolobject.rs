e616e2401716362827c8e8fb86260c5f02651b61
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-05-09 15:13:22 +0200

    Add bool object functions

---
+++ crates/capi/src/boolobject.rs
@@ -0,0 +1,36 @@
+use crate::object::define_py_check;
+use crate::{PyObject, pystate::with_vm};
+use core::ffi::{c_int, c_long};
+use rustpython_vm::AsObject;
+
+define_py_check!(PyBool_Check, types.bool_type);
+
+#[unsafe(no_mangle)]
+pub extern "C" fn Py_IsTrue(obj: *mut PyObject) -> c_int {
+    with_vm(|vm| unsafe {
+        obj.as_ref()
+            .map(|obj| obj.is(&vm.ctx.true_value))
+            .unwrap_or_default()
+    })
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn Py_IsFalse(obj: *mut PyObject) -> c_int {
+    with_vm(|vm| unsafe {
+        obj.as_ref()
+            .map(|obj| obj.is(&vm.ctx.false_value))
+            .unwrap_or_default()
+    })
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn PyBool_FromLong(value: c_long) -> *mut PyObject {
+    with_vm(|vm| {
+        if value == 0 {
+            &vm.ctx.false_value
+        } else {
+            &vm.ctx.true_value
+        }
+        .to_owned()
+    })
+}
