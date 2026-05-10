a4bb6571d265ab28e3b52ff8f0c32764c6a500b1
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-05-10 14:30:58 +0300

    Add a dummy `nt._is_inputhook_installed` function

--- crates/vm/src/stdlib/nt.rs
+++ crates/vm/src/stdlib/nt.rs
@@ -2239,4 +2239,10 @@ pub(crate) mod module {
         super::super::os::module_exec(vm, module)?;
         Ok(())
     }
+
+    #[pyfunction]
+    fn _is_inputhook_installed() -> bool {
+        // TODO: Implement the actual logic here
+        false
+    }
 }
