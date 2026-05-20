e62897ba4e9fa109d152163ce28c2722b6fc94e2
Jeong, YunWon <jeong@youknowone.org>
2026-05-20 17:10:14 +0900

    Re-export mmap libc constants through host_env
    Move direct libc MADV_*, MAP_*, PROT_*, EOVERFLOW references in
    stdlib::mmap to host_env::mmap re-exports. Replace libc::c_int with
    core::ffi::c_int.

--- crates/host_env/src/mmap.rs
+++ crates/host_env/src/mmap.rs
@@ -59,7 +59,7 @@ pub use libc::MAP_STACK;
 #[cfg(target_os = "freebsd")]
 pub use libc::{MADV_AUTOSYNC, MADV_CORE, MADV_NOCORE, MADV_NOSYNC, MADV_PROTECT};
 
-#[cfg(windows)]
+#[cfg(unix)]
 pub use libc::EOVERFLOW;
 
 #[cfg(windows)]
