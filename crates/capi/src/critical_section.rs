ee95e8d587cbd3da8e1c1963505e0fd8b0b50def
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-06-04 14:24:33 +0200

    Add left over c-api functions

--- crates/capi/src/critical_section.rs
+++ crates/capi/src/critical_section.rs
@@ -3,8 +3,29 @@ use crate::PyObject;
 #[repr(C)]
 pub struct PyCriticalSection;
 
-#[unsafe(no_mangle)]
-pub extern "C" fn PyCriticalSection_Begin(_c: *mut PyCriticalSection, _op: *mut PyObject) {}
+#[repr(C)]
+pub struct PyCriticalSection2;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn PyCriticalSection_End(_c: *mut PyCriticalSection) {}
+pub extern "C" fn PyCriticalSection_Begin(c: *mut PyCriticalSection, op: *mut PyObject) {
+    let _ = (c, op);
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn PyCriticalSection_End(c: *mut PyCriticalSection) {
+    let _ = c;
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn PyCriticalSection2_Begin(
+    c: *mut PyCriticalSection2,
+    a: *mut PyObject,
+    b: *mut PyObject,
+) {
+    let _ = (c, a, b);
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn PyCriticalSection2_End(c: *mut PyCriticalSection2) {
+    let _ = c;
+}
--- crates/capi/src/import.rs
+++ crates/capi/src/import.rs
@@ -1,6 +1,7 @@
 use crate::{PyObject, pystate::with_vm};
 use core::ffi::{CStr, c_char};
-use rustpython_vm::builtins::{PyDict, PyModule, PyStr};
+use rustpython_vm::builtins::{PyCode, PyDict, PyModule, PyStr};
+use rustpython_vm::import::import_code_obj;
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn PyImport_Import(name: *mut PyObject) -> *mut PyObject {
@@ -35,6 +36,30 @@ pub unsafe extern "C" fn PyImport_AddModuleRef(name: *const c_char) -> *mut PyOb
                     Ok(module.to_owned())
                 },
             )
+    })
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn PyImport_ExecCodeModuleEx(
+    name: *const c_char,
+    co: *mut PyObject,
+    pathname: *const c_char,
+) -> *mut PyObject {
+    with_vm(|vm| {
+        let name = unsafe { CStr::from_ptr(name) }.to_str().map_err(|_| {
+            vm.new_system_error("PyImport_ExecCodeModuleEx called with non utf8 name")
+        })?;
+        let code = unsafe { &*co }.try_downcast_ref::<PyCode>(vm)?;
+        let module = import_code_obj(vm, name, code.to_owned(), false)?;
+
+        if !pathname.is_null() {
+            let pathname = unsafe { CStr::from_ptr(pathname) }.to_str().map_err(|_| {
+                vm.new_system_error("PyImport_ExecCodeModuleEx called with non utf8 pathname")
+            })?;
+            module.set_attr("__file__", vm.ctx.new_str(pathname), vm)?;
+        }
+
+        Ok(module)
     })
 }
 
