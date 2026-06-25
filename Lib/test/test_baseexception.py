b28f29313fe1d7497ee8235614abe74ce386e99e
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-06-25 16:33:51 +0300

    Update some tests to 3.14.6 (#8167)
    * Add `test_fileutils.py` from 3.14.6
    
    * Align patch for `test_zipimport_support.py`
    
    * Update `test_bigmem.py` to 3.14.6
    
    * Update `test_exception_group.py`
    
    * Align patches for `test_baseexception.py`
    
    * Update `test_exceptions.py`
    
    * Update `test_dtrace.py`
    
    * Remove patch from `string_tests.py`

--- Lib/test/test_baseexception.py
+++ Lib/test/test_baseexception.py
@@ -79,10 +79,8 @@ class ExceptionClassTests(unittest.TestCase):
 
         # Underscore-prefixed (private) exceptions don't need to be documented
         exc_set = set(e for e in exc_set if not e.startswith('_'))
-        # RUSTPYTHON specific
-        exc_set.discard("JitError")
-        # XXX: RUSTPYTHON; IncompleteInputError will be officially introduced in Python 3.15
-        exc_set.discard("IncompleteInputError")
+        exc_set.discard("JitError")  # XXX: RUSTPYTHON specific
+        exc_set.discard("IncompleteInputError")  # XXX: RUSTPYTHON; IncompleteInputError will be officially introduced in Python 3.15
         self.assertEqual(len(exc_set), 0, "%s not accounted for" % exc_set)
 
     interface_tests = ("length", "args", "str", "repr")
