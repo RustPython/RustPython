d5e3c7b5066a696ebbd5fb6e200bd98b0110ef6b
ShaharNaveh <50263213+ShaharNaveh@users.noreply.github.com>
2026-06-26 17:15:15 +0300

    Update `test_patma.py` to 3.14.6

--- Lib/test/test_patma.py
+++ Lib/test/test_patma.py
@@ -6,6 +6,7 @@ import enum
 import inspect
 import sys
 import unittest
+from test import support
 
 
 @dataclasses.dataclass
@@ -2563,15 +2564,14 @@ class TestPatma(unittest.TestCase):
         self.assertEqual(y, 0)
         self.assertEqual(z, {0: 1})
 
-    # TODO: RUSTPYTHON
-    # def test_patma_241(self):
-    #     x = [[{0: 0}]]
-    #     match x:
-    #         case list([({-0-0j: int(real=0+0j, imag=0-0j) | (1) as z},)]):
-    #             y = 0
-    #     self.assertEqual(x, [[{0: 0}]])
-    #     self.assertEqual(y, 0)
-    #     self.assertEqual(z, 0)
+    def test_patma_241(self):
+        x = [[{0: 0}]]
+        match x:
+            case list([({-0-0j: int(real=0+0j, imag=0-0j) | (1) as z},)]):
+                y = 0
+        self.assertEqual(x, [[{0: 0}]])
+        self.assertEqual(y, 0)
+        self.assertEqual(z, 0)
 
     def test_patma_242(self):
         x = range(3)
@@ -3013,6 +3013,13 @@ class TestSyntaxErrors(unittest.TestCase):
         self.assert_syntax_error("""
         match ...:
             case a as a:
+                pass
+        """)
+
+    def test_multiple_assignments_to_name_in_pattern_6(self):
+        self.assert_syntax_error("""
+        match ...:
+            case a as a + 1:  # NAME and expression with no ()
                 pass
         """)
 
@@ -3492,6 +3499,7 @@ class TestTracing(unittest.TestCase):
         self.assertListEqual(self._trace(f, 1), [1, 2, 3])
         self.assertListEqual(self._trace(f, 0), [1, 2, 5, 6])
 
+    @support.skip_wasi_stack_overflow()
     def test_parser_deeply_nested_patterns(self):
         # Deeply nested patterns can cause exponential backtracking when parsing.
         # See gh-93671 for more information.
