68a8e8318b2a542d73d064a19dc0e5f7e95cf420
Luigi The <luigigithub@gmail.com>
2026-07-02 15:50:43 -0400

    test snippet fix + doc error

--- extra_tests/snippets/jit.py
+++ extra_tests/snippets/jit.py
@@ -1,15 +1,18 @@
-def foo():
+def foo() -> int:
     a = 5
     return 10 + a
 
 
-def bar():
+def bar() -> float:
     a = 1e6
     return a / 5.0
 
 
-def baz(a: int, b: int):
+def baz(a: float, b: float) -> float:
     return a + b + 12
+
+def tuple_identity(t: tuple) -> tuple:
+    return t
 
 
 def tests():
@@ -17,6 +20,9 @@ def tests():
     assert bar() == 2e5
     assert baz(17, 20) == 49
     assert baz(17, 22.5) == 51.5
+
+    tupleId = (1, 2)
+    assert tuple_identity(tupleId) == tupleId
 
 
 tests()
@@ -26,4 +32,5 @@ if hasattr(foo, "__jit__"):
     foo.__jit__()
     bar.__jit__()
     baz.__jit__()
+    tuple_identity.__jit__()
     tests()
