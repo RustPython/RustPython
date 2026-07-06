adf599537188894ef40edc7afb7fe3631ae6de14
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-07-06 14:18:50 +0200

    Review

--- crates/capi/src/pyframe.rs
+++ crates/capi/src/pyframe.rs
@@ -6,7 +6,7 @@ use rustpython_vm::frame::Frame;
 pub unsafe extern "C" fn PyFrame_GetCode(frame: *mut PyObject) -> *mut PyObject {
     with_vm(|vm| {
         let frame = unsafe { &*frame }.try_downcast_ref::<Frame>(vm)?;
-        Ok(frame.code.clone())
+        Ok(frame.f_code())
     })
 }
 
@@ -14,11 +14,6 @@ pub unsafe extern "C" fn PyFrame_GetCode(frame: *mut PyObject) -> *mut PyObject 
 pub unsafe extern "C" fn PyFrame_GetLineNumber(frame: *mut PyObject) -> core::ffi::c_int {
     with_vm(|vm| {
         let frame = unsafe { &*frame }.try_downcast_ref::<Frame>(vm)?;
-        let lineno = if frame.lasti() == 0 {
-            frame.code.first_line_number.map_or(1, |n| n.get())
-        } else {
-            frame.current_location().line.get()
-        };
-        Ok(lineno as core::ffi::c_int)
+        Ok(frame.f_lineno() as core::ffi::c_int)
     })
 }
