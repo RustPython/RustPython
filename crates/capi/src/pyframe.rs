d3f0729fdff740c99dece6855d5ad342ef833157
Bas Schoenmaeckers <7943856+bschoenmaeckers@users.noreply.github.com>
2026-07-09 07:14:56 +0200

    Add more eval functions to c-api (#8244)

--- crates/capi/src/pyframe.rs
+++ crates/capi/src/pyframe.rs
@@ -1,19 +1,21 @@
-use crate::PyObject;
 use crate::pystate::with_vm;
+use core::ffi::c_int;
+use rustpython_vm::Py;
+use rustpython_vm::builtins::PyCode;
 use rustpython_vm::frame::Frame;
 
+pub type PyFrameObject = Py<Frame>;
+pub type PyCodeObject = Py<PyCode>;
+
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn PyFrame_GetCode(frame: *mut PyObject) -> *mut PyObject {
-    with_vm(|vm| {
-        let frame = unsafe { &*frame }.try_downcast_ref::<Frame>(vm)?;
-        Ok(frame.f_code())
-    })
+pub unsafe extern "C" fn PyFrame_GetCode(frame: *mut PyFrameObject) -> *mut PyCodeObject {
+    with_vm(|_vm| Ok(unsafe { &*frame }.f_code()))
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn PyFrame_GetLineNumber(frame: *mut PyObject) -> core::ffi::c_int {
-    with_vm(|vm| {
-        let frame = unsafe { &*frame }.try_downcast_ref::<Frame>(vm)?;
-        Ok(frame.f_lineno() as core::ffi::c_int)
+pub unsafe extern "C" fn PyFrame_GetLineNumber(frame: *mut PyFrameObject) -> c_int {
+    with_vm(|_vm| {
+        let lineno = unsafe { &*frame }.f_lineno();
+        Ok(lineno.try_into().unwrap_or(c_int::MAX))
     })
 }
