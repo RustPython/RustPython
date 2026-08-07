f76f442b8663649b85684ffd856ccb53da9f9136
KangDora <ioprt123@gmail.com>
2026-08-08 08:00:12 +0900

    Apply ruff formatting to the new list sort tests

--- extra_tests/snippets/builtin_list.py
+++ extra_tests/snippets/builtin_list.py
@@ -242,7 +242,13 @@ assert sorted([(1, 2, 3), (0, 3, 6)], key=lambda x: x[0]) == [(0, 3, 6), (1, 2, 
 assert sorted([(1, 2, 3), (0, 3, 6)], key=lambda x: x[1]) == [(1, 2, 3), (0, 3, 6)]
 assert sorted([(1, 2), (), (5,)], key=len) == [(), (5,), (1, 2)]
 
-assert sorted(["b", "a", "é", "z\U0001F600", "z"]) == ["a", "b", "z", "z\U0001F600", "é"]
+assert sorted(["b", "a", "é", "z\U0001f600", "z"]) == [
+    "a",
+    "b",
+    "z",
+    "z\U0001f600",
+    "é",
+]
 assert sorted([10**30, -(10**30), 5, 0]) == [-(10**30), 0, 5, 10**30]
 assert sorted([True, False, True]) == [False, True, True]
 
