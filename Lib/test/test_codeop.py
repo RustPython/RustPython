5f8603d56ada7cfe6cd1ceb1dd12750e1135df7a
Jeong, YunWon <jeong@youknowone.org>
2026-09-11 20:50:04 +0900

    Remove expectedFailure from passing test_codeop cases
    test_invalid_warning and test_raw_raises_error now emit
    SyntaxWarning on parse-fail invalid escapes.
    
    Assisted-by: Grok:4.6

--- Lib/test/test_codeop.py
+++ Lib/test/test_codeop.py
@@ -348,7 +348,6 @@ class CodeopTests(unittest.TestCase):
         compiler("'\\e' + (")
         self.assertEqual(w, [])
 
-    @unittest.expectedFailure  # TODO: RUSTPYTHON; AssertionError: 0 != 1
     @subTests('compiler', RAW_COMPILERS)
     def test_raw_raises_error(self, compiler):
         warnings_cm = warnings_helper.check_warnings(
@@ -358,7 +357,6 @@ class CodeopTests(unittest.TestCase):
             compiler("'\\e' + (", "<input>", 'single')
         self.assertEqual(len(w.warnings), 1)
 
-    @unittest.expectedFailure  # TODO: RUSTPYTHON; AssertionError: 0 != 1
     @subTests('compiler', COMPILERS)
     def test_invalid_warning(self, compiler):
         with warnings.catch_warnings(record=True) as w:
