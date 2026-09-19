24339893e6b047c2a79f3f482767504cc8e8f51a
Jeong, YunWon <jeong@youknowone.org>
2026-09-20 01:19:29 +0900

    Unskip linux RLock repr; keep fork pickling hang skip
    test_repr_rlock passed on Linux spawn/fork/forkserver. Connection
    pickling hangs under the fork start method, so skip it only there.
    
    Build the apple-container image from rust:bookworm; rust:bullseye
    apt indexes 404.
    
    Assisted-by: Grok:4.6

--- Lib/test/test_multiprocessing_forkserver/test_processes.py
+++ Lib/test/test_multiprocessing_forkserver/test_processes.py
@@ -3,13 +3,5 @@ from test._test_multiprocessing import install_tests_in_module_dict
 
 install_tests_in_module_dict(globals(), 'forkserver', only_type="processes")
 
-import sys  # TODO: RUSTPYTHON
-class WithProcessesTestLock(WithProcessesTestLock):  # TODO: RUSTPYTHON
-    @unittest.skipIf(  # TODO: RUSTPYTHON
-        sys.platform == 'linux',  # TODO: RUSTPYTHON
-        'TODO: RUSTPYTHON flaky BrokenPipeError, flaky ConnectionRefusedError, flaky ConnectionResetError, flaky EOFError'
-    )  # TODO: RUSTPYTHON
-    def test_repr_rlock(self): super().test_repr_rlock()  # TODO: RUSTPYTHON
-
 if __name__ == '__main__':
     unittest.main()
