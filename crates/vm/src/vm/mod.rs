c476bb1759d73d2550fa37e841b75560802eb194
Lakshmi Priya <Lakshmi.Priya9@ibm.com>
2026-09-21 13:18:01 +0530

    fix: address review feedback on error message
    - Remove leading whitespace on continuation line
    - Update wording: 'If RUSTPYTHONPATH is already set, explicitly load it to path'
    - Use std::env::split_paths instead of split(':') for cross-platform support

--- crates/vm/src/vm/mod.rs
+++ crates/vm/src/vm/mod.rs
@@ -1143,15 +1143,16 @@ impl VirtualMachine {
             } else if path_contains_env {
                 "RUSTPYTHONPATH or PYTHONPATH is set, but it doesn't contain the encodings library. If you are customizing the RustPython vm/interpreter, try adding the stdlib directory to the path. If you are developing the RustPython interpreter, it might be a bug during development."
             } else {
-                "RUSTPYTHONPATH or PYTHONPATH is set, but it wasn't loaded to `PyConfig::paths::module_search_paths`. \
-Environment variables are not loaded into the Settings struct by default when embedding RustPython. \
-To fix this, add the stdlib path manually:\n\
+                "RUSTPYTHONPATH or PYTHONPATH is set, but it wasn't loaded to `PyConfig::paths::module_search_paths`. Environment variables are not loaded into the Settings struct by default when embedding RustPython. To fix this, add the stdlib path manually:\n\
 \n\
     let settings = rustpython_vm::Settings::default().with_path(\"/path/to/stdlib\".to_owned());\n\
 \n\
-Alternatively, set the RUSTPYTHONPATH environment variable and load it explicitly:\n\
+If `RUSTPYTHONPATH` is already set, explicitly load it to path:\n\
 \n\
-    settings.path_list.extend(std::env::var(\"RUSTPYTHONPATH\").unwrap_or_default().split(':').map(String::from));\n\
+    settings.path_list.extend(\n\
+        std::env::split_paths(&std::env::var_os(\"RUSTPYTHONPATH\").unwrap_or_default())\n\
+            .map(|path| path.to_string_lossy().into_owned()),\n\
+    );\n\
 \n\
 If you are developing the RustPython interpreter itself, this might be a bug during development."
 
