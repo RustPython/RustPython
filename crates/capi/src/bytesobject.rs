405fbfb3dabf4f94169035903b8395572e7a557a
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-09-23 12:49:29 +0200

    Impl `PyBytes_FromString` `PyBytes_FromObject` `PyBytes_AsStringAndSize`

--- crates/capi/src/bytesobject.rs
+++ crates/capi/src/bytesobject.rs
@@ -1,6 +1,6 @@
 use crate::object::define_py_check;
 use crate::{PyObject, pystate::with_vm};
-use core::ffi::c_char;
+use core::ffi::{CStr, c_char, c_int};
 use rustpython_vm::builtins::PyBytes;
 
 define_py_check!(fn PyBytes_Check, types.bytes_type);
@@ -30,6 +30,19 @@ pub unsafe extern "C" fn PyBytes_FromStringAndSize(
 }
 
 #[unsafe(no_mangle)]
+pub unsafe extern "C" fn PyBytes_FromString(s: *const c_char) -> *mut PyObject {
+    with_vm(|vm| {
+        let data = unsafe { CStr::from_ptr(s) }.to_bytes().to_vec();
+        Ok(vm.ctx.new_bytes(data))
+    })
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn PyBytes_FromObject(obj: *mut PyObject) -> *mut PyObject {
+    with_vm(|vm| unsafe { &*obj }.try_bytes_like(vm, |bytes| vm.ctx.new_bytes(bytes.to_vec())))
+}
+
+#[unsafe(no_mangle)]
 pub unsafe extern "C" fn PyBytes_Size(bytes: *mut PyObject) -> isize {
     with_vm(|vm| {
         let bytes = unsafe { &*bytes }.try_downcast_ref::<PyBytes>(vm)?;
@@ -42,6 +55,29 @@ pub unsafe extern "C" fn PyBytes_AsString(bytes: *mut PyObject) -> *mut c_char {
     with_vm(|vm| {
         let bytes = unsafe { &*bytes }.try_downcast_ref::<PyBytes>(vm)?;
         Ok(bytes.as_bytes().as_ptr())
+    })
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn PyBytes_AsStringAndSize(
+    obj: *mut PyObject,
+    buffer: *mut *mut c_char,
+    length: *mut isize,
+) -> c_int {
+    with_vm(|vm| {
+        let data = unsafe { &*obj }.try_downcast_ref::<PyBytes>(vm)?.as_bytes();
+
+        if length.is_null() {
+            if data.contains(&0) {
+                return Err(vm.new_value_error("embedded null byte"));
+            }
+        } else {
+            unsafe { *length = data.len() as isize };
+        };
+
+        unsafe { *buffer = data.as_ptr().cast_mut().cast() };
+
+        Ok(())
     })
 }
 
